1fd945a29914da2f2a547dc2e64df33cac7d504a
Jun <jun.juno@kakaostyle.com>
2022-04-23 01:16:44 +0900

    chore: commitlint rules 명시적으로 추가

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,19 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // 새로운 기능
+        'fix', // 버그 수정
+        'hotfix', // 핫픽스
+        'docs', // 문서
+        'refactor', // 리팩토링
+        'test', // 테스트 코드
+        'chore', // 기타 자잘한 작업
+        'revert', // 리버트 커밋
+      ],
+    ],
+  },
+};
