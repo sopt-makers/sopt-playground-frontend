bc30f559138e85ac8771c234c50b52cb9d8948a9
Jun <jun.juno@kakaostyle.com>
2022-04-30 18:49:26 +0900

    chore: storybook msw 세팅, 예시 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,12 @@
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+initialize();
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { cacheTime: 300000, refetchOnWindowFocus: false, staleTime: 300000, retry: 1 } },
+});
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -7,3 +16,12 @@ export const parameters = {
     },
   },
 };
+
+export const decorators = [
+  (Story) => (
+    <QueryClientProvider client={queryClient}>
+      <Story />
+    </QueryClientProvider>
+  ),
+  mswDecorator,
+];
---
+++ components/common/example/Example.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, Story } from '@storybook/react';
+import Example, { API_PATH } from 'components/common/example/Example';
+import { rest } from 'msw';
+
+const random = { message: 'https://images.dog.ceo/breeds/spaniel-irish/n02102973_377.jpg', status: 'success' };
+
+export default {
+  title: 'components/예시',
+  component: Example,
+  parameters: {
+    msw: {
+      handlers: [
+        rest.get(API_PATH, (_req, res, ctx) => {
+          return res(ctx.json(random));
+        }),
+      ],
+    },
+  },
+} as Meta;
+
+export const Default: Story = () => <Example />;
+Default.storyName = 'msw 예시';
