a8155c5ab1a5cfba90435e35fe78be557279ffb8
Jun <jun.juno@kakaostyle.com>
2022-05-22 17:57:54 +0900

    chore: svgr 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   stories: ['../**/*.stories.@(js|jsx|ts|tsx|mdx)'],
@@ -15,6 +16,15 @@ module.exports = {
       test: /\.(css|scss)$/,
       use: ['style-loader', 'css-loader', 'sass-loader'],
     });
+
+    // MEMO: 스토리북에선 svg를 file-loader로 읽어오는데, 이를 svgr로 읽어오도록 설정
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test.test('.svg'));
+    fileLoaderRule.exclude = /\.svg$/;
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
     return config;
   },
   staticDirs: ['../public'],
--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
