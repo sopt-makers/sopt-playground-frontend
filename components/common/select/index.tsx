ead671f790b568f5d47e1c8e681373873b17ccaf
Jun <jun.juno@kakaostyle.com>
2022-05-27 02:52:07 +0900

    chore: select 수정

--- components/common/select/index.tsx
+++ components/common/select/index.tsx
@@ -19,11 +19,11 @@ export default Select;
 const StyledSelect = styled.select<Pick<SelectProps, 'width'>>`
   border: 1px solid transparent;
   border-radius: 6px;
-  background: top 22px right 16px ${colors.black80} no-repeat
+  background: top 22px right 16px ${colors.black60} no-repeat
     url("data:image/svg+xml,%3Csvg width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M9.42387 11.6742C9.18956 11.9086 8.80966 11.9086 8.57535 11.6742L3.70034 6.79924C3.46603 6.56492 3.46603 6.18503 3.70034 5.95071C3.93466 5.7164 4.31456 5.7164 4.54887 5.95071L8.99961 10.4014L13.4503 5.95071C13.6847 5.7164 14.0646 5.7164 14.2989 5.95071C14.5332 6.18503 14.5332 6.56492 14.2989 6.79924L9.42387 11.6742Z' fill='%23646464'/%3E%3C/svg%3E%0A");
   padding: 20px;
-  width: ${(p) => p.width}px;
   color: ${colors.gray100};
+  ${({ width }) => `width: ${width}px`};
 
   &:focus {
     border-color: ${colors.purple100};
