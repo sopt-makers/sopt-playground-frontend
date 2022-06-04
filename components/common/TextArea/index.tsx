0b9ca3dd88ca8e72201b7fff6d3c7635e562ae4d
Jun <jun.juno@kakaostyle.com>
2022-06-04 23:57:13 +0900

    feat: textarea 구현

---
+++ components/common/TextArea/index.tsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+import { FC, HTMLAttributes, PropsWithChildren } from 'react';
+import { colors } from 'styles/common/colors';
+import { textStyles } from 'styles/common/typography';
+
+interface TextAreaProps extends HTMLAttributes<HTMLTextAreaElement> {}
+
+const TextArea: FC<PropsWithChildren<TextAreaProps>> = ({ ...props }) => {
+  return <StyledTextArea {...props}></StyledTextArea>;
+};
+export default TextArea;
+
+const StyledTextArea = styled.textarea`
+  transition: all 0.2s;
+  border: 1.5px solid ${colors.black60};
+  border-radius: 6px;
+  background-color: ${colors.black60};
+  padding: 14px 20px;
+  width: 100%;
+  resize: none;
+  color: ${colors.white};
+  ${textStyles.SUIT_16_M}
+
+  &::placeholder {
+    color: ${colors.gray100};
+  }
+
+  &:focus {
+    outline: none;
+    border-color: ${colors.purple100};
+    background-color: ${colors.black80};
+  }
+`;
