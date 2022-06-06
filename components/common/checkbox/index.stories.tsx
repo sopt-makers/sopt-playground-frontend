8e16f2314bdd14c80ef1c1eecb8e6c1e401df662
Jun <jun.juno@kakaostyle.com>
2022-06-06 22:22:35 +0900

    feat: checkbox 수정

--- components/common/checkbox/index.stories.tsx
+++ components/common/checkbox/index.stories.tsx
@@ -1,9 +1,13 @@
 import { ComponentMeta, Story } from '@storybook/react';
 import Checkbox from 'components/common/checkbox';
+import { useState } from 'react';
 
 export default {
   components: Checkbox,
 } as ComponentMeta<typeof Checkbox>;
 
-export const Basic: Story = () => <Checkbox />;
+export const Basic: Story = () => {
+  const [checked, onChange] = useState<boolean>(false);
+  return <Checkbox checked={checked} onChange={(e) => onChange(e.target.checked)} />;
+};
 Basic.storyName = '기본';
