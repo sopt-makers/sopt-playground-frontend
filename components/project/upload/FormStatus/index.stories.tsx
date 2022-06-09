53903d43ddbebe3a95a6932b919f204bdb10a2dd
Jun <jun.juno@kakaostyle.com>
2022-06-10 03:57:14 +0900

    chore: formStatus 스토리 추가

---
+++ components/project/upload/FormStatus/index.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { formItems } from 'components/project/upload/constants';
+import UploadStatus from 'components/project/upload/FormStatus';
+
+export default {
+  component: UploadStatus,
+} as ComponentMeta<typeof UploadStatus>;
+
+const Template: ComponentStory<typeof UploadStatus> = (args) => <UploadStatus {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  formItems,
+};
+Default.storyName = '기본';
