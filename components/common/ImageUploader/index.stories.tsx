b392cab43c56e4e154fab37d0c2c0ad93d7eb1ac
Jun <jun.juno@kakaostyle.com>
2022-06-27 02:35:49 +0900

    feat: image uploader 구현

---
+++ components/common/ImageUploader/index.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentMeta } from '@storybook/react';
+import { Controller, useForm } from 'react-hook-form';
+import ImageUploader from '.';
+
+export default {
+  component: ImageUploader,
+} as ComponentMeta<typeof ImageUploader>;
+
+export const Default = () => {
+  const { control } = useForm<{ image: File }>();
+
+  return <Controller name='image' control={control} render={({ field }) => <ImageUploader {...field} />} />;
+};
+Default.args = {};
+Default.storyName = '기본';
