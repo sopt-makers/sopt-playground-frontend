89b02ad7ab943887e7586be1637b75e8b0041ae4
Jun <jun.juno@kakaostyle.com>
2022-07-02 17:54:46 +0900

    chore: 스키마 검증 추가, 팀원 이름 카테고리에 따라 변경

--- pages/project/upload/index.tsx
+++ pages/project/upload/index.tsx
@@ -1,14 +1,12 @@
 import styled from '@emotion/styled';
-import { formItems, ServiceType, Category, Period } from '@/components/project/upload/constants';
+import { categoryLabel, formItems } from '@/components/project/upload/constants';
 import { FC } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { colors } from '@/styles/colors';
-import TextArea from '@/components/common/TextArea';
 import FormStatus from '@/components/project/upload/FormStatus';
 import useMemberForm from '@/components/project/upload/MemberForm/useMemberForm';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-import FormDateTerm from '@/components/project/upload/ProjectPeriod';
 import ProjectServiceType from '@/components/project/upload/ProjectServiceType';
 import useLinkForm from '@/components/project/upload/LinkForm/useLinkForm';
 import { Link } from '@/components/project/upload/LinkForm/constants';
@@ -23,23 +21,34 @@ import ProjectSummary from '@/components/project/upload/ProjectSummary';
 import ProjectDetail from '@/components/project/upload/ProjectDetail';
 import ProjectLink from '@/components/project/upload/ProjectLink';
 import ProjectImageSection from '@/components/project/upload/ProjectImageSection';
+import { Period, ServiceType, Category, FormItem } from '@/components/project/upload/types';
 
 const schema = yup.object().shape({
   'name': yup.string().required('프로젝트 이름을 입력해주세요'),
-  'officialActivity': yup.string().required('어디서 진행했는지 선택해주세요'),
-  'termDate.dateFrom': yup.date().required('프로젝트 기간을 입력해주세요'),
-  'termDate.dateTo': yup.date().required('프로젝트 기간을 입력해주세요'),
+  'generation': yup.string(),
+  'category': yup.string().required('어디서 진행했는지 선택해주세요'),
+  'isAvaliable': yup.boolean().required(),
+  'isFounding': yup.boolean().required(),
+  'serviceType': yup.array().required('서비스 형태를 선택해주세요.'),
+  'period.startAt': yup.date().required('프로젝트 시작일을 입력해주세요'),
+  'period.endAt': yup.date().required('프로젝트 종료일을 입력해주세요'),
+  'summary': yup.string().required('프로젝트 한줄 소개를 입력해주세요'),
+  'detail': yup.string().required('프로젝트 설명을 입력해주세요'),
+  'logoImage': yup.string().required('로고 이미지를 업로드해 주세요'),
+  'thumbnailIamge': yup.string(),
+  'projectIamge': yup.string(),
+  'link': yup.object(),
 });
 
 export interface ProjectUploadForm {
   name: string;
-  generation: string;
+  generation?: string;
   generationChecked: boolean;
   category: Category;
   isAvailable: boolean;
   isFounding: boolean;
-  originalMembers: any;
-  additionalMembers: any;
+  members: any;
+  releaseMembers: any;
   serviceType: ServiceType[];
   period: Period;
   summary: string;
@@ -62,23 +71,34 @@ const ProjectUploadPage: FC = () => {
       },
       serviceType: [],
     },
+    mode: 'onSubmit',
   });
   const {
     handleSubmit,
+    watch,
     formState: { dirtyFields },
   } = methods;
   const { members, ...memberFormProps } = useMemberForm();
   const { members: releaseMembers, ...releaseMemberFormProps } = useMemberForm();
   const { links, ...linkFormProps } = useLinkForm();
+  const category = watch('category');
+  const label = categoryLabel?.[category] ?? '';
 
-  const _formItems = formItems.map((formItem) =>
-    dirtyFields?.[formItem.value]
-      ? {
-          ...formItem,
-          isDirty: true,
-        }
-      : formItem,
-  );
+  const _formItems = formItems
+    .map((formItem) =>
+      dirtyFields?.[formItem.value]
+        ? {
+            ...formItem,
+            isDirty: true,
+          }
+        : formItem,
+    )
+    .reduce(
+      (acc: FormItem[], cur) =>
+        cur.value === 'members' ? [...acc, { ...cur, label: `${label} 팀원` }] : [...acc, cur],
+
+      [],
+    );
 
   const onSubmit = (data: ProjectUploadForm) => {
     // TODO: api 연결
@@ -88,12 +108,12 @@ const ProjectUploadPage: FC = () => {
     <FormProvider {...methods}>
       <StyledForm onSubmit={handleSubmit(onSubmit)}>
         <FormStatus formItems={_formItems} />
-        <Project>
+        <ProjectContainer>
           <ProjectName />
           <ProjectGeneration />
           <ProjectCategory />
           <ProjectStatus />
-          <ProjectMembers members={members} {...memberFormProps} />
+          <ProjectMembers type={categoryLabel?.[category] ?? ''} members={members} {...memberFormProps} />
           <ProjectReleaseMembers members={releaseMembers} {...releaseMemberFormProps} />
           <ProjectServiceType />
           <ProjectPeriod />
@@ -101,7 +121,7 @@ const ProjectUploadPage: FC = () => {
           <ProjectDetail />
           <ProjectImageSection />
           <ProjectLink links={links} {...linkFormProps} />
-        </Project>
+        </ProjectContainer>
       </StyledForm>
     </FormProvider>
   );
@@ -113,10 +133,9 @@ const StyledForm = styled.form`
   display: flex;
   gap: 40px;
   justify-content: center;
-  margin: 167px auto 0;
 `;
 
-const Project = styled.section`
+const ProjectContainer = styled.div`
   display: flex;
   flex-direction: column;
   border-radius: 12px;
