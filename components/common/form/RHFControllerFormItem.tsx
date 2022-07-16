432dfbdb962ead07c4f81cce48b7031d19bfdeaa
jun <jun.juno@kakaostyle.com>
2022-07-16 17:03:40 +0900

    feat: hookform controller 래핑한 컴포넌트 구현

---
+++ components/common/form/RHFControllerFormItem.tsx
@@ -0,0 +1,52 @@
+import FormItem, { FormItemProps } from '@/components/common/form/FormItem';
+import React from 'react';
+import { useController, UseControllerProps, FieldValues, FieldPath } from 'react-hook-form';
+
+type RHFControllerFormItemProps<
+  T extends React.ElementType,
+  TFieldValues extends FieldValues,
+  TFieldPath extends FieldPath<TFieldValues>,
+> = {
+  component: T;
+} & FormItemProps &
+  UseControllerProps<TFieldValues, TFieldPath> &
+  Omit<React.ComponentProps<T>, 'onChange' | 'value'>;
+
+/**
+ * React-Hook-Form의 Controller를 통해 래핑된 FormItem 입니다.
+ * 기존의 라이브러리나 직접 제작한 컴포넌트인 경우, register가 아닌 Controller 방식으로 접근해야 하는데, 이를 위해 만들었습니다.
+ *
+ */
+const RHFControllerFormItem = <
+  T extends React.ElementType,
+  TFieldValues extends FieldValues,
+  TFieldPath extends FieldPath<TFieldValues>,
+>({
+  component: Component,
+  name,
+  control,
+  rules,
+  shouldUnregister,
+  defaultValue,
+  ...props
+}: RHFControllerFormItemProps<T, TFieldValues, TFieldPath>) => {
+  const {
+    field,
+    fieldState,
+    formState: { errors },
+  } = useController({ name, control, rules, shouldUnregister, defaultValue });
+
+  return (
+    <FormItem errorMessage={errors?.[name]?.message}>
+      <Component
+        {...({
+          error: !!fieldState.error,
+          ...field,
+          ...props,
+        } as React.ComponentProps<T>)}
+      />
+    </FormItem>
+  );
+};
+
+export default RHFControllerFormItem;
