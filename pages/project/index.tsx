434ae8d2bad06d959e9c08be0f0a910ed4b86172
jun <jun.juno@kakaostyle.com>
2022-07-16 20:00:12 +0900

    chore: 프로젝트 메인 레이아웃

--- pages/project/index.tsx
+++ pages/project/index.tsx
@@ -1,10 +1,37 @@
+import styled from '@emotion/styled';
+import ProjectCard from '@/components/project/main/ProjectCard';
+import { FC } from 'react';
 import useGetProjectListQuery from '@/components/project/upload/hooks/useGetProjectListQuery';
 
-const ProjectPage = () => {
+const ProjectPage: FC = () => {
   const { data } = useGetProjectListQuery();
-
-  console.log('[data]: ', data);
-  return <div>ProjectPage</div>;
+  
+  return (
+    <StyledContainer>
+      <StyledContent>
+        <h1>Projects</h1>
+        <StyledGridContainer>{/* Project Cards */}</StyledGridContainer>
+      </StyledContent>
+    </StyledContainer>
+  );
 };
 
 export default ProjectPage;
+
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+
+const StyledContent = styled.div`
+  justify-self: flex-start;
+  margin: 64px 0 0;
+`;
+
+const StyledGridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  row-gap: 30px;
+  column-gap: 64px;
+`;
