c28ceee490f126c5d8dbe0d595ff1af8ae5d7a73
jun <jun.juno@kakaostyle.com>
2022-07-16 19:06:10 +0900

    feat: 헤더 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import GlobalStyle from '@/styles/GlobalStyle';
+import Header from '@/components/common/Header';
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { cacheTime: 300000, refetchOnWindowFocus: false, staleTime: 300000, retry: 1 } },
@@ -12,6 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
         <GlobalStyle />
+        <Header />
         <Component {...pageProps} />
       </RecoilRoot>
     </QueryClientProvider>
