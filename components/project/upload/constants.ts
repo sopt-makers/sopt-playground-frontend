ac3a5360ac15d2ff71728eb02ab46e4c85a9548e
Jun <jun.juno@kakaostyle.com>
2022-08-21 18:52:09 +0900

    chore: 프로젝트 진행상태 > 이미지 선택값으로 변경

--- components/project/upload/constants.ts
+++ components/project/upload/constants.ts
@@ -84,7 +84,7 @@ export const FORM_ITEMS: FormItem[] = [
     label: '프로젝트 이미지',
     value: 'projectImage',
     isDirty: false,
-    isRequired: true,
+    isRequired: false,
   },
   {
     label: '링크',
@@ -93,13 +93,3 @@ export const FORM_ITEMS: FormItem[] = [
     isRequired: false,
   },
 ];
-
-// dirtyFields: defaultValue 를 설정했을때, defaultValue랑 같은지 다른지 여부로 판단
-// const _formItems = FORM_ITEMS.map((formItem) => ({
-//   ...formItem,
-//   isDirty: dirtyFields?.[formItem.value] ? true : formItem.isDirty,
-// })).reduce(
-//   (acc: FormItem[], cur) => (cur.value === 'members' ? [...acc, { ...cur, label: `${label} 팀원` }] : [...acc, cur]),
-//   [],
-// );
-// 팀원 - 필수 팀원은 하나라도 들어가고, 값이 모두 채워져 있을 경우
