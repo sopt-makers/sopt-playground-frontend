da1cd48aaaba0861855bb80f341c0514d48517ea
eunsukimme <eunsu.dev@gmail.com>
2022-08-21 17:08:37 +0900

    feat: add info section

--- pages/project/[projectId]/index.tsx
+++ pages/project/[projectId]/index.tsx
@@ -1,7 +1,10 @@
 import useGetProjectQuery from '@/components/project/upload/hooks/useGetProjectQuery';
+import { colors } from '@/styles/colors';
+import { textStyles } from '@/styles/typography';
 import styled from '@emotion/styled';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import dayjs from 'dayjs';
 
 export default function ProjectDetailPage() {
   const router = useRouter();
@@ -13,13 +16,30 @@ export default function ProjectDetailPage() {
     console.log(data);
   }, [data]);
 
+  const startAt = dayjs(data?.start_at).format('YYYY-MM');
+  const endAt = data?.end_at ? dayjs(data.end_at).format('YYYY-MM') : '';
+
   return (
     <Container>
       <Header>
-        <LogoImageWrapper>
-          <LogoImage src={data?.logo_image} alt={data?.name} />
-        </LogoImageWrapper>
-        <div></div>
+        <ServiceTypeWrapper>
+          {data?.service_type.map((type) => (
+            <ServiceType key={type}>{type}</ServiceType>
+          ))}
+        </ServiceTypeWrapper>
+        <ServiceInfoWrapper>
+          <LogoImageWrapper>
+            <LogoImage src={data?.logo_image} alt={data?.name} />
+          </LogoImageWrapper>
+          <InfoWrapper>
+            <Name>{data?.name}</Name>
+            <Description>{data?.summary}</Description>
+            <div>
+              <StartEndAt>{startAt}</StartEndAt>
+              {endAt ? <StartEndAt> - {endAt}</StartEndAt> : <InProgress>진행 중</InProgress>}
+            </div>
+          </InfoWrapper>
+        </ServiceInfoWrapper>
       </Header>
     </Container>
   );
@@ -27,14 +47,34 @@ export default function ProjectDetailPage() {
 
 const Container = styled.div`
   margin: 0 auto;
-  border: 1px solid red;
   width: 100%;
   max-width: 1200px;
 `;
 const Header = styled.div`
   display: flex;
+  flex-direction: column;
+  gap: 22px;
+`;
+const ServiceTypeWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 194px;
+`;
+const ServiceType = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 2000px;
+  box-shadow: 0 2px 4px 0 rgb(0 0 0 / 15%);
+  background-color: white;
+  padding: 6px 12px;
+  color: ${colors.black40};
+  ${textStyles.SUIT_12_B};
+`;
+const ServiceInfoWrapper = styled.div`
+  display: flex;
   gap: 44px;
-  align-items: flex-end;
+  align-items: center;
 `;
 const LogoImageWrapper = styled.div`
   flex-shrink: 0;
@@ -45,4 +85,34 @@ const LogoImageWrapper = styled.div`
 `;
 const LogoImage = styled.img`
   object-fit: cover;
+`;
+const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Name = styled.h2`
+  margin-bottom: 18px;
+  line-height: 100%;
+  font-size: 44px;
+  font-weight: 700;
+`;
+const Description = styled.p`
+  margin-bottom: 32px;
+  line-height: 100%;
+  font-size: 24px;
+  font-weight: 400;
+`;
+const StartEndAt = styled.span`
+  display: inline-block;
+  line-height: 100%;
+  color: ${colors.gray60};
+  font-size: 18px;
+  font-weight: 500;
+`;
+const InProgress = styled.span`
+  margin-left: 12px;
+  line-height: 100%;
+  color: white;
+  font-size: 18px;
+  font-weight: 800;
 `;
