d0afb3ffdafba0446a8f541fc766228d42bed97f
Jun <jun.juno@kakaostyle.com>
2022-09-01 02:24:51 +0900

    feat: 헤더 모바일 대응

--- components/common/Header/index.tsx
+++ components/common/Header/index.tsx
@@ -1,16 +1,14 @@
 import { FC, useState } from 'react';
 import styled from '@emotion/styled';
 import { colors } from '@/styles/colors';
-import IconLogoMobile from '@/public/icons/icon-logo-mobile.svg';
-import IconMenu from '@/public/icons/icon-menu.svg';
 import Button from '@/components/common/Button';
 import Link from 'next/link';
-import useScreenSize from '@/hooks/useScreenSize';
 import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
 import Menu from '@/components/common/Header/Menu';
 
+const HEADER_MOBILE_MEDIA_QUERY = '(max-width: 600px)';
+
 const Header: FC = () => {
-  const { isMobile } = useScreenSize();
   const [isMenuOpened, setIsMenuOpened] = useState<boolean>(false);
 
   const onToggle = () => {
@@ -20,28 +18,23 @@ const Header: FC = () => {
   return (
     <>
       <StyledHeader>
-        <StyledLeftSection>
-          <Link href='/' passHref>
-            {!isMobile ? <StyledIconLogo alt='logo' src='/icons/logo.png' /> : <IconLogoMobile />}
-          </Link>
-        </StyledLeftSection>
-        <StyledRightSection>
-          {!isMobile && (
-            <Link passHref href='/project/upload'>
-              <a>
-                <StyledUploadButton variant='primary' size='fill'>
-                  + 내 프로젝트 올리기
-                </StyledUploadButton>
-              </a>
-            </Link>
-          )}
-          <Link passHref href='/auth/login'>
-            <a>
-              <StyledLoginButton size='fill'>로그인</StyledLoginButton>
-            </a>
-          </Link>
-          <StyledIconMenu onClick={onToggle} />
-        </StyledRightSection>
+        <Link href='/' passHref>
+          <StyledIconLogo alt='logo' src='/icons/icon-logo.svg' />
+        </Link>
+        <Empty />
+        <Link passHref href='/project/upload'>
+          <a>
+            <StyledUploadButton variant='primary' size='fill'>
+              + 내 프로젝트 올리기
+            </StyledUploadButton>
+          </a>
+        </Link>
+        <Link passHref href='/auth/login'>
+          <a>
+            <StyledLoginButton size='fill'>로그인</StyledLoginButton>
+          </a>
+        </Link>
+        <StyledIconMenu alt='menu' src='/icons/icon-menu.svg' onClick={onToggle} />
       </StyledHeader>
       {isMenuOpened && <Menu onToggle={onToggle} />}
     </>
@@ -53,24 +46,34 @@ export default Header;
 const StyledHeader = styled.header`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  margin: 0 auto;
   background-color: ${colors.black100};
   padding: 24px 48px;
+  width: 1060px;
   height: 92px;
 
+  @media (max-width: 1060px) {
+    width: 100%;
+  }
+
   @media ${MOBILE_MEDIA_QUERY} {
+    justify-content: center;
+    margin: 0;
+    margin-top: 40px;
     padding: 14px 20px;
     height: 56px;
   }
 `;
 
-const StyledLeftSection = styled.section`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+const Empty = styled.div`
+  flex: 1;
+  @media ${MOBILE_MEDIA_QUERY} {
+    display: none;
+  }
 `;
 
 const StyledIconLogo = styled.img`
+  justify-self: center;
   cursor: pointer;
   width: 125px;
   height: 41px;
@@ -81,23 +84,36 @@ const StyledIconLogo = styled.img`
   }
 `;
 
-const StyledRightSection = styled.section`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
 const StyledUploadButton = styled(Button)`
   padding: 11px 21px;
+
+  @media ${HEADER_MOBILE_MEDIA_QUERY} {
+    display: none;
+  }
 `;
 
 const StyledLoginButton = styled(Button)`
   margin: 0 0 0 10px;
   border: 1px solid #534d64;
   padding: 11px 21px;
+  font-weight: 700;
+
+  @media ${HEADER_MOBILE_MEDIA_QUERY} {
+    display: none;
+  }
 `;
 
-const StyledIconMenu = styled(IconMenu)`
-  margin-left: 36px;
+const StyledIconMenu = styled.img`
+  margin-left: 44px;
   cursor: pointer;
+  width: 24px;
+  height: 24px;
+
+  @media ${MOBILE_MEDIA_QUERY} {
+    position: absolute;
+    top: 63px;
+    right: 30px;
+    width: 16px;
+    height: 16px;
+  }
 `;
