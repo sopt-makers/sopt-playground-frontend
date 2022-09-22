5f06db8bab0dbc6c4db3b40814f38d711bc06276
jynam <njy1007@gmail.com>
2022-09-22 14:59:08 +0900

    chore: 토스트 + 회원가입 링크 스토리북

---
+++ components/projects/upload/Toast/ToastWithSignupLink.stories.tsx
@@ -0,0 +1,26 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import ProjectToast from '@/components/projects/upload/Toast';
+import SignupLink from '@/components/projects/upload/SignupLink';
+import { RecoilRoot } from 'recoil';
+
+const ToastWithSignupLink = ({ duration }: { duration?: number }) => {
+  return (
+    <RecoilRoot>
+      <ProjectToast duration={duration} />
+      <SignupLink />
+    </RecoilRoot>
+  );
+};
+
+export default {
+  component: ToastWithSignupLink,
+} as ComponentMeta<typeof ToastWithSignupLink>;
+
+const Template: ComponentStory<typeof ToastWithSignupLink> = (args) => <ToastWithSignupLink {...args} />;
+
+export const Default = Template.bind({});
+Default.storyName = '기본';
+
+export const Duration2000 = Template.bind({});
+Default.args = { duration: 2000 };
+Default.storyName = '기본';
