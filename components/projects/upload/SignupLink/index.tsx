ff875e12237be539c859606376bf9975641593bc
jynam <njy1007@gmail.com>
2022-09-25 19:26:49 +0900

    refactor: 파라미터 구조 변경

--- components/projects/upload/SignupLink/index.tsx
+++ components/projects/upload/SignupLink/index.tsx
@@ -9,11 +9,10 @@ import { ToastContext } from '@/components/projects/upload/ToastProvider/context
 const SignupLink: FC = () => {
   const { showToast } = useContext(ToastContext);
   const onCopy = () =>
-    copyToClipboard(
-      '회원가입 링크',
-      () => showToast('링크가 클립보드에 저장되었습니다'),
-      () => showToast('다시 시도해주세요'),
-    );
+    copyToClipboard('회원가입 링크', {
+      onSuccess: () => showToast('링크가 클립보드에 저장되었습니다'),
+      onError: () => showToast('다시 시도해주세요'),
+    });
 
   return <StyledContainer onClick={onCopy}>회원가입 링크 복사</StyledContainer>;
 };
--- utils/index.ts
+++ utils/index.ts
@@ -1,9 +1,9 @@
-export const copyToClipboard = async (text: string, onSuccess?: () => void, onError?: () => void) => {
+export const copyToClipboard = async (text: string, options?: { onSuccess?: () => void; onError?: () => void }) => {
   try {
     if (!text) throw '빈 문자열입니다';
     await navigator.clipboard.writeText(text);
-    onSuccess?.();
+    options?.onSuccess?.();
   } catch (error) {
-    onError?.();
+    options?.onError?.();
   }
 };
