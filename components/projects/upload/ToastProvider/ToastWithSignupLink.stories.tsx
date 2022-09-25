3ad26416b17b73c623c46e8aa03a5015e8086b06
jynam <njy1007@gmail.com>
2022-09-25 19:33:47 +0900

    fix: 바뀐 토스트 구현 내용에 맞추어 스토리북 수정

---
+++ components/projects/upload/ToastProvider/ToastWithSignupLink.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import SignupLink from '@/components/projects/upload/SignupLink';
+import ToastProvider from '@/components/projects/upload/ToastProvider';
+
+const ToastWithSignupLink = () => {
+  return (
+    <ToastProvider>
+      <SignupLink />
+    </ToastProvider>
+  );
+};
+
+export default {
+  component: ToastWithSignupLink,
+} as ComponentMeta<typeof ToastWithSignupLink>;
+
+const Template: ComponentStory<typeof ToastWithSignupLink> = () => <ToastWithSignupLink />;
+
+export const Default = Template.bind({});
+Default.storyName = '기본';
