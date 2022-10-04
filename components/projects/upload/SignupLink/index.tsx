4544d4c46d948c479c5cf770912135775f67cad4
jynam <njy1007@gmail.com>
2022-10-04 23:08:02 +0900

    fix: 복사될 링크 환경 변수 활용하여 수정

--- components/projects/upload/SignupLink/index.tsx
+++ components/projects/upload/SignupLink/index.tsx
@@ -10,7 +10,7 @@ import { copyToClipboard } from '@/utils';
 const SignupLink: FC = () => {
   const { showToast } = useContext(ToastContext);
   const onCopy = () =>
-    copyToClipboard('https://sopt-project.pages.dev/auth/verify', {
+    copyToClipboard(`${process.env.NEXT_PUBLIC_ORIGIN}/auth/verify`, {
       onSuccess: () => showToast('링크가 클립보드에 저장되었습니다'),
       onError: () => showToast('다시 시도해주세요'),
     });
