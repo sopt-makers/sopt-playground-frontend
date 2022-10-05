cb164603a779f05ccbb3be01e7bf94d1a52647d3
jun <jun.juno@kakaostyle.com>
2022-10-05 22:29:26 +0900

    chore: 로그인 테스트용 코드 주입

---
+++ constants/Config.ts
@@ -0,0 +1,5 @@
+const isLocal = !!process.env.NEXT_PUBLIC_LOCAL;
+
+export default {
+  isLocal,
+};
--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -6,16 +6,18 @@ import FacebookButton from '@/components/auth/identityProvider/facebook/Facebook
 import useFacebookAuth from '@/components/auth/identityProvider/useFacebookAuth';
 import SquareLink from '@/components/common/SquareLink';
 import { textStyles } from '@/styles/typography';
+import useLocalLogin from '@/hooks/useLocalLogin';
 
 const LoginPage: FC = () => {
   const facebookAuth = useFacebookAuth();
+  const { isLocal, onClickLocalLogin } = useLocalLogin();
 
   return (
     <StyledLoginPage>
       <LoginTitle>SOPT Internal에 오신걸 환영합니다</LoginTitle>
       <LoginDescription>SOPT회원만 이용할 수 있어요.</LoginDescription>
       <LinkContainer>
-        <FacebookButton onClick={facebookAuth.login}>페이스북으로 로그인</FacebookButton>
+        <FacebookButton onClick={isLocal ? onClickLocalLogin : facebookAuth.login}>페이스북으로 로그인</FacebookButton>
         <Link href='/auth/verify' passHref>
           <SquareLink>회원가입</SquareLink>
         </Link>
