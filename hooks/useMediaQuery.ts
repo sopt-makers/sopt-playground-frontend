802c860eb73507b7e80e85060be281afe4aec5b3
Jun <jun.juno@kakaostyle.com>
2022-10-06 15:18:14 +0900

    feat: useMediaQuery 훅 추가

---
+++ hooks/useMediaQuery.ts
@@ -0,0 +1,29 @@
+import { useCallback, useEffect, useState } from 'react';
+
+const useMediaQuery = (width: number) => {
+  const [targetReached, setTargetReached] = useState(false);
+
+  const updateTarget = useCallback((e: MediaQueryListEvent) => {
+    if (e.matches) {
+      setTargetReached(true);
+    } else {
+      setTargetReached(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    const media = window.matchMedia(`(max-width: ${width}px)`);
+    media.addEventListener('change', updateTarget);
+
+    // Check on mount (callback is not called until a change occurs)
+    if (media.matches) {
+      setTargetReached(true);
+    }
+
+    return () => media.removeEventListener('change', updateTarget);
+  }, []);
+
+  return targetReached;
+};
+
+export default useMediaQuery;
