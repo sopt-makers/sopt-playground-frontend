90c9a9d5fb34e8d2221b96c3d6597d07db62010b
lee jeongmin <ljm991108@gmail.com>
2022-10-10 18:13:18 +0900

    feat: 데스크탑 뷰 헤더 수정

--- components/common/Header/index.tsx
+++ components/common/Header/index.tsx
@@ -1,28 +1,52 @@
 import styled from '@emotion/styled';
 import Link from 'next/link';
-import { FC } from 'react';
+import LogoIcon from 'public/icons/icon-logo.svg';
+import MemberIcon from 'public/icons/icon-member-32.svg';
+import { FC, useState } from 'react';
 
 import useAuth from '@/components/auth/useAuth';
 
 const Header: FC = () => {
   const { logout } = useAuth();
+  const [isUserDropdownOpened, setIsUserDropdownOpened] = useState(false);
+  const currentPath = window.location.pathname;
 
   return (
     <StyledHeader>
-      <Link href='/' passHref>
-        <TextLinkButton>SOPT</TextLinkButton>
-      </Link>
-      <Link href='/' passHref>
-        <TextLinkButton>멤버</TextLinkButton>
-      </Link>
-      <Link href='/projects' passHref>
-        <TextLinkButton>프로젝트</TextLinkButton>
-      </Link>
-      <Spacer />
-      <Link href='/projects/upload' passHref>
-        <TextLinkButton>내 프로젝트 올리기</TextLinkButton>
-      </Link>
-      <TextLinkButton onClick={logout}>로그아웃</TextLinkButton>
+      <HeaderGroup>
+        <Link href='/' passHref>
+          <TextLinkButton isCurrentPath={currentPath === '/'}>
+            <StyledLogo>
+              <LogoIcon />
+            </StyledLogo>
+          </TextLinkButton>
+        </Link>
+        <Link href='/projects' passHref>
+          <TextLinkButton isCurrentPath={currentPath === '/projects'}>프로젝트</TextLinkButton>
+        </Link>
+        <Link href='' passHref>
+          <TextLinkButton isCurrentPath={currentPath === ''}>Web Product</TextLinkButton>
+        </Link>
+      </HeaderGroup>
+      <HeaderGroup>
+        <Link href='/projects/upload' passHref>
+          <UploadButton>
+            <span>+</span>내 프로젝트 올리기
+          </UploadButton>
+        </Link>
+
+        <UserButton onClick={() => setIsUserDropdownOpened((e) => !e)}>
+          <MemberIcon />
+          <span>세글자</span>
+        </UserButton>
+      </HeaderGroup>
+
+      {isUserDropdownOpened && (
+        <UserDropdown>
+          <div>내 프로필</div>
+          <div onClick={logout}>로그아웃</div>
+        </UserDropdown>
+      )}
     </StyledHeader>
   );
 };
@@ -31,16 +55,86 @@ export default Header;
 
 const StyledHeader = styled.header`
   display: flex;
+  position: relative;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
+  padding: 18px 36px;
+  line-height: 100%;
+  letter-spacing: -0.01em;
+  font-size: 14px;
 `;
 
-const TextLinkButton = styled.a`
+const HeaderGroup = styled.div`
+  display: flex;
+  align-items: center;
+
+  &:first-child {
+    gap: 16px;
+  }
+
+  &:nth-child(2) {
+    gap: 8px;
+  }
+`;
+
+const StyledLogo = styled.div`
+  margin-right: 40px;
+  padding: 10px;
+`;
+
+const TextLinkButton = styled.a<{ isCurrentPath: boolean }>`
   cursor: pointer;
-  padding: 5px 10px;
+  color: ${({ isCurrentPath }) => (isCurrentPath ? '#fff' : '#C0C5C9')};
+  font-weight: ${({ isCurrentPath }) => (isCurrentPath ? 700 : 500)};
 `;
 
-const Spacer = styled.span`
-  display: block;
-  flex-grow: 1;
+const UploadButton = styled.a`
+  box-sizing: border-box;
+  border-radius: 32px;
+  background-color: #8040ff;
+  cursor: pointer;
+  padding: 12px 20px;
+  height: 38px;
+  font-weight: 700;
+
+  & > span {
+    margin-right: 4px;
+    font-weight: 700;
+  }
+`;
+
+const UserButton = styled.a`
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 19px;
+  background: #1c1d1e;
+  cursor: pointer;
+  padding: 3px 12px 3px 4px;
+  height: 38px;
+  font-weight: 700;
+
+  & > span {
+    width: 64px;
+    text-align: center;
+  }
+`;
+
+const UserDropdown = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  position: absolute;
+  top: 80px;
+  right: 36px;
+  flex-direction: column;
+  gap: 25px;
+  border-radius: 14px;
+  background: #272828;
+  padding: 25px 20px;
+  width: 176px;
+
+  & > div {
+    cursor: pointer;
+  }
 `;
