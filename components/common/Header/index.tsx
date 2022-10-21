d4616c2fb307e7016cadb759c82ed9ecbfa8f9f9
lee jeongmin <ljm991108@gmail.com>
2022-10-21 16:51:10 +0900

    feat: 헤더 모바일 뷰 추가

--- components/common/Header/index.tsx
+++ components/common/Header/index.tsx
@@ -1,45 +1,58 @@
 import styled from '@emotion/styled';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import LogoIcon from 'public/icons/icon-logo.svg';
-import MemberIcon from 'public/icons/icon-member-32.svg';
+import MemberIcon from 'public/icons/icon-member.svg';
+import MenuIcon from 'public/icons/icon-menu.svg';
 import { FC, useState } from 'react';
 
 import useAuth from '@/components/auth/useAuth';
+import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
 
 const Header: FC = () => {
   const { logout } = useAuth();
   const [isUserDropdownOpened, setIsUserDropdownOpened] = useState(false);
-  const currentPath = window.location.pathname;
+
+  const router = useRouter();
 
   return (
     <StyledHeader>
-      <HeaderGroup>
+      <LeftGroup>
+        <div className='mobile-only'>
+          <MenuIcon />
+        </div>
         <Link href='/' passHref>
-          <TextLinkButton isCurrentPath={currentPath === '/'}>
+          <TextLinkButton isCurrentPath={router.pathname === '/'}>
             <StyledLogo>
               <LogoIcon />
             </StyledLogo>
           </TextLinkButton>
         </Link>
-        <Link href='/projects' passHref>
-          <TextLinkButton isCurrentPath={currentPath === '/projects'}>프로젝트</TextLinkButton>
-        </Link>
-        <Link href='' passHref>
-          <TextLinkButton isCurrentPath={currentPath === ''}>Web Product</TextLinkButton>
-        </Link>
-      </HeaderGroup>
-      <HeaderGroup>
-        <Link href='/projects/upload' passHref>
-          <UploadButton>
-            <span>+</span>내 프로젝트 올리기
-          </UploadButton>
-        </Link>
+
+        <MenuGroup className='pc-only'>
+          <Link href='/projects' passHref>
+            <TextLinkButton isCurrentPath={router.pathname === '/projects'}>프로젝트</TextLinkButton>
+          </Link>
+          {/* <Link href='/web-product' passHref>
+          <TextLinkButton isCurrentPath={router.pathname === ''}>Web Product</TextLinkButton>
+        </Link> */}
+        </MenuGroup>
+      </LeftGroup>
+
+      <RightGroup>
+        <div className='pc-only'>
+          <Link href='/projects/upload' passHref>
+            <UploadButton>
+              <span>+</span>내 프로젝트 올리기
+            </UploadButton>
+          </Link>
+        </div>
 
         <UserButton onClick={() => setIsUserDropdownOpened((e) => !e)}>
           <MemberIcon />
           <span>세글자</span>
         </UserButton>
-      </HeaderGroup>
+      </RightGroup>
 
       {isUserDropdownOpened && (
         <UserDropdown>
@@ -62,24 +75,41 @@ const StyledHeader = styled.header`
   line-height: 100%;
   letter-spacing: -0.01em;
   font-size: 14px;
+
+  @media ${MOBILE_MEDIA_QUERY} {
+    padding: 12px;
+    height: 56px;
+    font-size: 12px;
+  }
 `;
 
-const HeaderGroup = styled.div`
+const LeftGroup = styled.div`
   display: flex;
   align-items: center;
+  font-size: 18px;
+  font-weight: 500;
+`;
 
-  &:first-child {
-    gap: 16px;
-  }
+const MenuGroup = styled.div`
+  display: flex;
+  gap: 16px;
+`;
 
-  &:nth-child(2) {
-    gap: 8px;
-  }
+const RightGroup = styled.div`
+  display: flex;
+  gap: 8px;
+  align-items: center;
 `;
 
 const StyledLogo = styled.div`
   margin-right: 40px;
   padding: 10px;
+  @media ${MOBILE_MEDIA_QUERY} {
+    svg {
+      width: 49px;
+      height: auto;
+    }
+  }
 `;
 
 const TextLinkButton = styled.a<{ isCurrentPath: boolean }>`
@@ -115,9 +145,28 @@ const UserButton = styled.a`
   height: 38px;
   font-weight: 700;
 
+  svg {
+    width: 32px;
+    height: auto;
+  }
+
   & > span {
     width: 64px;
     text-align: center;
+  }
+
+  @media ${MOBILE_MEDIA_QUERY} {
+    padding: 3px 10px 3px 3px;
+    height: 28px;
+
+    svg {
+      width: 22px;
+    }
+
+    & > span {
+      width: 42px;
+      text-align: center;
+    }
   }
 `;
 
@@ -133,8 +182,18 @@ const UserDropdown = styled.div`
   background: #272828;
   padding: 25px 20px;
   width: 176px;
+  font-size: 16px;
 
   & > div {
     cursor: pointer;
+  }
+
+  @media ${MOBILE_MEDIA_QUERY} {
+    top: 56px;
+    right: 20px;
+    gap: 20px;
+    padding: 22px 20px;
+    width: 144px;
+    font-size: 15px;
   }
 `;
