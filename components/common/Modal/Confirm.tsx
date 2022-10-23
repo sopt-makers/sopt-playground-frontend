2d969befdd449d10d599b3ed9c3b5f27f5cfad56
Jun <jun.juno@kakaostyle.com>
2022-10-23 17:33:25 +0900

    chore(c): modal 작업 중

---
+++ components/common/Modal/Confirm.tsx
@@ -0,0 +1,84 @@
+import styled from '@emotion/styled';
+import { FC, PropsWithChildren, Suspense, useState } from 'react';
+import { createRoot, Root } from 'react-dom/client';
+
+import Button from '@/components/common/Button';
+import Modal from '@/components/common/Modal';
+
+interface ConfirmModalProps {
+  title?: string;
+  cancelText?: string;
+  okText?: string;
+  afterClose?: () => void;
+}
+const ConfirmModal: FC<PropsWithChildren<ConfirmModalProps>> = ({
+  title = '',
+  okText,
+  cancelText,
+  afterClose,
+  children,
+  ...props
+}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const onClose = () => {
+    afterClose?.();
+    setIsOpen(false);
+  };
+
+  return (
+    <Modal title={title} isOpen={isOpen} onClose={onClose} {...props}>
+      {children}
+      <StyledModalFooter>
+        <Button onClick={onClose}>{okText ?? '취소'}</Button>
+        <Button variant='primary'>{cancelText ?? '완료'}</Button>
+      </StyledModalFooter>
+    </Modal>
+  );
+};
+
+const StyledModalFooter = styled.footer`
+  display: flex;
+  gap: 14px;
+  align-items: center;
+  justify-content: center;
+  margin-top: 42px;
+`;
+
+export default ConfirmModal;
+
+function createConfirmElement(props: ConfirmModalProps) {
+  const divTarget = document.createElement('div');
+  divTarget.id = 'pastel-alert';
+  document.body.appendChild(divTarget);
+  const root = createRoot(divTarget);
+  root.render(
+    <Suspense fallback={null}>
+      <ConfirmModal
+        {...props}
+        afterClose={() => {
+          removeConfirmElement(divTarget, root);
+          props.afterClose?.();
+        }}
+      />
+    </Suspense>,
+  );
+}
+
+function removeConfirmElement(target: HTMLElement, root: Root) {
+  if (target && target.parentNode) {
+    root.unmount();
+    target.parentNode.removeChild(target);
+  }
+}
+
+export function Confirm(props: ConfirmModalProps): Promise<void> {
+  return new Promise((resolve) => {
+    createConfirmElement({
+      ...props,
+      afterClose: () => {
+        resolve();
+        props.afterClose?.();
+      },
+    });
+  });
+}
