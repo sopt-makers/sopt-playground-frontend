a9164ee74615ae127c065917545d5895804ce39e
Coby <appdler96@gmail.com>
2022-11-05 09:50:19 +0900

    chore: 프로젝트가 없을 때 터지지 않도록 처리 & 텍스트 추가

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -7,7 +7,9 @@ import Header from '@/components/common/Header';
 import Text from '@/components/common/Text';
 import ProjectCard from '@/components/projects/main/ProjectCard';
 import useGetProjectListQuery from '@/components/projects/upload/hooks/useGetProjectListQuery';
+import { colors } from '@/styles/colors';
 import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
+import { textStyles } from '@/styles/typography';
 import { setLayout } from '@/utils/layout';
 
 const ProjectPage: FC = () => {
@@ -18,23 +20,27 @@ const ProjectPage: FC = () => {
     <AuthRequired>
       <StyledContainer>
         <StyledContent>
-          <StyledLength typography='SUIT_22_B'>{data?.projects.length} Projects</StyledLength>
-          <StyledGridContainer>
-            {data?.projects.map((project) => (
-              <ProjectCard
-                key={project.id}
-                category={project.category}
-                summary={project.summary}
-                generation={project.generation}
-                links={project.links}
-                logoImage={project.logo_image}
-                name={project.name}
-                serviceType={project.service_type}
-                thumbnailImage={project.thumbnail_image}
-                onClick={() => router.push(`/projects/detail?projectId=${project.id}`)}
-              />
-            ))}
-          </StyledGridContainer>
+          <StyledLength typography='SUIT_22_B'>{data?.projects?.length ?? 0} Projects</StyledLength>
+          {data?.projects == null ? (
+            <StyledNoData>현재 등록된 프로젝트가 없습니다.</StyledNoData>
+          ) : (
+            <StyledGridContainer>
+              {data?.projects.map((project) => (
+                <ProjectCard
+                  key={project.id}
+                  category={project.category}
+                  summary={project.summary}
+                  generation={project.generation}
+                  links={project.links}
+                  logoImage={project.logo_image}
+                  name={project.name}
+                  serviceType={project.service_type}
+                  thumbnailImage={project.thumbnail_image}
+                  onClick={() => router.push(`/projects/detail?projectId=${project.id}`)}
+                />
+              ))}
+            </StyledGridContainer>
+          )}
         </StyledContent>
       </StyledContainer>
     </AuthRequired>
@@ -96,5 +102,14 @@ const StyledGridContainer = styled.div`
     justify-content: start;
     column-gap: 0;
     row-gap: 24px;
+  }
+`;
+
+const StyledNoData = styled.div`
+  margin-top: 120px;
+  color: ${colors.gray60};
+  ${textStyles.SUIT_16_M}
+  @media ${MOBILE_MEDIA_QUERY} {
+    ${textStyles.SUIT_14_M}
   }
 `;
