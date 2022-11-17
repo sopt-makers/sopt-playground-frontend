01d1079ae53f1d850ba4faff10bb47feb565f16a
Tekiter <tekiter8@gmail.com>
2022-11-17 17:13:12 +0900

    feat: Footer 추가

---
+++ components/common/Footer/index.tsx
@@ -0,0 +1,44 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import Link from 'next/link';
+import { FC } from 'react';
+
+import { colors } from '@/styles/colors';
+
+interface FooterProps {
+  className?: string;
+}
+
+const Footer: FC<FooterProps> = ({}) => {
+  return (
+    <StyledFooter>
+      <Link href='/makers' passHref>
+        <FooterLink highlight>만든 사람들</FooterLink>
+      </Link>
+      <FooterLink>의견 제안하기</FooterLink>
+    </StyledFooter>
+  );
+};
+
+export default Footer;
+
+const StyledFooter = styled.div`
+  display: flex;
+  border-top: 1px solid ${colors.black40};
+  background-color: ${colors.black80};
+  padding: 0 0 0 38px;
+  height: 48px;
+`;
+
+const FooterLink = styled.a<{ highlight?: boolean }>`
+  padding: 17px 10px;
+
+  ${(props) =>
+    props.highlight
+      ? css`
+          color: ${colors.white100};
+        `
+      : css`
+          color: ${colors.gray40};
+        `}
+`;
