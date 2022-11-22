ce4bea38e89f67ff91bb10c6c156e2583d7e893a
Jun <jun.juno@kakaostyle.com>
2022-11-22 20:59:16 +0900

    chore: deploy to heroku

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,18 +1,13 @@
 module.exports = {
   ci: {
     collect: {
-      url: [
-        // 'http://localhost:3000/members',
-        // 'http://localhost:3000/members/upload',
-        // 'http://localhost:3000/members/detail',
-        'http://localhost:3000/projects',
-        'http://localhost:3000/projects/upload',
-        'http://localhost:3000/projects/detail',
-      ],
+      url: ['http://localhost:3000/projects', 'http://localhost:3000/projects/upload'],
       startServerCommand: 'npm run start',
     },
     upload: {
-      target: 'temporary-public-storage',
+      target: 'lhci',
+      serverBaseUrl: 'https://shrouded-atoll-56755.herokuapp.com/app/projects',
+      token: '689daa0b-ed2e-4de1-aeb5-be14c09e0c7d',
     },
   },
 };
