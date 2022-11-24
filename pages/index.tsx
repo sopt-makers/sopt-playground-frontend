ed4cc6505096158b3048fe166a8b301f4d4378f4
Tekiter <tekiter8@gmail.com>
2022-11-24 15:20:35 +0900

    fix: 이동 코드를 명시적으로 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
-import AuthRequired from '@/components/auth/AuthRequired';
 import useAuth from '@/components/auth/useAuth';
 import Header from '@/components/common/Header';
 import { setLayout } from '@/utils/layout';
@@ -14,10 +13,12 @@ const Home: NextPage = () => {
   useEffect(() => {
     if (isLoggedIn) {
       router.replace('/members');
+    } else {
+      router.replace('/auth/login');
     }
   }, [isLoggedIn, router]);
 
-  return <AuthRequired>{}</AuthRequired>;
+  return null;
 };
 
 setLayout(Home, (page) => {
