dcc8aaeccf9b5d94c18030edfaa341f637ee7738
Tekiter <tekiter8@gmail.com>
2022-11-24 15:19:16 +0900

    fix: member에 라우트가 갇히는 문제 해결

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,22 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 import AuthRequired from '@/components/auth/AuthRequired';
+import useAuth from '@/components/auth/useAuth';
 import Header from '@/components/common/Header';
 import { setLayout } from '@/utils/layout';
 
 const Home: NextPage = () => {
+  const { isLoggedIn } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      router.replace('/members');
+    }
+  }, [isLoggedIn, router]);
+
   return <AuthRequired>{}</AuthRequired>;
 };
 
