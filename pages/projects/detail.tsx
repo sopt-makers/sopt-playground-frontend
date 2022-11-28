864d775e43beea75bba50afe71393c40ebfabaad
Tekiter <tekiter8@gmail.com>
2022-11-28 18:54:15 +0900

    feat: deprecated 경고 추가

--- pages/projects/detail.tsx
+++ pages/projects/detail.tsx
@@ -4,6 +4,7 @@ import { FC, useEffect } from 'react';
 import SiteHeader from '@/components/common/Header';
 import { setLayout } from '@/utils/layout';
 
+/** @deprecated 기존 URL 링크 호환용 리다이렉트 페이지 */
 const ProjectDetailPage: FC = () => {
   const router = useRouter();
 
