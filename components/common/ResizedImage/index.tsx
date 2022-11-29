2577cf66931abe76d83b9e24947c7d354aaa5de2
Tekiter <tekiter8@gmail.com>
2022-11-29 23:20:23 +0900

    feat: 캐시 없는 리사이징 이미지 스킵 추가

--- components/common/ResizedImage/index.tsx
+++ components/common/ResizedImage/index.tsx
@@ -1,4 +1,6 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
+
+const WAIT_TIME = 3000;
 
 const getResizedImage = (src: string, width: number) => {
   return `https://wsrv.nl/?url=${encodeURIComponent(src)}&w=${width}&output=webp`;
@@ -15,15 +17,38 @@ interface ImageProps {
 }
 
 const ResizedImage: FC<ImageProps> = ({ className, src, width, alt, onLoad }) => {
-  const [failed, setFailed] = useState(false);
+  const [isUsingOriginal, setIsUsingOriginal] = useState(false);
 
-  const handleLoadError = () => {
-    setFailed(true);
+  const timeoutTokenRef = useRef<ReturnType<typeof setTimeout>>();
+
+  const cancelReplacementTimer = () => {
+    if (timeoutTokenRef.current !== undefined) {
+      clearTimeout(timeoutTokenRef.current);
+    }
   };
+
+  const handleResizedLoadError = () => {
+    setIsUsingOriginal(true);
+  };
+
+  const handleResizedLoaded = () => {
+    cancelReplacementTimer();
+    onLoad?.();
+  };
+
+  useEffect(() => {
+    timeoutTokenRef.current = setTimeout(() => {
+      setIsUsingOriginal(true);
+    }, WAIT_TIME);
+
+    return () => {
+      cancelReplacementTimer();
+    };
+  }, []);
 
   return (
     <>
-      {failed ? (
+      {isUsingOriginal ? (
         <img src={src} alt={alt} className={className} onLoad={onLoad} loading='lazy' decoding='async' />
       ) : (
         <img
@@ -33,8 +58,8 @@ const ResizedImage: FC<ImageProps> = ({ className, src, width, alt, onLoad }) =>
           className={className}
           loading='lazy'
           decoding='async'
-          onLoad={onLoad}
-          onError={handleLoadError}
+          onLoad={handleResizedLoaded}
+          onError={handleResizedLoadError}
         />
       )}
     </>
