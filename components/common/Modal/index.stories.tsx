ed9469cd3f9544c6df2d2ae5a3adc40f7585c6d9
Jun <jun.juno@kakaostyle.com>
2022-12-01 09:33:43 +0900

    feat: 컨펌 트리거 모달 제작 중

--- components/common/Modal/index.stories.tsx
+++ components/common/Modal/index.stories.tsx
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import Button from '@/components/common/Button';
 
 import Modal from '.';
-import ConfirmModal, { Confirm } from './Confirm';
 
 export default {
   component: Modal,
@@ -21,18 +20,3 @@ export const Default = () => {
   );
 };
 Default.storyName = '기본';
-
-export const 컨펌 = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const onConfirm = async () => {
-    const data = await Confirm({});
-    console.log('data:', data);
-  };
-
-  return (
-    <>
-      <Button onClick={onConfirm}>클릭하여 모달 열기</Button>
-      <ConfirmModal title='컨펌 모달 타이틀' />
-    </>
-  );
-};
