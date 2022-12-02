714dd46a8dfd6fa40aabdd249ce8cc81f5d1f938
jynam <njy1007@gmail.com>
2022-12-02 23:01:33 +0900

    fix: gtag 빠진 스크립트 추가

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          `,
+          }}
+        />
+      </body>
+    </Html>
+  );
+}
