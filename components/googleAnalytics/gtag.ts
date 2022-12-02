39039fe9027f27030d898e5528f51763f200d532
jynam <njy1007@gmail.com>
2022-12-02 17:09:16 +0900

    fix: window 객체 타입 체크 추가

--- components/googleAnalytics/gtag.ts
+++ components/googleAnalytics/gtag.ts
@@ -1,7 +1,12 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const gtag = (window as any).gtag;
+const gtag =
+  typeof window !== 'undefined'
+    ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (window as any).gtag
+    : () => {
+        // do nothing
+      };
 
 export const pageview = (url: string) => {
   gtag('config', GA_TRACKING_ID, {
