d3c3113c1f196796ab574de5da68fc344ebd939c
Jun <jun.juno@kakaostyle.com>
2022-12-04 21:15:53 +0900

    feat: Confirm 모달 promise로 구현

--- components/common/Modal/index.stories.tsx
+++ components/common/Modal/index.stories.tsx
@@ -1,9 +1,9 @@
 import { Meta } from '@storybook/react';
-import { useState } from 'react';
 
 import Button from '@/components/common/Button';
 import { Alert } from '@/components/common/Modal/Alert';
 import { Confirm } from '@/components/common/Modal/Confirm';
+import useModalState from '@/components/common/Modal/useModalState';
 
 import Modal from '.';
 
@@ -12,22 +12,25 @@ export default {
 } as Meta;
 
 export const Default = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { isOpen, onClose, onOpen } = useModalState();
 
   return (
     <>
-      <Button onClick={() => setIsOpen(true)}>클릭하여 모달 열기</Button>
-      <Modal title='모달 예시' isOpen={isOpen} onClose={() => setIsOpen(false)} />
+      <Button onClick={onOpen}>클릭하여 모달 열기</Button>
+      <Modal title='모달 예시' isOpen={isOpen} onClose={onClose} />
     </>
   );
 };
 Default.storyName = '기본';
 
 export const ConfirmModal = () => {
-  const onConfirm = () => {
-    Confirm({
+  const onConfirm = async () => {
+    const result = await Confirm({
       title: '컨펌 모달',
     });
+    if (result) {
+      alert('confirm!');
+    }
   };
 
   return (
