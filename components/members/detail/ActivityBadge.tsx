2fc7418c85bc05e6f4a72101e1a96d41f51014af
lee jeongmin <ljm991108@gmail.com>
2022-12-13 14:06:38 +0900

    feat: 상세 내 기수 정보의 프로젝트 뱃지에 카테고리 표시

---
+++ components/members/detail/ActivityBadge.tsx
@@ -0,0 +1,57 @@
+import styled from '@emotion/styled';
+import { FC, useEffect, useState } from 'react';
+
+import { Activity } from '@/api/members/type';
+import { getProjectById } from '@/api/projects';
+import { categoryLabel } from '@/components/projects/upload/constants';
+import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
+import { textStyles } from '@/styles/typography';
+
+const ActivityBadge: FC<Activity> = (activity) => {
+  const [category, setCategory] = useState('');
+  const getProjectCategory = async () => {
+    if (activity.isProject) {
+      const project = await getProjectById(activity.id.toString());
+      setCategory(categoryLabel[project.category]);
+    }
+  };
+
+  useEffect(() => {
+    getProjectCategory();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log('activity', activity);
+  return (
+    <Container key={activity.id}>
+      {category && <Category>{category}</Category>}
+      {activity.team}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  border-radius: 13px;
+  background: #8040ff;
+  padding: 6px 14px;
+  line-height: 100%;
+  letter-spacing: -0.01em;
+  font-size: 14px;
+  font-weight: 500;
+  ${textStyles.SUIT_14_M}
+  @media ${MOBILE_MEDIA_QUERY} {
+    margin: 0;
+    width: fit-content;
+    white-space: nowrap;
+  }
+`;
+
+const Category = styled.span`
+  ${textStyles.SUIT_14_B}
+
+  margin-right: 4px;
+`;
+
+export default ActivityBadge;
