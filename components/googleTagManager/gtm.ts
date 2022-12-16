360794abacd1521e50581497dea676426e60f9ea
jynam <njy1007@gmail.com>
2022-12-17 04:34:59 +0900

    refactor: window 객체가 없을 때의 예외 상황 더욱 명확히 처리

--- components/googleTagManager/gtm.ts
+++ components/googleTagManager/gtm.ts
@@ -2,11 +2,10 @@ const dataLayer =
   typeof window !== 'undefined'
     ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
       (window as any).dataLayer
-    : () => {
-        // do nothing
-      };
+    : undefined;
 
 export const pageview = (url: string) => {
+  if (!dataLayer) throw new Error('pageview push 실패');
   dataLayer.push({
     event: 'pageview',
     page: url,
