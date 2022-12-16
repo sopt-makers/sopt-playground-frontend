37986844fd1b6ba5d47d61221cf10c5350775da4
Tekiter <tekiter8@gmail.com>
2022-12-17 00:40:25 +0900

    feat: 페이지 이동 로딩바 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,24 @@
+import ProgressBar from '@badrap/bar-of-progress';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Router from 'next/router';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
 
 import Debugger from '@/components/debug/Debugger';
 import GoogleTagManagerScript from '@/components/googleTagManager/Script';
+import { colors } from '@/styles/colors';
 import GlobalStyle from '@/styles/GlobalStyle';
 import { getLayout } from '@/utils/layout';
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { cacheTime: 300000, refetchOnWindowFocus: false, staleTime: 300000, retry: 1 } },
 });
+
+const progress = new ProgressBar({ color: colors.purple80, size: 3 });
+Router.events.on('routeChangeStart', () => progress.start());
+Router.events.on('routeChangeComplete', () => progress.finish());
+Router.events.on('routeChangeError', () => progress.finish());
 
 function MyApp({ Component, pageProps }: AppProps) {
   const layout = getLayout(Component);
