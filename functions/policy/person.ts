452308742e4929ea395d21da4d7d2d942f728859
Tekiter <tekiter8@gmail.com>
2022-12-26 22:32:58 +0900

    feat: 미사용 페이지 제거

--- functions/policy/person.ts
+++
@@ -1,7 +0,0 @@
-const PERSON_POLICY_HREF = 'https://parangjy.notion.site/5ade645578b44a2ab7bbb04d478d24b4';
-
-export const onRequest: PagesFunction = async (context) => {
-  const { request } = context;
-
-  return fetch(PERSON_POLICY_HREF, request);
-};
