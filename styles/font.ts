fd61d37a3fb934820892f54bb12e8e4d3b6cd570
Tekiter <tekiter8@gmail.com>
2022-12-28 04:36:33 +0900

    feat: 폰트 로드 swap 정책 추가

--- styles/font.ts
+++ styles/font.ts
@@ -5,21 +5,25 @@ const font = css`
     font-family: SUIT;
     src: url('/fonts/SUIT-ExtraBold.ttf') format('truetype');
     font-weight: 800;
+    font-display: swap;
   }
   @font-face {
     font-family: SUIT;
     src: url('/fonts/SUIT-Bold.ttf') format('truetype');
     font-weight: 700;
+    font-display: swap;
   }
   @font-face {
     font-family: SUIT;
     src: url('/fonts/SUIT-SemiBold.ttf') format('truetype');
     font-weight: 600;
+    font-display: swap;
   }
   @font-face {
     font-family: SUIT;
     src: url('/fonts/SUIT-Medium.ttf') format('truetype');
     font-weight: 500;
+    font-display: swap;
   }
 `;
 
