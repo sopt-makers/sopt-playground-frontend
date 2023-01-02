ccc27705167ff8083376c8c63e1c3147a7a0560d
Jun <jun.juno@kakaostyle.com>
2023-01-02 15:38:09 +0900

    fix: queryKey limit 추가

--- apiHooks/members.ts
+++ apiHooks/members.ts
@@ -17,7 +17,7 @@ interface Variables {
 // 멤버 프로필 전체 조회
 export const useGetMemberProfile = ({ limit }: Variables) => {
   return useInfiniteQuery({
-    queryKey: ['getMemberProfile', window.location.search],
+    queryKey: ['getMemberProfile', window.location.search, limit],
     queryFn: async ({ pageParam: cursor = 0 }) => {
       const params = { limit, cursor };
 
