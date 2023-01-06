8f795a34a32edbbdca2b31516158f8b85f4ba41a
Jun <jun.juno@kakaostyle.com>
2023-01-06 19:23:19 +0900

    chore: font

--- styles/font.ts
+++ styles/font.ts
@@ -3,17 +3,17 @@ import localFont from '@next/font/local';
 export const myFont = localFont({
   src: [
     {
-      path: '../public/fonts/SUIT-ExtraBold.woff2',
+      path: '../public/fonts/SUIT-Medium.woff2',
       weight: '500',
       style: 'Medium',
     },
     {
-      path: '../public/fonts/SUIT-ExtraBold.woff2',
+      path: '../public/fonts/SUIT-SemiBold.woff2',
       weight: '600',
       style: 'SemiBold',
     },
     {
-      path: '../public/fonts/SUIT-ExtraBold.woff2',
+      path: '../public/fonts/SUIT-Bold.woff2',
       weight: '700',
       style: 'Bold',
     },
