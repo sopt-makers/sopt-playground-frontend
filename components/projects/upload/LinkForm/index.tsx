08d9acd419fc7c655d1fc179fbd750f4d2b7dc28
jynam <njy1007@gmail.com>
2023-01-11 04:38:00 +0900

    feat: 프로젝트 업로드 폼 모바일 키보드 UX 개선

--- components/projects/upload/LinkForm/index.tsx
+++ components/projects/upload/LinkForm/index.tsx
@@ -132,6 +132,7 @@ const LinkForm: FC = () => {
                                 onChange('https://' + value);
                               }
                             }}
+                            type='url'
                           />
                         </FormItem>
                       )}
