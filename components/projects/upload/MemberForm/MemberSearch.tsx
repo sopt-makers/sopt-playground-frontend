b5febdced55e7434ba99b825bc577029423c0a1f
Jun <jun.juno@kakaostyle.com>
2023-01-10 14:36:52 +0900

    feat: 멤버 검색 ux 개선 추가

--- components/projects/upload/MemberForm/MemberSearch.tsx
+++ components/projects/upload/MemberForm/MemberSearch.tsx
@@ -21,16 +21,39 @@ interface MemberSearchProps {
 }
 
 const MemberSearch: FC<MemberSearchProps> = ({ value, onChange, onSearch, members, name, error }) => {
+  const onClear = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+    e.stopPropagation();
+    onChange(undefined);
+  };
+
   return (
     <StyledContainer error={error}>
       <Combobox value={value} onChange={onChange}>
         <Combobox.Input
           name={name}
           className='search'
-          displayValue={(member: Member) => member?.name}
           onChange={onSearch}
-          placeholder='SOPT 멤버 검색'
+          placeholder={!value ? 'SOPT 멤버 검색' : ''}
+          readOnly={!!value}
         />
+        {value && (
+          <Combobox.Label className='label'>
+            <div style={{ display: 'flex', alignItems: 'center', columnGap: '6px' }}>
+              <ProfileImage
+                style={{ width: '24px', height: '24px' }}
+                src={value.profileImage ?? '/icons/icon-member-search-default.svg'}
+                alt='멤버의 프로필 이미지'
+              />
+              <Text>{value.name}</Text>
+            </div>
+            <ClearIcon
+              className='.icon-clear'
+              onClick={onClear}
+              src='/icons/icon-member-search-clear.svg'
+              alt='검색된 멤버 제거 아이콘'
+            />
+          </Combobox.Label>
+        )}
         {members.length > 0 && (
           <Combobox.Options className='options'>
             {members.map((member) => (
@@ -54,11 +77,31 @@ const MemberSearch: FC<MemberSearchProps> = ({ value, onChange, onSearch, member
 
 export default MemberSearch;
 
-const StyledContainer = styled.div<{ error?: booelan }>`
+const StyledContainer = styled.div<{ error?: booelan; isSelected: boolean }>`
   display: flex;
+  position: relative;
   flex-direction: column;
 
   ${textStyles.SUIT_14_M};
+
+  & > .label {
+    display: flex;
+    position: absolute;
+    align-items: center;
+    justify-content: space-between;
+    column-gap: 6px;
+    z-index: 1;
+    padding: 12px 20px;
+    width: 100%;
+    ${textStyles.SUIT_16_SB};
+    ${colors.gray10};
+
+    &:hover {
+      img {
+        opacity: 1;
+      }
+    }
+  }
 
   & .search {
     transition: all 0.2s;
@@ -140,4 +183,12 @@ const MemberInfo = styled.div`
   display: flex;
   gap: 4px;
   align-items: center;
+`;
+
+const ClearIcon = styled.img`
+  transition: opacity 0.1s linear;
+  opacity: 0;
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
 `;
