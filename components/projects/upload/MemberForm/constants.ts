eaf442b8e85811bfc2133a514c78096364455e87
Jun <jun.juno@kakaostyle.com>
2023-01-11 13:01:44 +0900

    feat: 역할에 Team leader 추가, 라벨 변경

--- components/projects/upload/MemberForm/constants.ts
+++ components/projects/upload/MemberForm/constants.ts
@@ -1,5 +1,5 @@
 import { Member } from '@/api/members/type';
-import { ProjectMember } from '@/api/projects/type';
+import { MemberRole, ProjectMember } from '@/api/projects/type';
 
 export type MemeberFormType = ProjectMember & {
   isEdit?: boolean;
@@ -14,3 +14,37 @@ export const DEFAULT_MEMBER: Partial<MemeberFormType> = {
   memberGeneration: undefined,
   isEdit: true,
 };
+
+export const MemberRoleInfo: {
+  label: string;
+  value: MemberRole;
+}[] = [
+  {
+    label: 'Team Leader',
+    value: 'TEAMLEADER',
+  },
+  {
+    label: 'PM',
+    value: 'PM',
+  },
+  {
+    label: 'Design',
+    value: 'DESIGN',
+  },
+  {
+    label: 'iOS',
+    value: 'IOS',
+  },
+  {
+    label: 'Android',
+    value: 'ANDROID',
+  },
+  {
+    label: 'Web',
+    value: 'WEB',
+  },
+  {
+    label: 'Server',
+    value: 'SERVER',
+  },
+];
