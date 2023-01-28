98ca6614b559c6bdab7530d44a8630262aafdf3b
Tekiter <tekiter8@gmail.com>
2023-01-28 22:23:49 +0900

    refactor: 디버그 패널에서 radix로 변경

--- components/debug/Panel.tsx
+++ components/debug/Panel.tsx
@@ -1,6 +1,6 @@
 import { css } from '@emotion/react';
-import { Disclosure } from '@headlessui/react';
-import { FC, ReactNode } from 'react';
+import * as Collapsible from '@radix-ui/react-collapsible';
+import { FC, ReactNode, useState } from 'react';
 
 import { colors } from '@/styles/colors';
 
@@ -13,20 +13,16 @@ interface PanelProps {
 }
 
 const Panel: FC<PanelProps> = ({ title, children }) => {
+  const [open, setOpen] = useState(true);
+
   return (
-    <Disclosure defaultOpen>
-      {({ open }) => (
-        <>
-          <Disclosure.Button css={disclosureButtonStyle} as='div'>
-            <span>{title}</span>
-            <span>{open ? ARROW_UP_SYMBOL : ARROW_DOWN_SYMBOL}</span>
-          </Disclosure.Button>
-          <Disclosure.Panel css={disclosurePanelStyle}>
-            <>{children}</>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+    <Collapsible.Root open={open} onOpenChange={setOpen}>
+      <Collapsible.Trigger css={disclosureButtonStyle}>
+        <span>{title}</span>
+        <span>{open ? ARROW_UP_SYMBOL : ARROW_DOWN_SYMBOL}</span>
+      </Collapsible.Trigger>
+      <Collapsible.Content css={collapsibleContentStyle}>{children}</Collapsible.Content>
+    </Collapsible.Root>
   );
 };
 
@@ -38,6 +34,7 @@ const disclosureButtonStyle = css`
   background-color: ${colors.purple60};
   cursor: pointer;
   padding: 8px 16px;
+  width: calc(100% - 30px);
   color: ${colors.black60};
 
   &:hover {
@@ -45,9 +42,38 @@ const disclosureButtonStyle = css`
   }
 `;
 
-const disclosurePanelStyle = css`
+const collapsibleContentStyle = css`
   margin: 8px 15px;
   padding: 5px 0 0;
+  overflow: hidden;
+
+  &[data-state='open'] {
+    animation: slide-down 200ms ease-out;
+  }
+
+  &[data-state='closed'] {
+    animation: slide-up 200ms ease-out;
+  }
+
+  @keyframes slide-down {
+    from {
+      height: 0;
+    }
+
+    to {
+      height: var(--radix-collapsible-content-height);
+    }
+  }
+
+  @keyframes slide-up {
+    from {
+      height: var(--radix-collapsible-content-height);
+    }
+
+    to {
+      height: 0;
+    }
+  }
 `;
 
 export default Panel;
