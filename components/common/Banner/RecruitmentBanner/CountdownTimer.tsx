f14413f295d489d774044dfb17b246bee91c05b8
jynam <njy1007@gmail.com>
2023-01-29 02:11:28 +0900

    refactor: 가독성 개선 위해 값 변수에 담아주기

--- components/common/Banner/RecruitmentBanner/CountdownTimer.tsx
+++ components/common/Banner/RecruitmentBanner/CountdownTimer.tsx
@@ -8,13 +8,15 @@ interface CountdownTimerProps {
 export default function CountdownTimer({ deadlineDate, finish }: CountdownTimerProps) {
   const { days, hours, minutes, seconds } = useCountdown(deadlineDate, finish);
 
+  const isRunning = days + hours + minutes + seconds > 0;
+
   const formatTime = (time: number) => {
     return time.toString().padStart(2, '0');
   };
 
   return (
     <>
-      {days + hours + minutes + seconds > 0 && (
+      {isRunning && (
         <>
           <span>{`지원 마감까지 ${days}일 ${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`}</span>
           <span>{`>`}</span>
