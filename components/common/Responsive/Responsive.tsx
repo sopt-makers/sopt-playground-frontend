904cc1497528a27a278ef4a18c7d27a107aa9b4d
Tekiter <tekiter8@gmail.com>
2023-02-04 03:15:13 +0900

    feat: 다른 화면 컴포넌트 언마운트 추가

--- components/common/Responsive/Responsive.tsx
+++ components/common/Responsive/Responsive.tsx
@@ -1,11 +1,14 @@
-import { FC, ReactNode, useContext } from 'react';
+import { FC, ReactNode, startTransition, useContext, useEffect, useState } from 'react';
 
 import { ResponsiveContext } from '@/components/common/Responsive/context';
+import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
 
 interface ResponsiveProps {
   children: ReactNode;
-  only: 'mobile' | 'desktop';
+  only: AvailableSize;
 }
+
+type AvailableSize = 'mobile' | 'desktop';
 
 const Responsive: FC<ResponsiveProps> = ({ children, only }) => {
   const { mobileOnlyClassName, desktopOnlyClassName } = useContext(ResponsiveContext);
@@ -18,7 +21,29 @@ const Responsive: FC<ResponsiveProps> = ({ children, only }) => {
     }
   })();
 
-  return <div className={selectedClassName}>{children}</div>;
+  const [current, setCurrent] = useState<AvailableSize | null>(null);
+  useEffect(() => {
+    const mobileMedia = window.matchMedia(MOBILE_MEDIA_QUERY);
+
+    const handleChange = (e: MediaQueryListEvent) => {
+      setCurrent(e.matches ? 'mobile' : 'desktop');
+    };
+
+    mobileMedia.addEventListener('change', handleChange);
+
+    // 초기 렌더링후 비활성 컴포넌트 언마운트는 천천히 해도 되므로 startTransition 으로 렌더링 우선순위 낮추기
+    startTransition(() => {
+      if (mobileMedia.matches) {
+        setCurrent('mobile');
+      } else {
+        setCurrent('desktop');
+      }
+    });
+
+    return () => mobileMedia.removeEventListener('change', handleChange);
+  }, []);
+
+  return current === null || only === current ? <div className={selectedClassName}>{children}</div> : <></>;
 };
 
 export default Responsive;
