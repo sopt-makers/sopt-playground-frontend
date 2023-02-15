1fad8771de5c96954483db10dbc8852d1df4c4f2
Tekiter <tekiter8@gmail.com>
2023-02-15 15:56:19 +0900

    feat: 모집 배너 제거 (#443)
    * feat: 모집 배너 제거
    
    * feat: 3기 모집 배너 복구
    
    * feat: 모집 알림 버튼 주석처리

--- pages/members/index.tsx
+++ pages/members/index.tsx
@@ -1,14 +1,12 @@
 import { FC } from 'react';
 
 import AuthRequired from '@/components/auth/AuthRequired';
-import RecruitingBanner from '@/components/common/Banner/RecruitingBanner';
 import MemberList from '@/components/members/main/MemberList';
 import { setLayout } from '@/utils/layout';
 
 const UserPage: FC = () => {
   return (
     <AuthRequired>
-      <RecruitingBanner />
       <MemberList />
     </AuthRequired>
   );
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -2,14 +2,12 @@ import _uniqBy from 'lodash/uniqBy';
 import { FC } from 'react';
 
 import AuthRequired from '@/components/auth/AuthRequired';
-import RecruitingBanner from '@/components/common/Banner/RecruitingBanner';
 import ProjectList from '@/components/projects/main/ProjectList';
 import { setLayout } from '@/utils/layout';
 
 const ProjectPage: FC = () => {
   return (
     <AuthRequired>
-      <RecruitingBanner />
       <ProjectList />
     </AuthRequired>
   );
