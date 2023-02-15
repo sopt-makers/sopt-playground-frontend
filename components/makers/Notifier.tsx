2f4eaa3244cec546a387bd5b6f69f51377b9729b
Tekiter <tekiter8@gmail.com>
2023-02-15 15:40:27 +0900

    feat: 3기 모집 배너 복구

--- components/makers/Notifier.tsx
+++ components/makers/Notifier.tsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import { FC } from 'react';
 
-import { NOTIFY_2ND_GENERATION_URL } from '@/constants/links';
+import { NOTIFY_2ND_GENERATION_URL as RECRUIT_NOTIFY_GENERATION_URL } from '@/constants/links';
 import IconBell from '@/public/icons/icon-bell.svg';
 import IconOutgoing from '@/public/icons/icon-link-outgoing.svg';
 import { colors } from '@/styles/colors';
@@ -15,16 +15,16 @@ interface NotifierProps {
 const Notifier: FC<NotifierProps> = ({ className }) => {
   return (
     <StyledJoinNotifier className={className}>
-      <Title>현재 makers 1기 진행 중이에요. 2기에서 만나요!</Title>
-      <SubTitle>2기 모집은 2023년 1-2월 중에 진행될 예정이에요.</SubTitle>
+      <Title>현재 makers 2기 진행 중이에요. 3기에서 만나요!</Title>
+      <SubTitle>3기 모집은 2023년 7-8월 중에 진행될 예정이에요.</SubTitle>
       <ButtonGroup>
-        <SubscribeButton href={NOTIFY_2ND_GENERATION_URL} target='_blank'>
+        <SubscribeButton href={RECRUIT_NOTIFY_GENERATION_URL} target='_blank'>
           <StyledBellIcon />
-          2기 모집 알림 신청
+          3기 모집 알림 신청
         </SubscribeButton>
-        <ExpiredButton href='https://sopt-makers.oopy.io/6c1762c3-054b-4e72-bbf3-439dcac4edee' target='_blank'>
+        <ExpiredButton href='https://sopt-makers.oopy.io/' target='_blank'>
           <StyledOutgoingIcon />
-          1기 모집글 보기 (마감)
+          2기 모집글 보기 (마감)
         </ExpiredButton>
       </ButtonGroup>
     </StyledJoinNotifier>
