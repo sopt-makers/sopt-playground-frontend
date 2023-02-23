c07dd4520f9cdea437877dce832e96b77a64b3c3
Tekiter <tekiter8@gmail.com>
2023-02-23 21:55:34 +0900

    refactor: OAuthLoginCallback 분리

---
+++ components/auth/callback/OAuthLoginCallback.tsx
@@ -0,0 +1,73 @@
+import { FC, useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
+import { useSetRecoilState } from 'recoil';
+
+import LoginCallbackView from '@/components/auth/callback/LoginCallbackView';
+import { accessTokenAtom } from '@/components/auth/states/accessTokenAtom';
+
+interface OAuthLoginCallbackProps {
+  processParam: ProcessParamFn;
+  onSuccess: () => void;
+}
+
+export type ProcessParamFn = (
+  url: URL,
+) => Promise<{ success: true; accessToken: string } | { success: false; error: ErrorTypes }>;
+type ErrorTypes = 'invalidNonce' | 'invalidURL' | 'notMember' | 'unknown';
+
+const OAuthLoginCallback: FC<OAuthLoginCallbackProps> = ({ processParam, onSuccess }) => {
+  const setAccessToken = useSetRecoilState(accessTokenAtom);
+
+  const [url, setUrl] = useState<URL | null>(null);
+
+  useEffect(() => {
+    setUrl(new URL(self.location.href));
+  }, []);
+
+  const { data, status } = useQuery(
+    ['facebookLoginCallback'],
+    async () => {
+      if (url === null) {
+        throw new Error('Invalid state');
+      }
+      return processParam(url);
+    },
+    {
+      enabled: url !== null,
+      onSuccess(result) {
+        if (result.success) {
+          setAccessToken(result.accessToken);
+          onSuccess?.();
+        }
+      },
+    },
+  );
+
+  if (url === null || status === 'idle' || status === 'loading') {
+    return <LoginCallbackView mode={{ type: 'loading' }} />;
+  }
+
+  if (status === 'error') {
+    return <LoginCallbackView mode={{ type: 'error', errorMessage: '알 수 없는 오류입니다.' }} />;
+  }
+
+  if (!data.success) {
+    if (data.error === 'invalidNonce') {
+      return <LoginCallbackView mode={{ type: 'error', errorMessage: '잘못된 접근입니다. (INVALID_NONCE)' }} />;
+    } else if (data.error === 'notMember') {
+      return (
+        <LoginCallbackView
+          mode={{ type: 'error', errorMessage: 'SOPT.org 회원이 아닙니다.\n먼저 회원 가입후, 다시 로그인해주세요.' }}
+        />
+      );
+    } else if (data.error === 'invalidURL') {
+      return <LoginCallbackView mode={{ type: 'error', errorMessage: '잘못된 접근입니다. (INVALID_URL)' }} />;
+    } else {
+      return <LoginCallbackView mode={{ type: 'error', errorMessage: '알 수 없는 오류입니다.' }} />;
+    }
+  }
+
+  return <LoginCallbackView mode={{ type: 'loading' }} />;
+};
+
+export default OAuthLoginCallback;
--- pages/auth/callback/facebook/login.tsx
+++ pages/auth/callback/facebook/login.tsx
@@ -1,77 +1,34 @@
-import styled from '@emotion/styled';
 import { useRouter } from 'next/router';
 import { FC } from 'react';
-import { useQuery } from 'react-query';
-import { useSetRecoilState } from 'recoil';
 
-import LoginCallbackView from '@/components/auth/callback/LoginCallbackView';
+import OAuthLoginCallback, { ProcessParamFn } from '@/components/auth/callback/OAuthLoginCallback';
 import useFacebookAuth from '@/components/auth/identityProvider/useFacebookAuth';
-import { accessTokenAtom } from '@/components/auth/states/accessTokenAtom';
 import useLastUnauthorized from '@/components/auth/util/useLastUnauthorized';
-import useStringRouterQuery from '@/hooks/useStringRouterQuery';
 
 const FacebookLoginCallbackPage: FC = () => {
   const router = useRouter();
   const facebookAuth = useFacebookAuth();
   const lastUnauthorized = useLastUnauthorized();
-  const setAccessToken = useSetRecoilState(accessTokenAtom);
 
-  const { query: queryData, status: queryStatus } = useStringRouterQuery(['code', 'state'] as const);
+  const processParam: ProcessParamFn = async (url) => {
+    const code = url.searchParams.get('code');
+    const state = url.searchParams.get('state');
 
-  const { data, status } = useQuery(
-    ['facebookLoginCallback'],
-    async () => {
-      if (queryStatus !== 'success') {
-        throw new Error('Invalid state');
-      }
-      return facebookAuth.sendLoginRequest(queryData.code, queryData.state);
-    },
-    {
-      enabled: queryStatus === 'success',
-      onSuccess(result) {
-        if (result.success) {
-          setAccessToken(result.accessToken);
-          router.replace(lastUnauthorized.popPath() ?? '/');
-        }
-      },
-    },
-  );
-
-  if (queryStatus === 'error') {
-    return <LoginCallbackView mode={{ type: 'error', errorMessage: '잘못된 접근입니다.' }} />;
-  }
-
-  if (queryStatus === 'loading' || status === 'idle' || status === 'loading') {
-    return <LoginCallbackView mode={{ type: 'loading' }} />;
-  }
-
-  if (status === 'error') {
-    return <LoginCallbackView mode={{ type: 'error', errorMessage: '알 수 없는 오류입니다.' }} />;
-  }
-
-  if (!data.success) {
-    if (data.error === 'invalidNonce') {
-      return <LoginCallbackView mode={{ type: 'error', errorMessage: '잘못된 접근입니다. (INVALID_NONCE)' }} />;
-    } else if (data.error === 'notMember') {
-      return (
-        <LoginCallbackView
-          mode={{ type: 'error', errorMessage: 'SOPT.org 회원이 아닙니다.\n먼저 회원 가입후, 다시 로그인해주세요.' }}
-        />
-      );
-    } else {
-      return <LoginCallbackView mode={{ type: 'error', errorMessage: '알 수 없는 오류입니다.' }} />;
+    if (!code || !state) {
+      return {
+        success: false,
+        error: 'invalidURL',
+      };
     }
-  }
 
-  return <StyledFacebookLoginCallback>잠시 후 이동합니다..</StyledFacebookLoginCallback>;
+    return await facebookAuth.sendLoginRequest(code, state);
+  };
+
+  const handleSuccess = () => {
+    router.replace(lastUnauthorized.popPath() ?? '/');
+  };
+
+  return <OAuthLoginCallback processParam={processParam} onSuccess={handleSuccess} />;
 };
 
 export default FacebookLoginCallbackPage;
-
-const StyledFacebookLoginCallback = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 100px;
-`;
