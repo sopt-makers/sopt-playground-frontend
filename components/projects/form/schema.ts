8d2bcdd94566500b2e6e7866242c5e51ff20fe96
Jun <jun.juno@kakaostyle.com>
2023-02-27 00:03:20 +0900

    feat: 기수 스키마 추가

--- components/projects/form/schema.ts
+++ components/projects/form/schema.ts
@@ -4,6 +4,7 @@ export const dateStringSchema = z.string().regex(/^\d{4}.(0[1-9]|1[0-2])/g, '날
 
 export const uploadSchema = z.object({
   name: z.string().min(1, '프로젝트 이름을 입력해주세요.'),
+  generation: z.string().min(1, '기수를 입력해주세요.'),
   period: z.object({
     startAt: dateStringSchema,
     endAt: dateStringSchema.nullable(),
@@ -14,6 +15,7 @@ export type ProjectFormType = z.infer<typeof uploadSchema>;
 
 export const defaultUploadValues: ProjectFormType = {
   name: '',
+  generation: '',
   period: {
     startAt: '',
     endAt: '',
