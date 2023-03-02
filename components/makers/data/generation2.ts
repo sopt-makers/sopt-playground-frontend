b074621cb2455962d3411f2d3b884fc4bc39165d
Tekiter <tekiter8@gmail.com>
2023-03-03 02:23:05 +0900

    feat: 2기 정보 추가

---
+++ components/makers/data/generation2.ts
@@ -0,0 +1,105 @@
+import { MakersGeneration } from '@/components/makers/data/types';
+
+export const generation2: MakersGeneration = {
+  title: '2기',
+  teams: [
+    {
+      title: 'makers lead',
+      people: [{ type: 'member', id: 1, name: '이정연' }],
+    },
+    {
+      title: 'makers organizer',
+      description:
+        'makers의 지속 가능한 활동 운영을 포함해서 makers의 문화를 만들어가며, 메이커들이 오너십을 가지고 제품을 더 잘 만들어갈 수 있도록 고민하는 역할을 해요.',
+      people: [
+        { type: 'member', id: 44, name: '이채연' },
+        { type: 'member', id: 23, name: '이예서' },
+        { type: 'member', id: 186, name: '김서진' },
+        { type: 'member', id: 30, name: '김인우' },
+        { type: 'member', id: 3, name: '이준호' },
+        { type: 'member', id: 227, name: '한지우' },
+      ],
+    },
+    {
+      title: 'SOPT 공식 홈페이지 팀',
+      description: 'SOPT를 대내외적으로 알릴 수 있는 공식홈페이지를 개선하고 발전시켜요.',
+      link: 'https://sopt.org',
+      people: [
+        { type: 'member', id: 4, name: '박이정', position: 'PM' },
+        { type: 'member', id: 264, name: '안채량', position: '디자이너' },
+        { type: 'raw', name: '최영훈', position: '백엔드 개발자' },
+        { type: 'member', id: 51, name: '박서원', position: '백엔드 개발자' },
+        { type: 'member', id: 186, name: '김서진', position: '웹 프론트엔드 개발자' },
+        { type: 'member', id: 187, name: '이주함', position: '웹 프론트엔드 개발자' },
+      ],
+    },
+    {
+      title: '플레이그라운드 팀',
+      description:
+        'SOPT 구성원들을 위한 서비스, SOPT 플레이그라운드를 만들고 있어요. 앱잼, 솝커톤, 솝텀 등 멋진 프로젝트들이 잘 기록될 수 있도록 프로젝트 기능을 담당해요. 또한 이천여명의 SOPT 구성원들이 어떻게 하면 더 잘 소통하고 가치를 연결할 수 있을지 고민하는 팀이에요.',
+      people: [
+        { type: 'member', id: 128, name: '고수현', position: 'PM' },
+        { type: 'member', id: 1, name: '이정연', position: 'PM' },
+        { type: 'member', id: 227, name: '한지우', position: '디자이너' },
+        { type: 'member', id: 64, name: '이화정', position: '디자이너' },
+        { type: 'member', id: 7, name: '송정우', position: '백엔드 개발자' },
+        { type: 'member', id: 99, name: '한유진', position: '백엔드 개발자' },
+        { type: 'member', id: 8, name: '남주영', position: '웹 프론트엔드 개발자' },
+        { type: 'member', id: 3, name: '이준호', position: '웹 프론트엔드 개발자' },
+        { type: 'member', id: 2, name: '박건영', position: '웹 프론트엔드 개발자' },
+      ],
+    },
+    {
+      title: '모임 팀',
+      description: 'SOPT 플레이그라운드 내에서 구성원들이 하나로 모일 수 있는 모임 서비스를 만들고 있어요.',
+      people: [
+        { type: 'member', id: 10, name: '주수한', position: 'PM' },
+        { type: 'member', id: 30, name: '김인우', position: '디자이너' },
+        { type: 'raw', name: '손은서', position: '디자이너' },
+        { type: 'raw', name: '강영우', position: '백엔드 개발자' },
+        { type: 'member', id: 13, name: '김은수', position: '웹 프론트엔드 개발자' },
+        { type: 'member', id: 36, name: '이재훈', position: '웹 프론트엔드 개발자' },
+        { type: 'member', id: 28, name: '백지연', position: '웹 프론트엔드 개발자' },
+      ],
+    },
+    {
+      title: 'SOPT APP 팀',
+      description:
+        'SOPT 공식 앱을 만드는 팀이에요. SOPT를 더욱 재미있고, 알차게 활동할 수 있도록 도와주는 서비스를 만들고 있어요.',
+      people: [
+        { type: 'member', id: 5, name: '김나연', position: 'PM' },
+        { type: 'member', id: 66, name: '손혜정', position: '디자이너' },
+        { type: 'member', id: 260, name: '주어진', position: '백엔드 개발자' },
+        { type: 'member', id: 21, name: '이현우', position: 'Android 개발자' },
+        { type: 'raw', name: '최민정', position: 'Android 개발자' },
+        { type: 'member', id: 45, name: '이준호', position: 'iOS 개발자' },
+        { type: 'member', id: 35, name: '이세진', position: 'iOS 개발자' },
+        { type: 'member', id: 72, name: '이승호', position: 'iOS 개발자' },
+      ],
+    },
+    {
+      title: '운영 서비스 팀',
+      description: '',
+      people: [
+        { type: 'member', id: 265, name: '김유빈', position: 'PM' },
+        { type: 'member', id: 74, name: '배민주', position: '디자이너' },
+        { type: 'member', id: 251, name: '권소희', position: '웹 프론트엔드 개발자' },
+        { type: 'member', id: 115, name: '김형겸', position: '웹 프론트엔드 개발자' },
+        { type: 'raw', name: '김영인', position: 'iOS 개발자' },
+        { type: 'member', id: 22, name: '윤수빈', position: 'iOS 개발자' },
+        { type: 'raw', name: '정화진', position: 'Android 개발자' },
+        { type: 'member', id: 258, name: '한승현', position: 'Android 개발자' },
+        { type: 'member', id: 112, name: '김소현', position: '서버 개발자' },
+        { type: 'member', id: 205, name: '이용택', position: '서버 개발자' },
+      ],
+    },
+    {
+      title: '브랜딩',
+      description: '',
+      people: [
+        { type: 'raw', name: '김민영', position: '디자이너' },
+        { type: 'member', id: 238, name: '이서영', position: '디자이너' },
+      ],
+    },
+  ],
+};
