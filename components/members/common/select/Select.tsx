83d9c9fa93c86ae9a67185bbc861772edd248569
Jun <jun.juno@kakaostyle.com>
2023-03-13 08:54:05 +0900

    feat: 스타일 수정, 스크롤바 추가

--- components/members/common/select/Select.tsx
+++ components/members/common/select/Select.tsx
@@ -1,5 +1,6 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import * as ScrollArea from '@radix-ui/react-scroll-area';
 import * as Select from '@radix-ui/react-select';
 import { FC, PropsWithChildren } from 'react';
 
@@ -34,11 +35,23 @@ const SelectRoot: FC<PropsWithChildren<SelectProps>> = ({
         </Select.Icon>
       </StyledTrigger>
       <Select.Portal>
-        <StyledContent>
-          <Select.ScrollUpButton />
-          <Select.Viewport>{children}</Select.Viewport>
-          <Select.ScrollDownButton />
-        </StyledContent>
+        <ScrollArea.Root type='always'>
+          <StyledContent position='popper' width={width}>
+            <StyledScrollUpButton>
+              <IconSelectArrow width={18} height={18} alt='select-arrow-up' />
+            </StyledScrollUpButton>
+            <Select.Viewport asChild>
+              <ScrollArea.Viewport>{children}</ScrollArea.Viewport>
+            </Select.Viewport>
+            <StyledScrollDownButton>
+              <IconSelectArrow width={18} height={18} alt='select-arrow-down' />
+            </StyledScrollDownButton>
+
+            <StyledScrollbar orientation='vertical'>
+              <StyledThumb />
+            </StyledScrollbar>
+          </StyledContent>
+        </ScrollArea.Root>
       </Select.Portal>
     </Select.Root>
   );
@@ -71,10 +84,56 @@ const StyledTrigger = styled(Select.Trigger)<Pick<SelectProps, 'width' | 'error'
   }
 `;
 
-const StyledContent = styled(Select.Content)`
+const StyledContent = styled(Select.Content)<Pick<SelectProps, 'width'>>`
+  margin-top: 4px;
   border-radius: 12px;
   background: ${colors.black60};
   padding: 8px;
+  max-height: 262px;
+  overflow: scroll;
+
+  ${({ width }) => width && buildCSSWithLength('width', width)}
+`;
+
+const StyledScrollUpButton = styled(Select.ScrollUpButton)`
+  display: flex;
+  justify-content: center;
+  transform: rotate(180deg);
+`;
+const StyledScrollDownButton = styled(Select.ScrollDownButton)`
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledScrollbar = styled(ScrollArea.Scrollbar)`
+  display: flex;
+  user-select: none;
+  touch-action: none;
+  padding: 8px 0;
+
+  &[data-orientation='vertical'] {
+    width: 4px;
+  }
+`;
+
+const StyledThumb = styled(ScrollArea.Thumb)`
+  position: relative;
+  right: 8px;
+  flex: 1;
+  border-radius: 4px;
+  background-color: ${colors.gray80};
+
+  &::before {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate('-50%', '-50%');
+    width: 100%;
+    min-width: 44;
+    height: 100%;
+    min-height: 56px;
+    content: '';
+  }
 `;
 
 interface SelectItemProps {
