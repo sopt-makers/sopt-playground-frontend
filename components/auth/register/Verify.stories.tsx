d8b76d7d255ebb994e5cf9cc0c6449119183e80e
Tekiter <tekiter8@gmail.com>
2023-03-15 18:12:06 +0900

    feat: 탭 추가

--- components/auth/register/Verify.stories.tsx
+++ components/auth/register/Verify.stories.tsx
@@ -6,10 +6,17 @@ export default {
   component: Verify,
   parameters: {},
   decorators: [],
+  argTypes: {
+    byPhone: { control: false },
+    byEmail: { control: false },
+  },
 } as ComponentMeta<typeof Verify>;
 
 const Template: ComponentStory<typeof Verify> = (args) => <Verify {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  byPhone: <div style={{ backgroundColor: '#a9d7b4', height: '100px' }}>byPhone</div>,
+  byEmail: <div style={{ backgroundColor: '#a0abdd', height: '100px' }}>byEmail</div>,
+};
 Default.storyName = '기본';
--- components/auth/register/Verify.tsx
+++ components/auth/register/Verify.tsx
@@ -1,25 +1,47 @@
 import styled from '@emotion/styled';
-import { FC } from 'react';
+import * as Tabs from '@radix-ui/react-tabs';
+import { AnimatePresence, m } from 'framer-motion';
+import { FC, forwardRef, ReactNode } from 'react';
 
 import Stepper from '@/components/auth/register/Stepper';
+import { colors } from '@/styles/colors';
 import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
 import { textStyles } from '@/styles/typography';
 
-interface VerifyByPhoneProps {}
+interface VerifyByPhoneProps {
+  byPhone: ReactNode;
+  byEmail: ReactNode;
+}
 
-const VerifyByPhone: FC<VerifyByPhoneProps> = ({}) => {
+const Verify: FC<VerifyByPhoneProps> = ({ byPhone, byEmail }) => {
   return (
-    <StyledVerifyByPhone>
+    <StyledVerify>
       <Stepper step={1} />
-
       <Title>SOPT 회원인증</Title>
-    </StyledVerifyByPhone>
+      <Description>
+        Playground는 SOPT 회원만을 위한 공간이에요.
+        <br />
+        SOPT 회원임을 인증하기 위해 전화번호를 입력해 주세요.
+      </Description>
+      <TabsRoot defaultValue='byPhone'>
+        <TabsList>
+          <TabsTrigger value='byPhone'>전화번호 인증</TabsTrigger>
+          <TabsTrigger value='byEmail'>이메일 인증</TabsTrigger>
+        </TabsList>
+        <TabsContent key='byPhone' value='byPhone'>
+          {byPhone}
+        </TabsContent>
+        <TabsContent key='byEmail' value='byEmail'>
+          {byEmail}
+        </TabsContent>
+      </TabsRoot>
+    </StyledVerify>
   );
 };
 
-export default VerifyByPhone;
+export default Verify;
 
-const StyledVerifyByPhone = styled.div`
+const StyledVerify = styled.div`
   display: flex;
   flex-direction: column;
   width: 420px;
@@ -27,6 +49,7 @@ const StyledVerifyByPhone = styled.div`
 
 const Title = styled.h2`
   margin-top: 95px;
+  text-align: center;
 
   ${textStyles.SUIT_32_SB}
 
@@ -35,4 +58,46 @@ const Title = styled.h2`
 
     ${textStyles.SUIT_24_B}
   }
+`;
+
+const Description = styled.p`
+  margin-top: 12px;
+  text-align: center;
+  color: ${colors.gray60};
+
+  ${textStyles.SUIT_16_M}
+
+  @media ${MOBILE_MEDIA_QUERY} {
+    margin-top: 20px;
+
+    ${textStyles.SUIT_12_M}
+  }
+`;
+
+const TabsRoot = styled(Tabs.Root)`
+  margin-top: 48px;
+`;
+
+const TabsList = styled(Tabs.List)`
+  display: flex;
+  gap: 12px;
+`;
+
+const TabsTrigger = styled(Tabs.Trigger)`
+  flex-grow: 1;
+  transition: 0.2s background-color, 0.2s color;
+  border-radius: 10px;
+  background-color: ${colors.black80};
+  cursor: pointer;
+  height: 48px;
+  color: ${colors.gray30};
+
+  &[data-state='active'] {
+    background-color: ${colors.purpledim100};
+    color: ${colors.purple40};
+  }
+`;
+
+const TabsContent = styled(Tabs.Content)`
+  margin-top: 48px;
 `;
