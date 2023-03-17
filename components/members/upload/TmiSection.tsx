be23e060112f6525523f2a18a29a857a7f08a76f
jynam <njy1007@gmail.com>
2023-03-18 03:52:12 +0900

    feat: 섹션 뼈대 만들기

---
+++ components/members/upload/TmiSection.tsx
@@ -0,0 +1,10 @@
+import MemberFormHeader from '@/components/members/upload/forms/FormHeader';
+import { MemberFormSection } from '@/components/members/upload/forms/FormSection';
+
+export default function TmiSection() {
+  return (
+    <MemberFormSection>
+      <MemberFormHeader title='SOPT만 아는 나의 TMI' />
+    </MemberFormSection>
+  );
+}
