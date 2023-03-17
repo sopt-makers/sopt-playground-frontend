4c77e5df02baddc9b82c1f4cd36f93a1843b8672
jynam <njy1007@gmail.com>
2023-03-18 02:14:25 +0900

    chore: Reat Query v3 -> v4 쿼리 키 array 타입 고정 이슈 대응

--- components/common/Example/index.tsx
+++ components/common/Example/index.tsx
@@ -5,7 +5,7 @@ export const API_PATH = 'https://dog.ceo/api/breeds/image/random';
 const fetch = axios.get(API_PATH);
 
 const Example = () => {
-  const { data, isLoading } = useQuery('getDog', () => fetch);
+  const { data, isLoading } = useQuery(['getDog'], () => fetch);
   if (isLoading) {
     return <p>loading...</p>;
   }
--- pages/projects/upload/index.tsx
+++ pages/projects/upload/index.tsx
@@ -126,14 +126,14 @@ const ProjectUploadPage: FC = () => {
       if (isEditPage && postId) {
         await putProject({ id: Number(postId), data: input });
         router.push(playgroundLink.projectList());
-        queryClient.invalidateQueries('getProjectListQuery');
-        queryClient.invalidateQueries('getProjectQuery');
+        queryClient.invalidateQueries(['getProjectListQuery']);
+        queryClient.invalidateQueries(['getProjectQuery']);
       } else if (!isEditPage && !postId) {
         mutate(input, {
           onSuccess: () => {
             toast.show({ message: '프로젝트가 성공적으로 업로드 되었습니다.' });
             router.push(playgroundLink.projectList());
-            queryClient.invalidateQueries('getProjectListQuery');
+            queryClient.invalidateQueries(['getProjectListQuery']);
           },
         });
       }
