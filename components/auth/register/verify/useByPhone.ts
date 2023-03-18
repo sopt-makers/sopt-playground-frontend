93a4b4ee153e2aae781536e5c926c2d9d9515bb9
Tekiter <tekiter8@gmail.com>
2023-03-18 23:49:35 +0900

    feat: 에러 처리 추가

--- components/auth/register/verify/useByPhone.ts
+++ components/auth/register/verify/useByPhone.ts
@@ -1,8 +1,29 @@
 import { to } from 'await-to-js';
+import axios, { AxiosError } from 'axios';
 import { useRef, useState } from 'react';
 
 import { postSMSCode, postSMSToken } from '@/api/auth';
 import { ByPhoneStates } from '@/components/auth/register/verify/view/ByPhoneView';
+
+interface ErrorResponse {
+  success: boolean;
+  code: string;
+  message: string;
+}
+
+const phoneErrorMap: Record<string, string> = {
+  emptySoptUser:
+    '솝트 활동 시 사용한 전화번호가 아니예요.\n인증을 실패하신 경우 하단에서 다른 방법으로 인증할 수 있어요.',
+  shouldRetry: '일시적인 오류에요. 다시 시도해주세요.',
+  alreadyTaken: '이미 가입된 전화번호에요.',
+  unknownError: '알 수 없는 오류가 발생했어요.',
+};
+
+const codeErrorMap: Record<string, string> = {
+  wrongCode: '인증번호가 일치하지 않아요. 번호를 확인한 후 다시 입력해 주세요.',
+  notExistedCode: '인증번호가 일치하지 않아요. 번호를 확인한 후 다시 입력해 주세요.',
+  expiredCode: '인증번호가 만료되었어요. 인증번호를 다시 받아주세요.',
+};
 
 const useByPhone = (onSuccess?: (registerToken: string) => void) => {
   const [state, setState] = useState<ByPhoneStates>({ type: 'phoneReady' });
@@ -16,9 +37,28 @@ const useByPhone = (onSuccess?: (registerToken: string) => void) => {
     const [err, _] = await to(postSMSCode({ phone: phone.replace(/-/g, '') }));
 
     if (err) {
+      if (axios.isAxiosError(err)) {
+        const message = (() => {
+          const e = err as AxiosError<ErrorResponse>;
+          const code = e.response?.data.code;
+
+          if (code && code in phoneErrorMap) {
+            return phoneErrorMap[code];
+          }
+          return '알 수 없는 오류에요.';
+        })();
+
+        setState({
+          type: 'phoneError',
+          message,
+        });
+
+        return;
+      }
+
       setState({
         type: 'phoneError',
-        message: 'ERROR',
+        message: '알 수 없는 오류에요.',
       });
       return;
     }
@@ -49,9 +89,28 @@ const useByPhone = (onSuccess?: (registerToken: string) => void) => {
     const [err, result] = await to(postSMSToken({ code }));
 
     if (err) {
+      if (axios.isAxiosError(err)) {
+        const message = (() => {
+          const e = err as AxiosError<ErrorResponse>;
+          const code = e.response?.data.code;
+
+          if (code && code in codeErrorMap) {
+            return codeErrorMap[code];
+          }
+          return '알 수 없는 오류에요.';
+        })();
+
+        setState({
+          type: 'codeError',
+          message,
+        });
+
+        return;
+      }
+
       setState({
         type: 'codeError',
-        message: 'ERROR',
+        message: '알 수 없는 오류에요.',
       });
       return;
     }
