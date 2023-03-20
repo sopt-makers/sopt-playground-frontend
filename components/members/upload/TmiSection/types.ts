7bfa1d260eec3716f26ff58561bffe40f1cab671
jynam <njy1007@gmail.com>
2023-03-21 03:42:33 +0900

    chore: 쓸모 없는 주석 삭제

--- components/members/upload/TmiSection/types.ts
+++ components/members/upload/TmiSection/types.ts
@@ -1,5 +1,3 @@
-// export type MbtiIndicator = 'E' | 'I' | 'N' | 'S' | 'F' | 'T' | 'J' | 'P';
-
 import { MBTI_INDEX_LIST } from '@/components/members/upload/TmiSection/constants';
 
 export type MbtiIndicatorPosition = 'left' | 'right';
@@ -10,7 +8,5 @@ export type Mbti = [
   MbtiIndicatorPosition | null,
   MbtiIndicatorPosition | null,
 ];
-
-// export type MbtiIndicatorSet = Record<MbtiIndicatorPosition, string>;
 
 export type MbtiIndex = typeof MBTI_INDEX_LIST[number];
