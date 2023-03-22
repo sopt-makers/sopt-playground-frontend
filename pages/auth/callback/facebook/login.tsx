8fa5fd449b52621cce563c1fa2c979f112a35e60
Tekiter <tekiter8@gmail.com>
2023-03-22 16:41:14 +0900

    feat: 마지막 로그인 메시지 추가

--- pages/auth/callback/facebook/login.tsx
+++ pages/auth/callback/facebook/login.tsx
@@ -1,13 +1,16 @@
 import { useRouter } from 'next/router';
 import { FC } from 'react';
+import { useSetRecoilState } from 'recoil';
 
 import OAuthLoginCallback, { ProcessParamFn } from '@/components/auth/callback/OAuthLoginCallback';
 import useFacebookAuth from '@/components/auth/identityProvider/facebook/useFacebookAuth';
+import { lastLoginMethodAtom } from '@/components/auth/states/lastLoginMethodAtom';
 import useLastUnauthorized from '@/components/auth/util/useLastUnauthorized';
 
 const FacebookLoginCallbackPage: FC = () => {
   const router = useRouter();
   const facebookAuth = useFacebookAuth();
+  const setLastLoginMethod = useSetRecoilState(lastLoginMethodAtom);
   const lastUnauthorized = useLastUnauthorized();
 
   const processParam: ProcessParamFn = async (url) => {
@@ -25,6 +28,7 @@ const FacebookLoginCallbackPage: FC = () => {
   };
 
   const handleSuccess = () => {
+    setLastLoginMethod('facebook');
     router.replace(lastUnauthorized.popPath() ?? '/');
   };
 
