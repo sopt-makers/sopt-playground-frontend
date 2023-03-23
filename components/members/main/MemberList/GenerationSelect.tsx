bcd2fde9bc2a42d6e23b72885888a00f8c0b47ac
Jun <jun.juno@kakaostyle.com>
2023-03-23 12:23:14 +0900

    feat: icon fill currentColor로 변경

--- components/members/main/MemberList/GenerationSelect.tsx
+++ components/members/main/MemberList/GenerationSelect.tsx
@@ -1,17 +1,27 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import React, { FC } from 'react';
 
 import Select from '@/components/members/common/select/Select';
 import { GENERATIONS } from '@/constants/generation';
-import SearchClearIcon from '@/public/icons/icon-search-clear.svg';
+import { colors } from '@/styles/colors';
 
 interface GenerationSelectProps {
   value?: string;
   onChange?: (value?: string) => void;
+  onClear?: () => void;
 }
-const GenerationSelect: FC<GenerationSelectProps> = ({ value, onChange }) => {
+const GenerationSelect: FC<GenerationSelectProps> = ({ value, onChange, onClear }) => {
   return (
-    <StyledSelect placeholder='기수' value={value} onChange={onChange}>
+    <StyledSelect
+      placeholder='기수'
+      value={value}
+      onChange={onChange}
+      width={100}
+      selected={!!value}
+      allowClear
+      onClear={onClear}
+    >
       {GENERATIONS.map((generation) => (
         <Select.Item key={generation} value={generation}>{`${generation}기`}</Select.Item>
       ))}
@@ -21,7 +31,16 @@ const GenerationSelect: FC<GenerationSelectProps> = ({ value, onChange }) => {
 
 export default GenerationSelect;
 
-const StyledSelect = styled(Select)`
+const StyledSelect = styled(Select)<{ selected: boolean }>`
+  transition: background-color 0.2s;
   border-radius: 20px;
-  width: 100px;
+  ${({ selected }) =>
+    selected &&
+    css`
+      background-color: ${colors.purple100};
+      color: ${colors.white};
+    `}
+
+  padding: 9px 20px;
+  max-height: 40px;
 `;
