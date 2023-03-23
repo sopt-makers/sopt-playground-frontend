13a211215f34ad6891053aeef42aa49ebb955e9b
jynam <njy1007@gmail.com>
2023-03-24 00:01:24 +0900

    fix: 프로필 업로드 안되는 버그 fix

--- components/members/upload/schema.ts
+++ components/members/upload/schema.ts
@@ -109,13 +109,4 @@ export const memberFormSchema = yup.object().shape({
       }),
     )
     .nullable(),
-  mbti: yup
-    .tuple([
-      yup.string().required('MBTI 4자리를 모두 선택해주세요.'),
-      yup.string().required('MBTI 4자리를 모두 선택해주세요.'),
-      yup.string().required('MBTI 4자리를 모두 선택해주세요.'),
-      yup.string().required('MBTI 4자리를 모두 선택해주세요.'),
-    ])
-    .nullable(),
-  mbtiDescription: yup.string().nullable(),
 });
