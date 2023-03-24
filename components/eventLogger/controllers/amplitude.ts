2a6ff85d4dcf8f82e1a9daca4ae58edaa240fb85
Tekiter <tekiter8@gmail.com>
2023-03-25 01:01:44 +0900

    feat: 유저 아이디 트래킹 추가

--- components/eventLogger/controllers/amplitude.ts
+++ components/eventLogger/controllers/amplitude.ts
@@ -2,9 +2,10 @@ import { createInstance } from '@amplitude/analytics-browser';
 
 import { EventLoggerController } from '@/components/eventLogger/types';
 
-export function createAmplitudeController(apiKey: string): EventLoggerController {
+export function createAmplitudeController(apiKey: string, userId: string | undefined): EventLoggerController {
   const instance = createInstance();
   instance.init(apiKey);
+  instance.setUserId(userId);
 
   return {
     clickEvent(key, params) {
