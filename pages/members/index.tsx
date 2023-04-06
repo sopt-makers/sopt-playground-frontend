eee82bb8f904d39690079bb9e80901f283fe8732
NaLeum <wognsdl@likelion.org>
2023-04-06 10:58:31 +0900

    feat: 스터디 모집 배너 제거

--- pages/members/index.tsx
+++ pages/members/index.tsx
@@ -1,14 +1,12 @@
 import { FC } from 'react';
 
 import AuthRequired from '@/components/auth/AuthRequired';
-import StudyBanner from '@/components/common/Banner/StudyBanner';
 import MemberList from '@/components/members/main/MemberList';
 import { setLayout } from '@/utils/layout';
 
 const UserPage: FC = () => {
   return (
     <AuthRequired>
-      <StudyBanner />
       <MemberList />
     </AuthRequired>
   );
