ca5584c0b0d65c2637dbecf4ac891a06c1db11fd
NaLeum <wognsdl@likelion.org>
2023-04-15 18:10:14 +0900

    feat: SOPT 1차 행사 배너 제작

---
+++ components/common/Banner/SOPTEventBanner/MobileEventBanner.tsx
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled';
+
+import { textStyles } from '@/styles/typography';
+
+import { TERM } from './constants';
+
+export default function MobileEventBanner() {
+  return (
+    <Container>
+      <Text>{`🌤️ ${TERM}차 행사 참여자 모집`}</Text>
+      <Text>{`>`}</Text>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background: linear-gradient(164.77deg, #010101 19.93%, #2b26ff 141.3%), #000000;
+  padding: 14px 16px;
+`;
+
+const Text = styled.div`
+  line-height: 100%;
+  color: #fff;
+
+  ${textStyles.SUIT_20_B}
+`;
---
+++ components/common/Banner/SOPTEventBanner/index.tsx
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
+
+import Responsive from '@/components/common/Responsive';
+import { playgroundLink } from '@/constants/links';
+
+import MobileEventBanner from './MobileEventBanner';
+const DesktopEventBanner = dynamic(() => import('./DesktopEventBanner'), { ssr: false });
+
+interface SOPTEventBannerProps {
+  className?: string;
+}
+
+export default function SOPTEventBanner({ className }: SOPTEventBannerProps) {
+  return (
+    <>
+      <Responsive only='mobile' asChild>
+        <Link href={playgroundLink.groupList()} target='_blank' className={className}>
+          <MobileEventBanner />
+        </Link>
+      </Responsive>
+      <Responsive only='desktop' asChild>
+        <Link href={playgroundLink.groupList()} target='_blank' className={className}>
+          <DesktopBannerContainer>
+            <DesktopEventBanner />
+          </DesktopBannerContainer>
+        </Link>
+      </Responsive>
+    </>
+  );
+}
+
+const DesktopBannerContainer = styled.section`
+  width: 100%;
+  height: 98px;
+`;
