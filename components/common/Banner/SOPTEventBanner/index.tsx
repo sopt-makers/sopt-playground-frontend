bfa562bbc95be32608e93598fbb0b66e485abb78
NaLeum <wognsdl@likelion.org>
2023-04-16 19:41:01 +0900

    fix: target='_blank' 제거

--- components/common/Banner/SOPTEventBanner/index.tsx
+++ components/common/Banner/SOPTEventBanner/index.tsx
@@ -3,8 +3,8 @@ import dynamic from 'next/dynamic';
 import Link from 'next/link';
 
 import Responsive from '@/components/common/Responsive';
-import { playgroundLink } from '@/constants/links';
 
+import { EVENT_LINK } from './constants';
 import MobileEventBanner from './MobileEventBanner';
 const DesktopEventBanner = dynamic(() => import('./DesktopEventBanner'), { ssr: false });
 
@@ -16,12 +16,12 @@ export default function SOPTEventBanner({ className }: SOPTEventBannerProps) {
   return (
     <>
       <Responsive only='mobile' asChild>
-        <Link href={playgroundLink.groupList()} target='_blank' className={className}>
+        <Link href={EVENT_LINK} className={className}>
           <MobileEventBanner />
         </Link>
       </Responsive>
       <Responsive only='desktop' asChild>
-        <Link href={playgroundLink.groupList()} target='_blank' className={className}>
+        <Link href={EVENT_LINK} className={className}>
           <DesktopBannerContainer>
             <DesktopEventBanner />
           </DesktopBannerContainer>
