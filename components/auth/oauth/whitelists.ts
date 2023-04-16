f7dcb6d7cb95fa8ee3758c49b0b92345eef89c69
Tekiter <tekiter8@gmail.com>
2023-04-16 15:40:11 +0900

    feat: 화이트리스트 변경

--- components/auth/oauth/whitelists.ts
+++ components/auth/oauth/whitelists.ts
@@ -1,5 +1,5 @@
 export const REDIRECT_URI_WHITELISTS = [
   'http://localhost:3000',
   'app://org.sopt.makers.android/oauth2redirect',
-  'app://org.sopt.makers.iOS/oauth2redirect',
+  'sopt-makers://org.sopt.makers.iOS/oauth2redirect',
 ];
