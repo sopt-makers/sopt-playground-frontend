1a48f2dcfd3b7b55e01132e6e7f85ed41455c646
NaReum <devnareum@gmail.com>
2023-04-17 22:33:03 +0900

    feat: 1차행사 모집 배너 철거 (#646)

--- pages/members/index.tsx
+++ pages/members/index.tsx
@@ -1,14 +1,12 @@
 import { FC } from 'react';
 
 import AuthRequired from '@/components/auth/AuthRequired';
-import SOPTEventBanner from '@/components/common/Banner/SOPTEventBanner';
 import MemberList from '@/components/members/main/MemberList';
 import { setLayout } from '@/utils/layout';
 
 const UserPage: FC = () => {
   return (
     <AuthRequired>
-      <SOPTEventBanner />
       <MemberList />
     </AuthRequired>
   );
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -2,14 +2,12 @@ import { uniqBy as _uniqBy } from 'lodash-es';
 import { FC } from 'react';
 
 import AuthRequired from '@/components/auth/AuthRequired';
-import SOPTEventBanner from '@/components/common/Banner/SOPTEventBanner';
 import ProjectList from '@/components/projects/main/ProjectList';
 import { setLayout } from '@/utils/layout';
 
 const ProjectPage: FC = () => {
   return (
     <AuthRequired>
-      <SOPTEventBanner />
       <ProjectList />
     </AuthRequired>
   );
