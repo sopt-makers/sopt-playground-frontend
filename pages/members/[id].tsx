ebc2b62b7817af7d2c40accde540560401147d68
남주영 <njy1007@gmail.com>
2023-04-20 11:14:09 +0900

    refactor: 멤버 상세 컴포넌트 폴더링 개선 (#649)

--- pages/members/[id].tsx
+++ pages/members/[id].tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 import AuthRequired from '@/components/auth/AuthRequired';
 import MobileHeader from '@/components/common/MobileHeader';
-import MemberDetail from '@/components/members/main/MemberDetail';
+import MemberDetail from '@/components/members/detail/MemberDetail';
 import useStringRouterQuery from '@/hooks/useStringRouterQuery';
 import { setLayout } from '@/utils/layout';
 
