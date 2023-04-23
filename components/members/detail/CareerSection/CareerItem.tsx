99145c93e705961aab8258594478615ba95cbb1c
jynam <njy1007@gmail.com>
2023-04-23 21:47:11 +0900

    feat: CareerItem 새 디자인 적용

--- components/members/detail/CareerSection/CareerItem.tsx
+++ components/members/detail/CareerSection/CareerItem.tsx
@@ -4,7 +4,6 @@ import { FC } from 'react';
 
 import { Career } from '@/components/members/detail/types';
 import { colors } from '@/styles/colors';
-import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
 import { textStyles } from '@/styles/typography';
 
 type CareerItemProps = {
@@ -14,15 +13,19 @@ type CareerItemProps = {
 const CareerItem: FC<CareerItemProps> = ({ career }) => {
   return (
     <Container>
+      <LineBetween>
+        <div className='circle' />
+        <div className='line' />
+      </LineBetween>
       <div>
-        <div>{career.companyName}</div>
-        <Divider />
-        <div>{career.title}</div>
-        <Divider className='pc-only' />
-      </div>
-      <div>
-        {`${dayjs(career.startDate).format('YYYY.MM')}${' - '}
-          ${career.isCurrent ? 'NOW' : dayjs(career.endDate).format('YYYY.MM')}`}
+        <CompanyName>{career.companyName}</CompanyName>
+        <SubContent>
+          <div className='job-position'>{career.title}</div>
+          <Divider />
+          <div>{`${dayjs(career.startDate).format('YYYY.MM')}${' - '}${
+            career.isCurrent ? 'NOW' : dayjs(career.endDate).format('YYYY.MM')
+          }`}</div>
+        </SubContent>
       </div>
     </Container>
   );
@@ -30,48 +33,52 @@ const CareerItem: FC<CareerItemProps> = ({ career }) => {
 
 const Container = styled.div`
   display: flex;
-  ${textStyles.SUIT_18_M}
+  gap: 12px;
+`;
 
-  & > div {
-    display: flex;
-    align-items: center;
+const LineBetween = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & > .circle {
+    margin-top: 6px;
+    border-radius: 50%;
+    background-color: ${colors.gray60};
+    width: 6px;
+    height: 6px;
   }
 
-  @media ${MOBILE_MEDIA_QUERY} {
-    flex-direction: column;
-    gap: 12px;
-    ${textStyles.SUIT_15_M};
-
-    color: ${colors.gray30};
-
-    &:not(:last-child) {
-      margin-bottom: 16px;
-    }
-
-    & > div:first-of-type {
-      display: flex;
-      align-items: center;
-      border: none;
-
-      & > div:first-of-type {
-        ${textStyles.SUIT_16_B};
-
-        color: ${colors.white100};
-      }
-    }
+  & > .line {
+    margin-top: 3.5px;
+    border-radius: 1px;
+    background-color: ${colors.gray60};
+    width: 1px;
+    height: 52px;
   }
 `;
 
-const Divider = styled.div`
-  margin: 0 20px;
-  background-color: ${colors.gray100};
-  width: 1.5px;
-  height: 14px;
+const SubContent = styled.div`
+  display: flex;
+  gap: 12px;
+  align-items: center;
+  color: ${colors.gray60};
 
-  @media ${MOBILE_MEDIA_QUERY} {
-    margin: 0 12px;
-    height: 12px;
-  }
+  ${textStyles.SUIT_14_M};
+`;
+
+const Divider = styled.div`
+  background-color: ${colors.gray100};
+  width: 1px;
+  height: 14px;
+`;
+
+const CompanyName = styled.div`
+  margin-bottom: 10px;
+  line-height: 100%;
+  color: ${colors.white};
+
+  ${textStyles.SUIT_18_M};
 `;
 
 export default CareerItem;
