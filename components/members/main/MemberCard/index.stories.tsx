8b539bd730a0b7e865081b3ef7ce317bbf6a1534
Tekiter <tekiter8@gmail.com>
2023-04-27 18:16:16 +0900

    fix: 멤버 카드 오류 수정 (#670)
    * fix: 가로선 색 수정
    
    * fix: 긴 소속 ellipsis 추가
    
    * feat: 애니메이션 변경
    
    * fix: 모바일 페이드 색상 오류 수정

--- components/members/main/MemberCard/index.stories.tsx
+++ components/members/main/MemberCard/index.stories.tsx
@@ -21,7 +21,7 @@ Default.args = {
   name: '박건영',
   imageUrl:
     'https://s3.ap-northeast-2.amazonaws.com/sopt-makers-internal//prod/image/project/45a0271c-895c-427b-9ff2-a2efb4daa7ce-1679161408215.jpg',
-  belongs: 'Google',
+  belongs: 'Google에서일하면어떤기분일까정말궁금하다',
   badges: [
     { content: '29기 웹', isActive: false },
     { content: '2기 메이커스', isActive: true },
