17793d9e826ba3d674d7885035c0e767d101b146
NaLeum <wognsdl@likelion.org>
2023-05-07 12:33:10 +0900

    feat: SOPTKATON 배너 제작

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -2,12 +2,14 @@ import { uniqBy as _uniqBy } from 'lodash-es';
 import { FC } from 'react';
 
 import AuthRequired from '@/components/auth/AuthRequired';
+import SOPTKATONBanner from '@/components/common/Banner/SOPTKATONBanner';
 import ProjectList from '@/components/projects/main/ProjectList';
 import { setLayout } from '@/utils/layout';
 
 const ProjectPage: FC = () => {
   return (
     <AuthRequired>
+      <SOPTKATONBanner />
       <ProjectList />
     </AuthRequired>
   );
