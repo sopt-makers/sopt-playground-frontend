29bafa646af3291572a5591a729fcee686b17362
남주영 <njy1007@gmail.com>
2023-05-27 02:05:37 +0900

    feat: 멘토링 카드 및 캐러셀 구현 (#710)
    * feat: 멘토링 카드 마크업
    
    * refactor: 스토리 이름 더욱 적절히 변경
    
    * feat: 캐러셀 컴포넌트 구현
    
    * refactor: 네이밍 정리
    
    * feat: indicator로 페이지 이동할 수 있도록
    
    * refactor: 가독성 개선
    
    * chore: 더 적절한 태그로 변경
    
    * chore: indicator 스타일 및 네이밍 개선
    
    * chore: 컨트롤 누를 수 없는 상태엔 cursor default로
    
    * chore: 레이아웃 그리드로 변경
    
    * fix: 페이지 이동 시 스크롤바 생기는 버그 fix
    
    * refactor: 캐러셀 방향 state 더 명확하게 표현

---
+++ components/common/Carousel/index.stories.tsx
@@ -0,0 +1,60 @@
+import styled from '@emotion/styled';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ReactNode } from 'react';
+
+import Carousel from '@/components/common/Carousel';
+import MentoringCard from '@/components/mentoring/MentoringCard';
+
+export default {
+  components: Carousel,
+  decorators: [
+    (Story) => (
+      <div style={{ width: '1414px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof Carousel>;
+
+const Template: ComponentStory<typeof Carousel> = (args) => <Carousel {...args} />;
+
+const MENTORING_DUMMY_DATA = [
+  {
+    mentor: { name: '남주영', career: '나사' },
+    keywords: ['별자리 찾기', '우주의 탄생 과정에 대해 알아보기'],
+    title: '정우와 함께하는 CGP Review',
+  },
+  {
+    mentor: { name: '송정우', career: 'AWS' },
+    keywords: ['코드 리뷰', '아무거나 물어보세용', '취업 준비 과정에서 우선 순위 정하기'],
+    title: '하둘셋넷다여일여아열하둘셋넷다여일여아열하둘셋넷다여일여아열',
+  },
+  {
+    mentor: { name: '박건영', career: '어둠의 커비단' },
+    keywords: ['코드 리뷰', '취업 준비 과정에서 우선 순위 정하기', '노동요추천-에스파스파이씨'],
+    title: '개발 천재 되는 법',
+  },
+  {
+    mentor: { name: '이정연', career: '당근마켓' },
+    keywords: ['개자이피엠'],
+    title: '개발 천재 되는 법',
+  },
+  {
+    mentor: { name: '이준호', career: '고수수현' },
+    keywords: ['고수수현', '중수수현', '하수수현'],
+    title: '개발 천재 되는 법',
+  },
+];
+
+const CardContainer = styled.div`
+  display: flex;
+  gap: 15px;
+  width: 100%;
+`;
+
+export const Basic = Template.bind({});
+Basic.args = {
+  itemList: MENTORING_DUMMY_DATA.map((dummy, index) => <MentoringCard {...dummy} key={index} />),
+  limit: 3,
+  renderItemContainer: (children: ReactNode) => <CardContainer>{children}</CardContainer>,
+};
