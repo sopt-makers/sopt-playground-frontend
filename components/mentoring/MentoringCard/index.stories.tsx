7d996ed575e7e96c19828869a624100b45074b84
jynam <njy1007@gmail.com>
2023-06-02 16:52:40 +0900

    fix: 스토리북 버그 fix

--- components/mentoring/MentoringCard/index.stories.tsx
+++ components/mentoring/MentoringCard/index.stories.tsx
@@ -13,6 +13,7 @@ Default.args = {
   mentor: { name: '남주영', career: '나사' },
   keywords: ['별자리 찾기', '우주의 탄생 과정에 대해 알아보기'],
   title: '정우와 함께하는 CGP Review',
+  isOpened: true,
 };
 Default.storyName = '기본';
 
@@ -21,6 +22,7 @@ LongData.args = {
   mentor: { name: '송정우', career: 'AWS' },
   keywords: ['코드 리뷰', '아무거나 물어보세용', '취업 준비 과정에서 우선 순위 정하기'],
   title: '하둘셋넷다여일여아열하둘셋넷다여일여아열하둘셋넷다여일여아열',
+  isOpened: true,
 };
 LongData.storyName = '긴 제목, 키워드';
 
