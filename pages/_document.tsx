b496c7dadd33bb322f3cb198e7fa2cf39c73cdbb
Jun <jun.juno@kakaostyle.com>
2023-06-14 09:42:12 +0900

    feat: 폰트 경량화 (#792)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,10 +6,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel='preload' href='/fonts/SUIT-ExtraBold.ttf' as='font' type='font/ttf' crossOrigin='' />
-        <link rel='preload' href='/fonts/SUIT-Bold.ttf' as='font' type='font/ttf' crossOrigin='' />
-        <link rel='preload' href='/fonts/SUIT-SemiBold.ttf' as='font' type='font/ttf' crossOrigin='' />
-        <link rel='preload' href='/fonts/SUIT-Medium.ttf' as='font' type='font/ttf' crossOrigin='' />
+        <link rel='preload' href='/fonts/SUIT-ExtraBold.woff2' as='font' type='font/woff2' crossOrigin='' />
+        <link rel='preload' href='/fonts/SUIT-Bold.woff2' as='font' type='font/woff2' crossOrigin='' />
+        <link rel='preload' href='/fonts/SUIT-SemiBold.woff2' as='font' type='font/woff2' crossOrigin='' />
+        <link rel='preload' href='/fonts/SUIT-Medium.woff2' as='font' type='font/woff2' crossOrigin='' />
       </Head>
       <body>
         <GoogleTagManagerNoscript />
