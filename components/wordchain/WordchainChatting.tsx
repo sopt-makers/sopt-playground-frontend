2bc4a8c67cfc611ee86485b0d2dfd9ad4b75b6c6
jynam <njy1007@gmail.com>
2023-06-17 20:11:53 +0900

    feat: 끝말잇기 페이지 레이아웃 잡기

---
+++ components/wordchain/WordchainChatting.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+import { colors } from '@/styles/colors';
+
+export default function WordchainChatting() {
+  return <Container>WordchainChatting</Container>;
+}
+
+const Container = styled.div`
+  border-radius: 30px;
+  background-color: ${colors.black80};
+  width: 790px;
+  height: 728px;
+`;
