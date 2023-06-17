410ef0f99ee3eee2cace4bfb2a5542ea1d798508
Jun <jun.juno@kakaostyle.com>
2023-06-17 20:28:17 +0900

    chore: layout 잡기

--- pages/wordchain/index.tsx
+++ pages/wordchain/index.tsx
@@ -8,24 +8,34 @@ export default function WordchainPage() {
   return (
     <AuthRequired>
       <Container>
-        <Title>SOPT와 함께하는 끝말잇기</Title>
-        <StyledWordchainChatting />
-        <Sidebar>Sidebar</Sidebar>
+        <Wrapper>
+          <Title>💬 SOPT와 함께하는 끝말잇기</Title>
+          <StyledWordchainChatting />
+          <Sidebar>Sidebar</Sidebar>
+        </Wrapper>
       </Container>
     </AuthRequired>
   );
 }
 
 const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+
+const Wrapper = styled.div`
   display: grid;
   grid:
     [row1-start] 'title title' min-content [row1-end]
     [row2-start] 'wordchainChatting sidebar' 728px [row2-end]
     / 790px 324px;
+  gap: 40px 32px;
 `;
 
 const Title = styled.h1`
   grid-area: title;
+  margin-top: 80px;
 `;
 
 const Sidebar = styled.aside`
