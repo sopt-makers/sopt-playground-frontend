f60f85d39c9a9e346ee2d57b870dafcfc0a973ec
jynam <njy1007@gmail.com>
2023-06-21 00:45:31 +0900

    feat: post api 연결

--- components/wordchain/WordchainChatting/index.tsx
+++ components/wordchain/WordchainChatting/index.tsx
@@ -1,16 +1,60 @@
 import styled from '@emotion/styled';
+import { InfiniteData, useQueryClient } from '@tanstack/react-query';
 import PaperAirplaneIcon from 'public/icons/icon-paper-airplane.svg';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
 
-import { useGetWordchain } from '@/api/endpoint/wordchain/getWordchain';
+import { useGetWordchain, UseGetWordchainResponse } from '@/api/endpoint/wordchain/getWordchain';
+import { usePostWord } from '@/api/endpoint/wordchain/postWord';
 import Wordchain from '@/components/wordchain/WordchainChatting/Wordchain';
 import { colors } from '@/styles/colors';
 
+const LIMIT = 50;
+
 export default function WordchainChatting() {
-  const { data: wordchainData } = useGetWordchain({ limit: 50 });
+  const { data: wordchainData } = useGetWordchain({ limit: LIMIT });
+  const [word, setWord] = useState('');
+  const queryClient = useQueryClient();
+  const { mutate: postWord } = usePostWord({
+    onSuccess: ({ word, user }) => {
+      setWord('');
+      queryClient.setQueryData<InfiniteData<UseGetWordchainResponse>>(['getWordchain', LIMIT], (old) => {
+        return old
+          ? {
+              ...old,
+              pages: [
+                {
+                  ...old.pages[0],
+                  wordchainList: [
+                    ...old.pages[0].wordchainList.slice(0, old.pages[0].wordchainList.length - 1),
+                    {
+                      ...old.pages[0].wordchainList[old.pages[0].wordchainList.length - 1],
+                      wordList: [
+                        ...old.pages[0].wordchainList[old.pages[0].wordchainList.length - 1].wordList,
+                        { content: word, user },
+                      ],
+                    },
+                  ],
+                },
+                ...old.pages.slice(1),
+              ],
+            }
+          : old;
+      });
+    },
+  });
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!wordchainData || word.length < 1) {
+      return;
+    }
+    const wordchainList = wordchainData.pages;
+    await postWord({ wordchainId: wordchainList[wordchainList.length - 1].id, word });
+    setWord('');
+  };
+
+  const handleChange = (value: string) => {
+    setWord(value);
   };
 
   return (
@@ -21,8 +65,14 @@ export default function WordchainChatting() {
         ))}
       </WordchainList>
       <Form onSubmit={handleSubmit}>
-        <StyledInput placeholder='단어를 입력해주세요. (단, 표준국어대사전에 있는 단어만 사용할 수 있어요.)' />
-        <StyledPaperAirplaneIcon />
+        <StyledInput
+          value={word}
+          onChange={(e) => handleChange(e.target.value)}
+          placeholder='단어를 입력해주세요. (단, 표준국어대사전에 있는 단어만 사용할 수 있어요.)'
+        />
+        <SubmitButton>
+          <PaperAirplaneIcon />
+        </SubmitButton>
       </Form>
     </Container>
   );
@@ -85,9 +135,10 @@ const StyledInput = styled.input<{ isError?: boolean }>`
   }
 `;
 
-const StyledPaperAirplaneIcon = styled(PaperAirplaneIcon)`
+const SubmitButton = styled.button`
   position: absolute;
   right: 24px;
   bottom: 24px;
-  cursor: pointer;
+  width: 20px;
+  height: 20px;
 `;
