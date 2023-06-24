67fcae3da82c61a12e5da597ccd2048471d29201
Tekiter <tekiter8@gmail.com>
2023-06-24 19:05:44 +0900

    chore: storybook 업그레이드

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
 module.exports = {
   stories: ['../**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
@@ -8,37 +7,39 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     'storybook-dark-mode',
-    'storybook-addon-next-router',
   ],
-  framework: '@storybook/react',
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
   webpackFinal: async (config) => {
-    config.resolve.plugins = [
-      ...(config.resolve.plugins || []),
-      new TsconfigPathsPlugin({
-        extensions: config.resolve.extensions,
-      }),
-    ];
-    config.module.rules.push({
-      test: /\.(css|scss)$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-    });
+    // config.resolve.plugins = [
+    //   ...(config.resolve.plugins || []),
+    //   new TsconfigPathsPlugin({
+    //     extensions: config.resolve.extensions,
+    //   }),
+    // ];
+
+    // config.module.rules.push({
+    //   test: /\.(css|scss)$/,
+    //   use: ['style-loader', 'css-loader', 'sass-loader'],
+    // });
 
     // MEMO: 스토리북에선 svg를 file-loader로 읽어오는데, 이를 svgr로 읽어오도록 설정
-    const fileLoaderRule = config.module.rules.find((rule) => rule.test.test('.svg'));
-    fileLoaderRule.exclude = /\.svg$/;
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
+    // const fileLoaderRule = config.module.rules.find((rule) => rule.test.test('.svg'));
+    // fileLoaderRule.exclude = /\.svg$/;
+    // config.module.rules.push({
+    //   test: /\.svg$/,
+    //   use: ['@svgr/webpack'],
+    // });
 
-    config.resolve.alias = {
-      '@': path.resolve(__dirname),
-    };
-
+    // config.resolve.alias = {
+    //   '@': path.resolve(__dirname),
+    // };
     return config;
   },
   staticDirs: ['../public'],
-  core: {
-    builder: '@storybook/builder-webpack5',
+  docs: {
+    autodocs: true,
   },
 };
