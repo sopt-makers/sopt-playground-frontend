e9e7ebff1a9155c2f7e4d40a711a9fd6d28f3d52
Jun <jun.juno@kakaostyle.com>
2023-07-17 13:50:02 +0900

    feat: 프로젝트 업로드 페이지 퍼블리싱 추가

---
+++ components/common/Divider/Divider.tsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+import { FC } from 'react';
+
+import { colors } from '@/styles/colors';
+
+interface DividerProps {
+  className?: string;
+  color?: string;
+}
+const Divider: FC<DividerProps> = ({ color = colors.black60, className }) => {
+  return <StyledDivider className={className} color={color} />;
+};
+
+export default Divider;
+
+const StyledDivider = styled.hr<Pick<DividerProps, 'color'>>`
+  border: none;
+  background-color: ${({ color }) => color};
+  width: 100%;
+  height: 1.5px;
+`;
--- pages/projects/upload/v2/index.tsx
+++ pages/projects/upload/v2/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import styled from '@emotion/styled';
 
+import AuthRequired from '@/components/auth/AuthRequired';
 import ProjectForm from '@/components/projects/form/ProjectForm';
+import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
+import { setLayout } from '@/utils/layout';
 
 const ProjectUploadPage = () => {
   return (
-    <div>
-      <ProjectForm submitButtonContent='프로젝트 등록하기' />
-    </div>
+    <AuthRequired>
+      <Container>
+        <ProjectForm submitButtonContent='프로젝트 등록하기' />
+      </Container>
+    </AuthRequired>
   );
 };
 
 export default ProjectUploadPage;
+
+setLayout(ProjectUploadPage, 'header');
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 98px 0;
+
+  @media ${MOBILE_MEDIA_QUERY} {
+    margin-top: 12px;
+  }
+`;
