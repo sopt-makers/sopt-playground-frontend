e0c78d524679aec1b8757556d53fb87bed493d65
Jun <jun.juno@kakaostyle.com>
2023-07-19 09:57:28 +0900

    fix: 이미지 슬라이더 네비게이션 버튼 포지션 수정

--- components/projects/main/ProjectImageSlider.tsx
+++ components/projects/main/ProjectImageSlider.tsx
@@ -26,9 +26,12 @@ const ProjectImageSlider: FC<ProjectImageSliderProps> = ({ images, className }) 
     <Container className={className}>
       <MainSwiperWrapper>
         <Responsive only='desktop'>
-          <button ref={prevButton}>
+          <PrevNavigationButton ref={prevButton}>
             <IconPrev />
-          </button>
+          </PrevNavigationButton>
+          <NextNavigationButton ref={nextButton}>
+            <IconNext />
+          </NextNavigationButton>
         </Responsive>
         <StyledSwiper
           thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
@@ -48,11 +51,6 @@ const ProjectImageSlider: FC<ProjectImageSliderProps> = ({ images, className }) 
             </StyledSwiperSlide>
           ))}
         </StyledSwiper>
-        <Responsive only='desktop'>
-          <button ref={nextButton}>
-            <IconNext />
-          </button>
-        </Responsive>
       </MainSwiperWrapper>
       <ThumbsSwiper spaceBetween={isMobile ? 8 : 20} slidesPerView='auto' onSwiper={setThumbsSwiper} modules={[Thumbs]}>
         {images.map((image, index) => (
@@ -76,9 +74,19 @@ const Container = styled.div`
 `;
 
 const MainSwiperWrapper = styled.div`
-  display: flex;
-  gap: 24px;
-  align-items: center;
+  position: relative;
+`;
+
+const PrevNavigationButton = styled.button`
+  position: absolute;
+  top: 50%;
+  left: -50px;
+`;
+
+const NextNavigationButton = styled.button`
+  position: absolute;
+  top: 50%;
+  right: -50px;
 `;
 
 const StyledSwiper = styled(Swiper)`
