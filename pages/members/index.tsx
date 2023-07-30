a1a2287f0679caa499b1dc054f969b3dacc2a22a
jynam <njy1007@gmail.com>
2023-07-30 23:30:31 +0900

    feat: 온보딩 배너 삭제

--- pages/members/index.tsx
+++ pages/members/index.tsx
@@ -1,13 +1,11 @@
 import styled from '@emotion/styled';
 import { FC } from 'react';
 
-import { useGetMemberOfMe } from '@/api/endpoint/members/getMemberOfMe';
 import AuthRequired from '@/components/auth/AuthRequired';
 import ActiveBannerSlot from '@/components/common/Banner/ActiveBannerSlot';
 import Responsive from '@/components/common/Responsive';
 import { MemberPageContentLayout } from '@/components/members/common/MemberPageLayout';
 import MemberList from '@/components/members/main/MemberList';
-import OnBoardingBanner from '@/components/members/main/MemberList/OnBoardingBanner';
 import MentoringList from '@/components/mentoring/MentoringList';
 import WordChainEntry from '@/components/wordchain/WordchainEntry/WordChainEntry';
 import { colors } from '@/styles/colors';
@@ -15,11 +13,6 @@ import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
 import { setLayout } from '@/utils/layout';
 
 const MemberPage: FC = () => {
-  const { data: memberOfMeData } = useGetMemberOfMe();
-
-  const hasProfile = !!memberOfMeData?.hasProfile;
-  const onboardingBanner = memberOfMeData && !hasProfile && <StyledOnBoardingBanner name={memberOfMeData.name ?? ''} />;
-
   return (
     <AuthRequired>
       <ActiveBannerSlot />
@@ -30,7 +23,7 @@ const MemberPage: FC = () => {
         <HDivider />
       </Responsive>
       <MentoringList />
-      <MemberList banner={onboardingBanner} />
+      <MemberList banner={<></>} />
     </AuthRequired>
   );
 };
@@ -44,14 +37,6 @@ const StyledWordChainEntry = styled(WordChainEntry)`
 
   @media ${MOBILE_MEDIA_QUERY} {
     margin-top: 16px;
-  }
-`;
-
-const StyledOnBoardingBanner = styled(OnBoardingBanner)`
-  margin-bottom: 90px;
-
-  @media ${MOBILE_MEDIA_QUERY} {
-    margin: 45px 0;
   }
 `;
 
