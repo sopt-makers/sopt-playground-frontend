9f17c0edf1fe77f29192767f1e5bbe11fbad01ca
Jun <jun.juno@kakaostyle.com>
2023-08-06 19:24:50 +0900

    fix: 프로젝트 진행상황 - 기수 버그 수정, 프로젝트 이미지 추가

--- components/projects/form/UploadProjectProgress.tsx
+++ components/projects/form/UploadProjectProgress.tsx
@@ -14,7 +14,13 @@ const UploadProjectProgress: FC<UploadProjectProgressProps> = ({ formState }) =>
 
   const items: FormProgressItem[] = [
     { title: '프로젝트 이름', active: dirtyFields.name, required: true },
-    { title: '기수', active: dirtyFields.generation, required: true },
+    {
+      title: '기수',
+      active:
+        dirtyFields.generation ||
+        (formState.defaultValues?.generation !== null && formState.defaultValues?.generation !== undefined),
+      required: true,
+    },
     { title: '어디서 진행했나요?', active: dirtyFields.category, required: true },
     {
       title: '팀원',
@@ -27,6 +33,11 @@ const UploadProjectProgress: FC<UploadProjectProgressProps> = ({ formState }) =>
     { title: '프로젝트 설명', active: dirtyFields.detail, required: true },
     { title: '로고 이미지', active: dirtyFields.logoImage, required: true },
     { title: '썸네일 이미지', active: dirtyFields.thumbnailImage, required: true },
+    {
+      title: '프로젝트 이미지',
+      active: dirtyFields.projectImages?.some((image) => Boolean(image) && image.imageUrl !== false),
+      required: true,
+    },
   ];
 
   return <StyledFormProgress title='등록 진행' progressLabel='프로젝트를 등록해주세요.' items={items} />;
