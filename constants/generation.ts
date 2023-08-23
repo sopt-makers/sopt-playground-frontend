962467e33b43218d7ab56964b3ea6a392fd02a9d
Tekiter <tekiter8@gmail.com>
2023-08-23 17:14:08 +0900

    feat: 33기 적용 (#934)

--- constants/generation.ts
+++ constants/generation.ts
@@ -1,3 +1,3 @@
-export const LATEST_GENERATION = 32;
+export const LATEST_GENERATION = 33;
 
 export const GENERATIONS = Array.from({ length: LATEST_GENERATION }, (_, i) => (i + 1).toString()).reverse();
