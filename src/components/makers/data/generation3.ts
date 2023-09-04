91102b30dceeba38145e60c03414c12ebbc438e8
Tekiter <tekiter8@gmail.com>
2023-09-04 16:43:07 +0900

    feat: 3기 정보 추가 (#938)

---
+++ src/components/makers/data/generation3.ts
@@ -0,0 +1,109 @@
+import { MakersGeneration } from '@/components/makers/data/types';
+
+export const generation3: MakersGeneration = {
+  title: '3기',
+  teams: [
+    {
+      title: 'makers 운영진',
+      people: [
+        { type: 'member', id: 265, name: '김유빈', position: '팀장' },
+        { type: 'member', id: 10, name: '주수한', position: '부팀장' },
+        { type: 'member', id: 13, name: '김은수', position: '운영진' },
+        { type: 'member', id: 2, name: '박건영', position: '운영진' },
+        { type: 'member', id: 22, name: '윤수빈', position: '운영진' },
+        { type: 'member', id: 64, name: '이화정', position: '운영진' },
+      ],
+    },
+    {
+      title: 'SOPT 공식 홈페이지 팀',
+      description: 'SOPT를 대내외적으로 알릴 수 있는 공식홈페이지를 개선하고 발전시켜요.',
+      link: 'https://sopt.org',
+      people: [
+        { type: 'member', id: 4, name: '박이정', position: 'PM' },
+        { type: 'member', id: 569, name: '임찬기', position: '백엔드 개발자' },
+        { type: 'member', id: 303, name: '권윤', position: '프로덕트 디자이너' },
+        { type: 'member', id: 554, name: '정그린', position: '프로덕트 디자이너' },
+        { type: 'member', id: 186, name: '김서진', position: '웹 프론트엔드 개발자' },
+        { type: 'member', id: 107, name: '송우영', position: '웹 프론트엔드 개발자' },
+        { type: 'member', id: 192, name: '이혜준', position: '웹 프론트엔드 개발자' },
+      ],
+    },
+    {
+      title: '플레이그라운드 팀',
+      description:
+        'SOPT 구성원들을 위한 서비스, SOPT 플레이그라운드를 만들고 있어요. 앱잼, 솝커톤, 솝텀 등 멋진 프로젝트들이 잘 기록될 수 있도록 프로젝트 기능을 담당해요. 또한 이천여명의 SOPT 구성원들이 어떻게 하면 더 잘 소통하고 가치를 연결할 수 있을지 고민하는 팀이에요.',
+      people: [
+        { type: 'member', id: 396, name: '김수연', position: 'PM' },
+        { type: 'member', id: 570, name: '김재민', position: 'PM' },
+        { type: 'member', id: 1, name: '이정연', position: '프로덕트 디자이너' },
+        { type: 'member', id: 64, name: '이화정', position: '프로덕트 디자이너' },
+        { type: 'member', id: 358, name: '민솔', position: '프로덕트 디자이너' },
+        { type: 'member', id: 99, name: '한유진', position: '백엔드 개발자' },
+        { type: 'member', id: 61, name: '이승헌', position: '백엔드 개발자' },
+        { type: 'member', id: 2, name: '박건영', position: '웹 프론트엔드 개발자' },
+        { type: 'member', id: 3, name: '이준호', position: '웹 프론트엔드 개발자' },
+        { type: 'member', id: 8, name: '남주영', position: '웹 프론트엔드 개발자' },
+        { type: 'member', id: 85, name: '서지수', position: '웹 프론트엔드 개발자' },
+      ],
+    },
+    {
+      title: 'Product Crew 팀',
+      description: 'SOPT 플레이그라운드 내에서 구성원들이 하나로 모일 수 있는 모임 서비스를 만들고 있어요.',
+      people: [
+        { type: 'member', id: 10, name: '주수한', position: 'PM' },
+        { type: 'member', id: 23, name: '이예서', position: '프로덕트 디자이너' },
+        { type: 'member', id: 78, name: '배민주', position: '프로덕트 디자이너' },
+        { type: 'member', id: 281, name: '강영우', position: '백엔드 개발자' },
+        { type: 'member', id: 171, name: '조예슬', position: '백엔드 개발자' },
+        { type: 'member', id: 13, name: '김은수', position: '웹 프론트엔드 개발자' },
+        { type: 'member', id: 36, name: '이재훈', position: '웹 프론트엔드 개발자' },
+        { type: 'member', id: 28, name: '백지연', position: '웹 프론트엔드 개발자' },
+        { type: 'member', id: 143, name: '김현수', position: '웹 프론트엔드 개발자' },
+      ],
+    },
+    {
+      title: 'SOPT APP 팀',
+      description:
+        'SOPT 공식 앱을 만드는 팀이에요. SOPT를 더욱 재미있고, 알차게 활동할 수 있도록 도와주는 서비스를 만들고 있어요.',
+      people: [
+        { type: 'member', id: 128, name: '고수현', position: 'PM' },
+        { type: 'member', id: 180, name: '심유나', position: 'PM' },
+        { type: 'member', id: 66, name: '손혜정', position: '프로덕트 디자이너' },
+        { type: 'member', id: 390, name: '백송현', position: '프로덕트 디자이너' },
+        { type: 'member', id: 410, name: '구건모', position: '백엔드 개발자' },
+        { type: 'member', id: 357, name: '정동규', position: '백엔드 개발자' },
+        { type: 'member', id: 559, name: '김다혜', position: 'Android 개발자' },
+        { type: 'member', id: 563, name: '최윤소', position: 'Android 개발자' },
+        { type: 'member', id: 35, name: '이세진', position: 'iOS 개발자' },
+        { type: 'member', id: 72, name: '이승호', position: 'iOS 개발자' },
+      ],
+    },
+    {
+      title: '운영 프로덕트 팀',
+      description:
+        'SOPT 활동 기수 회원과 임원진의 편리한 운영을 위해 공지, 출석 체크 등의 서비스를 만들어요. 전체 회원들이 SOPT의 운영과 행사 진행에 관련된 정보들을 한 곳에서 편하게 확인해요.',
+      people: [
+        { type: 'member', id: 265, name: '김유빈', position: 'PM' },
+        { type: 'member', id: 401, name: '손애진', position: 'PM' },
+        { type: 'member', id: 144, name: '강규태', position: '프로덕트 디자이너' },
+        { type: 'member', id: 112, name: '김소현', position: '서버 개발자' },
+        { type: 'member', id: 205, name: '이용택', position: '서버 개발자' },
+        { type: 'member', id: 251, name: '권소희', position: '웹 프론트엔드 개발자' },
+        { type: 'member', id: 115, name: '김형겸', position: '웹 프론트엔드 개발자' },
+        { type: 'member', id: 283, name: '정화진', position: 'Android 개발자' },
+        { type: 'member', id: 286, name: '김영인', position: 'iOS 개발자' },
+        { type: 'member', id: 22, name: '윤수빈', position: 'iOS 개발자' },
+      ],
+    },
+    {
+      title: '브랜드 커뮤니케이션 팀',
+      description: '',
+      people: [
+        { type: 'member', id: 203, name: '노영선', position: '브랜드 디자이너' },
+        { type: 'member', id: 294, name: '윤여진', position: '브랜드 디자이너' },
+        { type: 'member', id: 370, name: '조성아', position: '프로덕트 마케터' },
+        { type: 'member', id: 204, name: '이지윤', position: '프로덕트 마케터' },
+      ],
+    },
+  ],
+};
--- src/components/makers/data/index.ts
+++ src/components/makers/data/index.ts
@@ -1,6 +1,7 @@
 import { contributors } from '@/components/makers/data/contributors';
 import { generation1 } from '@/components/makers/data/generation1';
 import { generation2 } from '@/components/makers/data/generation2';
+import { generation3 } from '@/components/makers/data/generation3';
 import { MakersGeneration } from '@/components/makers/data/types';
 
-export const makersGenerationsData: MakersGeneration[] = [generation2, generation1, contributors];
+export const makersGenerationsData: MakersGeneration[] = [generation3, generation2, generation1, contributors];
