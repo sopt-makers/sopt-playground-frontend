c12a68545d3b1eae9bf5df4998001462a83052fe
Tekiter <tekiter8@gmail.com>
2023-09-05 15:43:34 +0900

    fix: 엔드포인트 메서드 수정

--- src/api/endpoint/auth/appleChange.ts
+++ src/api/endpoint/auth/appleChange.ts
@@ -4,6 +4,7 @@ import { createEndpoint } from '@/api/typedAxios';
 
 export const appleChangeEndpoint = createEndpoint({
   request: (data: { code: string; registerToken: string }) => ({
+    method: 'POST',
     url: '/api/v1/idp/apple/change',
     data,
   }),
