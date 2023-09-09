692c8b1f45d158cdea6ebd4e84eb1703b0a4d81f
Jun <jun.juno@kakaostyle.com>
2023-09-09 22:32:19 +0900

    chore: test를 위한 세팅 추가

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,7 @@ const customJestConfig: Config = {
     '^@/public/(.*)$': '<rootDir>/public/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
