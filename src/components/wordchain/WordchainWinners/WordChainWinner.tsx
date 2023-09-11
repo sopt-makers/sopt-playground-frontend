33bc2a9f648a699e8357df4fa871e50a7c6a2e9f
Seojisoo20191941 <amy0480@naver.com>
2023-09-11 17:36:52 +0900

    feat: 명예의 전당 상수 파일 삽입

---
+++ src/components/wordchain/WordchainWinners/WordChainWinner.tsx
@@ -0,0 +1,20 @@
+
+export default function WordChainWinner() {
+   const WORDCHAIN_WINNER_DATA={
+    "winners": [
+      {
+        "roomId": 25,
+        "winner": {
+          "id": 1,
+          "profileImage": "https://item.kakaocdn.net/do/cefb8ed496e0541d2a99293a1fa06233616b58f7bf017e58d417ccb3283deeb3",
+          "name": "서지수"
+        }
+      }
+    ],
+    "hasNext": true
+  }
+
+  return (
+    <></>
+  )
+}
