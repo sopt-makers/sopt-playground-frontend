afbb31e78d1881cee6ad8f51871a24a95f3af508
Seojisoo20191941 <amy0480@naver.com>
2023-09-11 17:57:54 +0900

    feat: 우승자 한 칸 스토리북 생성

---
+++ src/components/wordchain/WordchainWinners/WordChainWinner.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta } from '@storybook/react';
+
+import WordChainWinner from '@/components/wordchain/WordchainWinners/WordChainWinner';
+
+export default {
+  component: WordChainWinner,
+} as Meta<typeof WordChainWinner>;
+
+export const Default = {
+  args:  {
+    "roomId": 25,
+    "id": 1,
+    "profileImage": "https://item.kakaocdn.net/do/cefb8ed496e0541d2a99293a1fa06233616b58f7bf017e58d417ccb3283deeb3",
+    "name": "서지수"
+  },
+  name: '기본',
+};
