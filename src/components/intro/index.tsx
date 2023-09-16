c66aa81ba225e47bf743e55ce77378ca983c71f2
Tekiter <tekiter8@gmail.com>
2023-09-17 03:20:31 +0900

    feat: Typer 구현

---
+++ src/components/intro/index.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+import CatchPhraseSection from '@/components/intro/sections/CatchPhraseSection';
+
+interface IntroProps {}
+
+const Intro: FC<IntroProps> = ({}) => {
+  return (
+    <>
+      <CatchPhraseSection />
+    </>
+  );
+};
+
+export default Intro;
---
+++ src/pages/intro/index.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+import Intro from '@/components/intro';
+
+interface IntroPageProps {}
+
+const IntroPage: FC<IntroPageProps> = ({}) => {
+  return (
+    <>
+      <Intro />
+    </>
+  );
+};
+
+export default IntroPage;
