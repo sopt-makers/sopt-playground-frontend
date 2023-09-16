d4dab1c3b0460d520d187d91898248db60d6779d
Seojisoo20191941 <amy0480@naver.com>
2023-09-16 23:36:14 +0900

    refactor: 스토리북 타이핑 수정

--- src/components/wordchain/WordchainWinners/index.stories.tsx
+++ src/components/wordchain/WordchainWinners/index.stories.tsx
@@ -1,59 +1,63 @@
 import { Meta, StoryObj } from '@storybook/react';
+import { Fragment } from 'react';
 
 import WordchainWinners from '@/components/wordchain/WordchainWinners';
+import WordChainWinner from '@/components/wordchain/WordchainWinners/WordChainWinner';
 
-const meta = {
+export default {
   component: WordchainWinners,
-} satisfies Meta<typeof WordchainWinners>;
-export default meta;
+} as Meta;
 
-type Story = StoryObj<typeof meta>;
-
-export const Default = {
-  args: {
-    winners: [
-      {
-        roomId: 25,
-        winner: {
-          id: 1,
-          profileImage: 'https://item.kakaocdn.net/do/22b3b5f6c65114f383f5986c98828993616b58f7bf017e58d417ccb3283deeb3',
-          name: '남주영',
-        },
-      },
-      {
-        roomId: 24,
-        winner: {
-          id: 2,
-          profileImage: 'https://item.kakaocdn.net/do/22b3b5f6c65114f383f5986c98828993616b58f7bf017e58d417ccb3283deeb3',
-          name: '박건영',
-        },
-      },
-      {
-        roomId: 23,
-        winner: {
-          id: 3,
-          profileImage: 'https://item.kakaocdn.net/do/22b3b5f6c65114f383f5986c98828993616b58f7bf017e58d417ccb3283deeb3',
-          name: '서지수',
-        },
-      },
-      {
-        roomId: 22,
-        winner: {
-          id: 4,
-          profileImage: 'https://item.kakaocdn.net/do/22b3b5f6c65114f383f5986c98828993616b58f7bf017e58d417ccb3283deeb3',
-          name: '이준호',
-        },
-      },
-      {
-        roomId: 21,
-        winner: {
-          id: 5,
-          profileImage: 'https://item.kakaocdn.net/do/22b3b5f6c65114f383f5986c98828993616b58f7bf017e58d417ccb3283deeb3',
-          name: '솝트짱',
-        },
-      },
-    ],
-    hasNext: true,
+const winnerList = [
+  {
+    roomId: 25,
+    winner: {
+      id: 1,
+      profileImage: 'https://item.kakaocdn.net/do/22b3b5f6c65114f383f5986c98828993616b58f7bf017e58d417ccb3283deeb3',
+      name: '남주영',
+    },
   },
+  {
+    roomId: 24,
+    winner: {
+      id: 2,
+      profileImage: 'https://item.kakaocdn.net/do/22b3b5f6c65114f383f5986c98828993616b58f7bf017e58d417ccb3283deeb3',
+      name: '박건영',
+    },
+  },
+  {
+    roomId: 23,
+    winner: {
+      id: 3,
+      profileImage: 'https://item.kakaocdn.net/do/22b3b5f6c65114f383f5986c98828993616b58f7bf017e58d417ccb3283deeb3',
+      name: '서지수',
+    },
+  },
+  {
+    roomId: 22,
+    winner: {
+      id: 4,
+      profileImage: 'https://item.kakaocdn.net/do/22b3b5f6c65114f383f5986c98828993616b58f7bf017e58d417ccb3283deeb3',
+      name: '이준호',
+    },
+  },
+  {
+    roomId: 21,
+    winner: {
+      id: 5,
+      profileImage: 'https://item.kakaocdn.net/do/22b3b5f6c65114f383f5986c98828993616b58f7bf017e58d417ccb3283deeb3',
+      name: '솝트짱',
+    },
+  },
+];
+
+export const Default: StoryObj = {
+  render: () => (
+    <Fragment>
+      {winnerList.map(({ roomId, winner: { id, profileImage, name } }) => {
+        return <WordChainWinner key={id} roomId={roomId} profileImage={profileImage} name={name} isRecent={false} />;
+      })}
+    </Fragment>
+  ),
   name: '기본',
-} satisfies Story;
+};
