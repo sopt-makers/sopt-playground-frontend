e648d4b2040291f2f3ac0f24e7033bc756be3145
Jun <jun.juno@kakaostyle.com>
2023-09-21 00:51:35 +0900

    feat: og tag 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
 import GoogleTagManagerNoscript from '@/components/googleTagManager/Noscript';
+import { ORIGIN } from '@/constants/env';
 
 export default function Document() {
   return (
@@ -10,6 +11,9 @@ export default function Document() {
         <link rel='preload' href='/fonts/SUIT-Bold.woff2' as='font' type='font/woff2' crossOrigin='' />
         <link rel='preload' href='/fonts/SUIT-SemiBold.woff2' as='font' type='font/woff2' crossOrigin='' />
         <link rel='preload' href='/fonts/SUIT-Medium.woff2' as='font' type='font/woff2' crossOrigin='' />
+        <meta property='og:title' content='SOPT Playground' />
+        <meta property='og:description' content='솝트와 연결되고 싶으신가요?' />
+        <meta property='og:image' content={`${ORIGIN}/icons/img/og_playground.jpeg`} />
       </Head>
       <body>
         <GoogleTagManagerNoscript />
--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -1,7 +1,9 @@
+import Head from 'next/head';
 import { FC } from 'react';
 
 import AuthRequired from '@/components/auth/AuthRequired';
 import ProjectDetail from '@/components/projects/main/ProjectDetail';
+import { ORIGIN } from '@/constants/env';
 import useStringRouterQuery from '@/hooks/useStringRouterQuery';
 import { setLayout } from '@/utils/layout';
 
@@ -19,6 +21,12 @@ const ProjectPage: FC = () => {
   if (status === 'success') {
     return (
       <AuthRequired>
+        <Head>
+          <title>SOPT | 프로젝트 둘러보기</title>
+          <meta property='og:title' content='SOPT | 프로젝트 둘러보기' />
+          <meta property='og:description' content='자세한 내용이 궁금하신가요?' />
+          <meta property='og:image' content={`${ORIGIN}/icons/img/og_project.jpg`} />
+        </Head>
         <ProjectDetail projectId={query.id} />
       </AuthRequired>
     );
