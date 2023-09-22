f62938e34502c05a74df07c47930762dde01a6c2
Jun <jun.juno@kakaostyle.com>
2023-09-23 04:27:03 +0900

    chore: 환경변수 바꾼코드 롤백

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -19,8 +19,7 @@ export const CHANNEL_TALK_PLUGIN_KEY = required(
 
 function required(key: string, value: string | undefined): string {
   if (value === undefined) {
-    // throw new Error(`환경변수 ${key} 가 설정되지 않았습니다.`);
-    console.log(value);
+    throw new Error(`환경변수 ${key} 가 설정되지 않았습니다.`);
   }
-  return value ?? '';
+  return value;
 }
