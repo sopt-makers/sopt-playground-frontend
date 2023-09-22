49781d56e9f6ffab2240b71c079e56fa07840d50
Tekiter <tekiter8@gmail.com>
2023-09-23 04:52:28 +0900

    feat: 로그인시 인트로 리다이렉트

--- src/pages/intro/index.tsx
+++ src/pages/intro/index.tsx
@@ -1,10 +1,23 @@
-import { FC } from 'react';
+import { useRouter } from 'next/router';
+import { FC, useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
 
+import { accessTokenAtom } from '@/components/auth/states/accessTokenAtom';
 import Intro from '@/components/intro';
+import { playgroundLink } from '@/constants/links';
 
 interface IntroPageProps {}
 
 const IntroPage: FC<IntroPageProps> = ({}) => {
+  const router = useRouter();
+  const accessToken = useRecoilValue(accessTokenAtom);
+
+  useEffect(() => {
+    if (!(router.isReady && accessToken === null)) {
+      router.replace(playgroundLink.memberList());
+    }
+  }, [accessToken, router, router.isReady]);
+
   return (
     <>
       <Intro />
