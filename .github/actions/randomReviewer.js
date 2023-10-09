4ff1d43c438de7de1d334be287ea9ca0c2ae8d27
Jun <jun.juno@kakaostyle.com>
2023-10-09 12:33:22 +0900

    chore: 리뷰 반영 - pr에 리뷰어 있으면 지정하지 않도록 수정

--- .github/actions/randomReviewer.js
+++ .github/actions/randomReviewer.js
@@ -1,4 +1,3 @@
-// TODO: env로 빼기
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
 const prNumber = process.env.PR_NUMBER;
 const token = process.env.GITHUB_TOKEN;
@@ -13,24 +12,47 @@ const headers = {
   'X-GitHub-Api-Version': '2022-11-28',
 };
 
-fetch(`https://api.github.com/repos/${owner}/${repo}/pulls/${prNumber}/requested_reviewers`, {
-  method: 'POST',
-  headers,
-  body: JSON.stringify({
-    owner,
-    repo,
-    pull_number: prNumber,
-    reviewers: [selectedReviewer],
-  }),
-})
-  .then((response) => {
-    if (response != null) {
-      console.log(`${selectedReviewer}를 리뷰어로 선정했어요!`);
-    }
+const getReviewerResponse = () => {
+  return fetch(`https://api.github.com/repos/${owner}/${repo}/pulls/${prNumber}/requested_reviewers`, {
+    method: 'GET',
+    headers,
   })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('기존 PR의 reviewer 읽어오는데 실패했어요.');
+      }
+    })
+    .then((json) => {
+      if (json.users.length > 0) {
+        throw new Error('이미 PR에 리뷰어가 선정되어 있어요.');
+      }
+      return null;
+    });
+};
+
+const setReviewerResponse = () => {
+  return fetch(`https://api.github.com/repos/${owner}/${repo}/pulls/${prNumber}/requested_reviewers`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      owner,
+      repo,
+      pull_number: prNumber,
+      reviewers: [selectedReviewer],
+    }),
+  }).then((response) => {
+    if (response.ok) {
+      console.log(`${selectedReviewer}를 리뷰어로 선정했어요!`);
+    } else {
+      throw new Error('Failed to set reviewer');
+    }
+  });
+};
+
+getReviewerResponse()
+  .then(setReviewerResponse)
   .catch((error) => {
-    console.error(
-      '리뷰어 지정 과정에서 오류가 발생했어요.\n',
-      (error.response && error.response.data) || error.message,
-    );
+    console.error('리뷰어 지정 과정에서 오류가 발생했어요.\n', error.message);
   });
