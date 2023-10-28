d4cacb2b6c5c07fd1aaf1e67f6aa98b3473e7e79
Tekiter <tekiter8@gmail.com>
2023-10-29 03:36:05 +0900

    feat: 스크롤 영역 변경

--- src/components/community/list/FeedList.tsx
+++ src/components/community/list/FeedList.tsx
@@ -3,44 +3,47 @@ import { FC } from 'react';
 
 import CategorySelect from '@/components/community/list/CategorySelect';
 import { FeedDetailLink } from '@/components/community/queryParam';
+import { colors } from '@sopt-makers/colors';
 
 interface FeedListProps {}
 
 const FeedList: FC<FeedListProps> = ({}) => {
   return (
     <Container>
-      <CategorySelect
-        categories={[
-          {
-            id: 'part',
-            name: '파트',
-            hasAllCategory: false,
-            tags: [
-              { id: 'PM', name: '기획' },
-              { id: 'WEB', name: '웹' },
-              { id: 'SERVER', name: '서버' },
-            ],
-          },
-          {
-            id: 'sopt',
-            name: 'SOPT활동',
-            hasAllCategory: true,
-            tags: [
-              { id: 'SOPK', name: '솝커톤' },
-              { id: 'APPJAM', name: '앱잼' },
-            ],
-          },
-        ]}
-      />
-      <ListArea>
-        <ListAreaInner>
+      <ContainerInner>
+        <CategoryArea>
+          <CategorySelect
+            categories={[
+              {
+                id: 'part',
+                name: '파트',
+                hasAllCategory: false,
+                tags: [
+                  { id: 'PM', name: '기획' },
+                  { id: 'WEB', name: '웹' },
+                  { id: 'SERVER', name: '서버' },
+                ],
+              },
+              {
+                id: 'sopt',
+                name: 'SOPT활동',
+                hasAllCategory: true,
+                tags: [
+                  { id: 'SOPK', name: '솝커톤' },
+                  { id: 'APPJAM', name: '앱잼' },
+                ],
+              },
+            ]}
+          />
+        </CategoryArea>
+        <div>
           {[...new Array(100)].map((_, idx) => (
             <div key={idx} style={{ height: '80px' }}>
               <FeedDetailLink feedId={`feed${idx}`}>Feed {idx}</FeedDetailLink>
             </div>
           ))}
-        </ListAreaInner>
-      </ListArea>
+        </div>
+      </ContainerInner>
     </Container>
   );
 };
@@ -49,18 +52,20 @@ export default FeedList;
 
 const Container = styled.div`
   display: flex;
+  position: relative;
   flex-direction: column;
   height: 100%;
 `;
 
-const ListArea = styled.div`
-  position: relative;
-  flex: 1 1 0;
-`;
-
-const ListAreaInner = styled.div`
+const ContainerInner = styled.div`
   position: absolute;
   inset: 0;
   overflow-x: hidden;
   overflow-y: scroll;
+`;
+
+const CategoryArea = styled.div`
+  position: sticky;
+  top: 0;
+  background-color: ${colors.background};
 `;
