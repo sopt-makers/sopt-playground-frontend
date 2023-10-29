eedf765512ace7e17a16282d5faf8bea92198058
Seojisoo20191941 <amy0480@naver.com>
2023-10-29 16:56:32 +0900

    chore: 드롭다운 상태명 변경

--- src/components/feed/upload/CategorySelector/index.tsx
+++ src/components/feed/upload/CategorySelector/index.tsx
@@ -5,19 +5,19 @@ import CategoryHeader from '@/components/feed/upload/CategorySelector/CategoryHe
 import TagDropDown from '@/components/feed/upload/CategorySelector/TagDropDown';
 
 export default function CategorySelector() {
-  const [isDropDown, setIsDropDown] = useState<'mainOpen' | 'subOpen' | 'allClosed'>('mainOpen');
+  const [isDropDown, setIsDropDown] = useState<'categoryOpen' | 'tagOpen' | 'allClosed'>('categoryOpen');
 
   return (
     <>
       <CategoryDropDown
-        isOpen={isDropDown === 'mainOpen'}
-        onNext={() => setIsDropDown('subOpen')}
+        isOpen={isDropDown === 'categoryOpen'}
+        onNext={() => setIsDropDown('tagOpen')}
         onClose={() => setIsDropDown('allClosed')}
       />
       {/* 메인 카테고리가 '자유'가 아닌 경우에만 뜨도록 */}
       <TagDropDown
-        isOpen={isDropDown === 'subOpen'}
-        onBack={() => setIsDropDown('mainOpen')}
+        isOpen={isDropDown === 'tagOpen'}
+        onBack={() => setIsDropDown('categoryOpen')}
         onClose={() => setIsDropDown('allClosed')}
       />
       <CategoryHeader />
