abd02c92451e3ed8b475086ec50b828340df82b6
Seojisoo20191941 <amy0480@naver.com>
2023-10-30 21:12:21 +0900

    chore: constants 주석 풀기

--- src/components/feed/upload/CategorySelector/constants.ts
+++ src/components/feed/upload/CategorySelector/constants.ts
@@ -1,21 +1,21 @@
 import { CategorySelectorType } from '@/components/feed/upload/CategorySelector/types';
 
 export const CATEGORY_OPTIONS: CategorySelectorType[] = [
-  // {
-  //   category: '파트',
-  //   content: '기획, 디자인, 서버, 웹, iOS, Android에 대한 이야기를 해요.',
-  //   tags: ['기획', '디자인', '서버', '웹', 'iOS', 'Android'],
-  // },
-  // {
-  //   category: 'SOPT 활동',
-  //   content: '앱잼, 솝커톤, 솝텀을 포함해 다양한 SOPT에 대한 이야기를 해요.',
-  //   tags: ['자유', '앱잼', '솝커톤', '솝텀'],
-  // },
-  // { category: '취업/진로', content: '취업, 진로에 대한 다양한 이야기를 해요.', tags: ['자유', '후기', '꿀팁'] },
-  // {
-  //   category: '홍보',
-  //   content: '채용, 행사, 스터디, 설문조사 등 모든 홍보는 여기서 이야기 해요.',
-  //   tags: ['자유', '모집', '채용', '프로젝트', '행사'],
-  // },
-  // { category: '자유', content: '자유롭게 다양한 이야기를 해요.', tags: [] },
+  {
+    category: '파트',
+    content: '기획, 디자인, 서버, 웹, iOS, Android에 대한 이야기를 해요.',
+    tags: ['기획', '디자인', '서버', '웹', 'iOS', 'Android'],
+  },
+  {
+    category: 'SOPT 활동',
+    content: '앱잼, 솝커톤, 솝텀을 포함해 다양한 SOPT에 대한 이야기를 해요.',
+    tags: ['자유', '앱잼', '솝커톤', '솝텀'],
+  },
+  { category: '취업/진로', content: '취업, 진로에 대한 다양한 이야기를 해요.', tags: ['자유', '후기', '꿀팁'] },
+  {
+    category: '홍보',
+    content: '채용, 행사, 스터디, 설문조사 등 모든 홍보는 여기서 이야기 해요.',
+    tags: ['자유', '모집', '채용', '프로젝트', '행사'],
+  },
+  { category: '자유', content: '자유롭게 다양한 이야기를 해요.', tags: [] },
 ];
