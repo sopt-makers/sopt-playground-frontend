62ef84fac0dca2605cdae1e1a9178b1f8d84cab3
Tekiter <tekiter8@gmail.com>
2023-11-09 22:14:56 +0900

    feat: 모달 재구현 (#1056)
    * feat: 모달 재구현
    
    * feat: 애니메이션 추가
    
    * feat: 리뷰 반영

---
+++ src/components/common/Modal/parts/ModalButton.tsx
@@ -0,0 +1,31 @@
+import styled from '@emotion/styled';
+import * as Dialog from '@radix-ui/react-dialog';
+import { colors } from '@sopt-makers/colors';
+import { ComponentPropsWithoutRef, forwardRef } from 'react';
+
+import { textStyles } from '@/styles/typography';
+
+interface ModalCloseButtonProps extends ComponentPropsWithoutRef<typeof Dialog.Close> {
+  action?: 'normal' | 'close';
+}
+
+const ModalButton = forwardRef<HTMLButtonElement, ModalCloseButtonProps>(({ action = 'normal', ...props }, ref) => {
+  const As = action === 'normal' ? 'button' : Dialog.Close;
+
+  return <StyledButton ref={ref} as={As} {...props}></StyledButton>;
+});
+
+export default ModalButton;
+
+const StyledButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  background-color: ${colors.gray700};
+  padding: 12px 20px;
+  height: 48px;
+  color: ${colors.gray10};
+
+  ${textStyles.SUIT_16_SB};
+`;
