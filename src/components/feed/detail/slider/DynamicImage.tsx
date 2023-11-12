d9ad4dff4f596ecc9ebec654b3860d97bce832aa
Jun <jun.juno@kakaostyle.com>
2023-11-12 22:38:06 +0900

    feat: 리뷰 반영

--- src/components/feed/detail/slider/DynamicImage.tsx
+++ src/components/feed/detail/slider/DynamicImage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { forwardRef, useEffect, useState } from 'react';
 
 interface ImageSize {
   width: number | 'auto';
@@ -6,6 +6,7 @@ interface ImageSize {
 }
 
 interface DynamicImageProps {
+  className?: string;
   src: string;
   alt?: string;
   targetSize?: number;
@@ -15,39 +16,42 @@ interface DynamicImageProps {
  * @param targetSize width와 height 중 더 큰 값을 targetSize로 맞추고, 나머지 하나는 비율에 맞게 조정하여 렌더링 하는 컴포넌트
  * @returns
  */
-export const DynamicImage = ({ src, alt, targetSize = 600 }: DynamicImageProps) => {
-  const [imageSize, setImageSize] = useState<ImageSize>({
-    width: 0,
-    height: 0,
-  });
+export const DynamicImage = forwardRef<HTMLImageElement, DynamicImageProps>(
+  ({ className, src, alt, targetSize = 600 }) => {
+    const [imageSize, setImageSize] = useState<ImageSize>({
+      width: 0,
+      height: 0,
+    });
 
-  useEffect(() => {
-    const img = new Image();
-    img.src = src;
+    useEffect(() => {
+      const img = new Image();
+      img.src = src;
 
-    img.onload = () => {
-      const originalWidth = img.width;
-      const originalHeight = img.height;
+      img.onload = () => {
+        const originalWidth = img.width;
+        const originalHeight = img.height;
 
-      if (originalWidth > originalHeight) {
-        setImageSize({ width: targetSize, height: 'auto' });
-      } else {
-        setImageSize({ width: 'auto', height: targetSize });
-      }
-    };
-  }, [src, targetSize]);
+        if (originalWidth > originalHeight) {
+          setImageSize({ width: targetSize, height: 'auto' });
+        } else {
+          setImageSize({ width: 'auto', height: targetSize });
+        }
+      };
+    }, [src, targetSize]);
 
-  return (
-    <img
-      css={{
-        objectFit: 'cover',
-        width: imageSize.width,
-        height: imageSize.height,
-      }}
-      alt={alt}
-      src={src}
-      width={imageSize.width}
-      height={imageSize.height}
-    />
-  );
-};
+    return (
+      <img
+        className={className}
+        css={{
+          objectFit: 'cover',
+          width: imageSize.width,
+          height: imageSize.height,
+        }}
+        alt={alt}
+        src={src}
+        width={imageSize.width}
+        height={imageSize.height}
+      />
+    );
+  },
+);
