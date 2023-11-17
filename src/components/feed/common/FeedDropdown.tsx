2023a740b2e96acfae071ff66cba2a1378dcdeb4
Jun <jun.juno@kakaostyle.com>
2023-11-17 10:09:45 +0900

    feat: radix로 드롭다운 구현

---
+++ src/components/feed/common/FeedDropdown.tsx
@@ -0,0 +1,58 @@
+import styled from '@emotion/styled';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import dynamic from 'next/dynamic';
+import React, { PropsWithChildren, ReactNode } from 'react';
+
+const DropdownPortal = dynamic(() => import('@radix-ui/react-dropdown-menu').then((mod) => mod.Portal), {
+  ssr: false,
+});
+
+interface FeedDropdownProps {
+  trigger?: ReactNode;
+}
+
+const Base = ({ trigger, children }: PropsWithChildren<FeedDropdownProps>) => {
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>{trigger}</DropdownMenu.Trigger>
+      <DropdownPortal>
+        <StyledContent>{children}</StyledContent>
+      </DropdownPortal>
+    </DropdownMenu.Root>
+  );
+};
+
+const StyledContent = styled(DropdownMenu.Content)`
+  border-radius: 12px;
+  background-color: #252525;
+  width: 208px;
+`;
+
+interface ItemProps {
+  type?: 'default' | 'danger';
+}
+const Item = ({ type = 'default', children }: PropsWithChildren<ItemProps>) => {
+  return (
+    <StyledItem
+      css={{
+        color: type === 'danger' ? '#FF453A' : '#FFFFFF',
+      }}
+    >
+      {children}
+    </StyledItem>
+  );
+};
+
+const StyledItem = styled(DropdownMenu.Item)`
+  cursor: pointer;
+  padding: 16px;
+
+  :hover {
+    outline: none;
+  }
+`;
+
+export default Object.assign(Base, {
+  Item,
+  Seperator: DropdownMenu.Separator,
+});
