9f14c9162f474412cbda0e4120b15aa47c57fdc8
Tekiter <tekiter8@gmail.com>
2023-11-19 03:00:19 +0900

    feat: 스크롤 정책 수정

--- src/components/feed/list/FeedList.tsx
+++ src/components/feed/list/FeedList.tsx
@@ -10,6 +10,7 @@ import { FeedDetailLink } from '@/components/feed/common/queryParam';
 import { getMemberInfo } from '@/components/feed/common/utils';
 import CategorySelect from '@/components/feed/list/CategorySelect';
 import FeedCard from '@/components/feed/list/FeedCard';
+import { layoutCSSVariable } from '@/components/layout/utils';
 
 interface FeedListProps {}
 
@@ -32,52 +33,50 @@ const FeedList: FC<FeedListProps> = ({}) => {
 
   return (
     <Container>
-      <ContainerInner>
-        <CategoryArea>{categories && <CategorySelect categories={categories} />}</CategoryArea>
-        <Virtuoso
-          data={data?.pages.flatMap((page) => page.posts)}
-          useWindowScroll
-          endReached={() => {
-            fetchNextPage();
-          }}
-          itemContent={(_, post) => {
-            return (
-              <FeedDetailLink css={{ width: '100%' }} feedId={`${post.id}`}>
-                <FeedCard
-                  name={post.member.name}
-                  title={post.title}
-                  content={post.content}
-                  profileImage={post.member.profileImage}
-                  createdAt={post.createdAt}
-                  commentLength={post.commentCount}
-                  hits={post.hits}
-                  isBlindWriter={post.isBlindWriter}
-                  isQuestion={post.isQuestion}
-                  info={getMemberInfo({
-                    categoryId: post.categoryId,
-                    categoryName: post.categoryName,
-                    member: {
-                      activity: post.member.activity,
-                      careers: post.member.careers,
-                    },
-                  })}
-                >
-                  <FeedCard.Image>
-                    {post.images.map((image, index) => (
-                      <FeedCard.ImageItem key={`${image}-${index}`} src={image} />
-                    ))}
-                  </FeedCard.Image>
-                  <FeedCard.Comment>
-                    {post.comments.map((comment) => (
-                      <FeedCard.CommentItem key={comment.id} comment={comment.content} name={comment.member.name} />
-                    ))}
-                  </FeedCard.Comment>
-                </FeedCard>
-              </FeedDetailLink>
-            );
-          }}
-        />
-      </ContainerInner>
+      <CategoryArea>{categories && <CategorySelect categories={categories} />}</CategoryArea>
+      <Virtuoso
+        data={data?.pages.flatMap((page) => page.posts)}
+        useWindowScroll
+        endReached={() => {
+          fetchNextPage();
+        }}
+        itemContent={(_, post) => {
+          return (
+            <FeedDetailLink css={{ width: '100%' }} feedId={`${post.id}`}>
+              <FeedCard
+                name={post.member.name}
+                title={post.title}
+                content={post.content}
+                profileImage={post.member.profileImage}
+                createdAt={post.createdAt}
+                commentLength={post.commentCount}
+                hits={post.hits}
+                isBlindWriter={post.isBlindWriter}
+                isQuestion={post.isQuestion}
+                info={getMemberInfo({
+                  categoryId: post.categoryId,
+                  categoryName: post.categoryName,
+                  member: {
+                    activity: post.member.activity,
+                    careers: post.member.careers,
+                  },
+                })}
+              >
+                <FeedCard.Image>
+                  {post.images.map((image, index) => (
+                    <FeedCard.ImageItem key={`${image}-${index}`} src={image} />
+                  ))}
+                </FeedCard.Image>
+                <FeedCard.Comment>
+                  {post.comments.map((comment) => (
+                    <FeedCard.CommentItem key={comment.id} comment={comment.content} name={comment.member.name} />
+                  ))}
+                </FeedCard.Comment>
+              </FeedCard>
+            </FeedDetailLink>
+          );
+        }}
+      />
     </Container>
   );
 };
@@ -88,18 +87,11 @@ const Container = styled.div`
   display: flex;
   position: relative;
   flex-direction: column;
-  height: 100%;
-`;
-
-const ContainerInner = styled.div`
-  position: absolute;
-  inset: 0;
-  overflow-x: hidden;
-  overflow-y: scroll;
 `;
 
 const CategoryArea = styled.div`
   position: sticky;
-  top: 0;
+  top: ${layoutCSSVariable.globalHeaderHeight};
+  z-index: 1; /* Virtuoso가 sticky 위에 와버리는 문제때문에 z-index로 제어 */
   background-color: ${colors.background};
 `;
