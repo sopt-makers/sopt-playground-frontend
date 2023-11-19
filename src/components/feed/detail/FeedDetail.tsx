c82ac660bdc193110ec6916f03ce9c42014431bb
Jun <jun.juno@kakaostyle.com>
2023-11-19 18:27:09 +0900

    feat: 신고하기 기능 주석 추가

--- src/components/feed/detail/FeedDetail.tsx
+++ src/components/feed/detail/FeedDetail.tsx
@@ -11,6 +11,7 @@ import { useGetPostQuery } from '@/api/endpoint/feed/getPost';
 import { getPosts } from '@/api/endpoint/feed/getPosts';
 import { usePostCommentMutation } from '@/api/endpoint/feed/postComment';
 import { useGetMemberOfMe } from '@/api/endpoint/members/getMemberOfMe';
+import useAlert from '@/components/common/Modal/useAlert';
 import useConfirm from '@/components/common/Modal/useConfirm';
 import useToast from '@/components/common/Toast/useToast';
 import FeedDropdown from '@/components/feed/common/FeedDropdown';
@@ -29,6 +30,7 @@ const FeedDetail = ({ postId }: FeedDetailProps) => {
   const { mutate: postComment } = usePostCommentMutation(postId);
   const { mutate: deletePost } = useDeletePostMutation();
   const { mutate: deleteComment } = useDeleteCommentMutation();
+  const { alert } = useAlert();
   const { confirm } = useConfirm();
   const { copy } = useCopyText();
   const toast = useToast();
@@ -97,6 +99,25 @@ const FeedDetail = ({ postId }: FeedDetailProps) => {
     }
   };
 
+  const handleReport = async () => {
+    const result = await confirm({
+      title: '이 글을 신고하시겠습니까?',
+      description: '글을 신고할 경우, 메이커스에서 검토를 거쳐 적절한 조치 및 게시자 제재를 취해요.',
+      okButtonText: '신고하기',
+      cancelButtonText: '취소',
+    });
+
+    if (result) {
+      // TODO: 신고하기 api 작성되면 추가
+      // onSuccess로 이동
+      alert({
+        title: '신고해주셔서 감사해요.',
+        description:
+          '메이커스에서 빠르게 검토 후 적절한 조치를 취할게요 :) 건전한 커뮤니티를 만드는데 기여해주셔서 감사해요!',
+      });
+    }
+  };
+
   const handleDeleteComment = async (commentId: string) => {
     const result = await confirm({
       title: '댓글을 정말 삭제하시겠어요?',
@@ -148,7 +169,9 @@ const FeedDetail = ({ postId }: FeedDetailProps) => {
                   삭제
                 </FeedDropdown.Item>
               ) : null}
-              <FeedDropdown.Item type='danger'>신고</FeedDropdown.Item>
+              <FeedDropdown.Item type='danger' onClick={handleReport}>
+                신고
+              </FeedDropdown.Item>
             </FeedDropdown>
           </>
         }
