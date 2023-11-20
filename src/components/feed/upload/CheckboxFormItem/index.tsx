2d10f2a2d1dffa836b97e628d890edee0524acf8
남주영 (Finn) <njy1007@gmail.com>
2023-11-21 01:10:26 +0900

    feat: 커뮤니티 업로드 > 질문글/익명 체크박스, 코드 버튼 추가 (#1106)
    * feat: 코드 컴포넌트 추가
    
    * feat: 질문글, 익명 체크박스 구현

---
+++ src/components/feed/upload/CheckboxFormItem/index.tsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+import { colors } from '@sopt-makers/colors';
+import { ReactNode } from 'react';
+
+import { textStyles } from '@/styles/typography';
+
+interface CheckboxFormItemProps {
+  label: string;
+  children: ReactNode;
+}
+
+export default function CheckboxFormItem({ children, label }: CheckboxFormItemProps) {
+  return (
+    <CheckboxWrapper>
+      {children}
+      <Label>{label}</Label>
+    </CheckboxWrapper>
+  );
+}
+
+const CheckboxWrapper = styled.div`
+  display: flex;
+  gap: 4px;
+  align-items: center;
+`;
+
+const Label = styled.div`
+  line-height: 20px;
+  letter-spacing: -0.14px;
+  color: ${colors.gray10};
+
+  ${textStyles.SUIT_14_M}
+`;
