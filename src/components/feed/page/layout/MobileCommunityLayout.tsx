21989b5bb72c952ab0998c416cb680ee8f7f4257
Tekiter <tekiter8@gmail.com>
2023-11-21 00:52:17 +0900

    feat: 모바일 피드 상세 정책 변경 (#1110)

--- src/components/feed/page/layout/MobileCommunityLayout.tsx
+++ src/components/feed/page/layout/MobileCommunityLayout.tsx
@@ -1,23 +1,14 @@
 import styled from '@emotion/styled';
-import { colors } from '@sopt-makers/colors';
-import { m } from 'framer-motion';
 import { FC, ReactNode } from 'react';
-import { RemoveScroll } from 'react-remove-scroll';
+
 interface MobileCommunityLayoutProps {
-  isDetailOpen: boolean;
   listSlot: ReactNode;
-  detailSlot: ReactNode;
 }
 
-const MobileCommunityLayout: FC<MobileCommunityLayoutProps> = ({ isDetailOpen, listSlot, detailSlot }) => {
+const MobileCommunityLayout: FC<MobileCommunityLayoutProps> = ({ listSlot }) => {
   return (
     <Container>
       <ListSlotBox>{listSlot}</ListSlotBox>
-      <FullLayer>
-        <DetailSlotBox initial={{ x: '100%' }} animate={{ x: isDetailOpen ? '0%' : '100%' }} transition={{ bounce: 0 }}>
-          {isDetailOpen ? <RemoveScroll>{detailSlot}</RemoveScroll> : null}
-        </DetailSlotBox>
-      </FullLayer>
     </Container>
   );
 };
@@ -30,17 +21,4 @@ const Container = styled.div`
 
 const ListSlotBox = styled.div`
   position: relative;
-`;
-
-const FullLayer = styled.div`
-  position: fixed;
-  inset: 0;
-  z-index: 9999;
-  overflow: hidden;
-`;
-
-const DetailSlotBox = styled(m.div)`
-  background-color: ${colors.background};
-  width: 100%;
-  height: 100%;
 `;
