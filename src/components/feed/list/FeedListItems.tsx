ca9127fae6db637c5471b73dd0b8fca489c17d11
Tekiter <tekiter8@gmail.com>
2023-11-25 14:40:18 +0900

    feat: 리스트 에러 바운더리 추가

---
+++ src/components/feed/list/FeedListItems.tsx
@@ -0,0 +1,141 @@
+import { FC, ReactNode } from 'react';
+import { Virtuoso } from 'react-virtuoso';
+
+import { useGetPostsInfiniteQuery } from '@/api/endpoint/feed/getPosts';
+import Loading from '@/components/common/Loading';
+import FeedDropdown from '@/components/feed/common/FeedDropdown';
+import { useDeleteFeed } from '@/components/feed/common/hooks/useDeleteFeed';
+import { useReportFeed } from '@/components/feed/common/hooks/useReportFeed';
+import { useShareFeed } from '@/components/feed/common/hooks/useShareFeed';
+import { getMemberInfo } from '@/components/feed/common/utils';
+import FeedCard from '@/components/feed/list/FeedCard';
+
+interface FeedListItemsProps {
+  categoryId: string | undefined;
+  renderFeedDetailLink: (props: { children: ReactNode; feedId: string }) => ReactNode;
+}
+
+const FeedListItems: FC<FeedListItemsProps> = ({ categoryId, renderFeedDetailLink }) => {
+  const { data, refetch, fetchNextPage, isLoading, isError } = useGetPostsInfiniteQuery({
+    categoryId,
+  });
+
+  const { handleShareFeed } = useShareFeed();
+  const { handleDeleteFeed } = useDeleteFeed();
+  const { handleReport } = useReportFeed();
+
+  const flattenData = data?.pages.flatMap((page) => page.posts) ?? [];
+
+  return (
+    <>
+      <Virtuoso
+        data={flattenData}
+        useWindowScroll
+        endReached={() => {
+          fetchNextPage();
+        }}
+        itemContent={(_, post) => {
+          return renderFeedDetailLink({
+            feedId: `${post.id}`,
+            children: (
+              <FeedCard
+                name={post.member?.name ?? '익명'}
+                title={post.title}
+                content={post.content}
+                profileImage={post.member?.profileImage ?? null}
+                createdAt={post.createdAt}
+                commentLength={post.commentCount}
+                hits={post.hits}
+                isBlindWriter={post.isBlindWriter}
+                isQuestion={post.isQuestion}
+                info={getMemberInfo({
+                  categoryId: post.categoryId,
+                  categoryName: post.categoryName,
+                  member: {
+                    activity: post.member?.activity ?? { generation: 0, part: '' },
+                    careers: post.member?.careers ?? null,
+                  },
+                })}
+                rightIcon={
+                  <FeedDropdown
+                    trigger={
+                      <button>
+                        <FeedCard.Icon name='moreHorizon' />
+                      </button>
+                    }
+                  >
+                    {post.isMine ? <FeedDropdown.Item>수정</FeedDropdown.Item> : null}
+                    <FeedDropdown.Item
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleShareFeed(`${post.id}`);
+                      }}
+                    >
+                      공유
+                    </FeedDropdown.Item>
+                    {post.isMine ? (
+                      <FeedDropdown.Item
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handleDeleteFeed({
+                            postId: `${post.id}`,
+                            onSuccess: () => {
+                              refetch();
+                            },
+                          });
+                        }}
+                        type='danger'
+                      >
+                        삭제
+                      </FeedDropdown.Item>
+                    ) : null}
+                    <FeedDropdown.Item
+                      type='danger'
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleReport({ postId: `${post.id}` });
+                      }}
+                    >
+                      신고
+                    </FeedDropdown.Item>
+                  </FeedDropdown>
+                }
+              >
+                <FeedCard.Image>
+                  {post.images.map((image, index) => (
+                    <FeedCard.ImageItem key={`${image}-${index}`} src={image} />
+                  ))}
+                </FeedCard.Image>
+                <FeedCard.Comment>
+                  {post.comments.map((comment) =>
+                    comment.isBlindWriter ? (
+                      <FeedCard.CommentItem
+                        key={comment.id}
+                        comment={comment.content}
+                        isBlindWriter={comment.isBlindWriter}
+                      />
+                    ) : comment.member ? (
+                      <FeedCard.CommentItem
+                        key={comment.id}
+                        comment={comment.content}
+                        isBlindWriter={comment.isBlindWriter}
+                        name={comment.member.name}
+                      />
+                    ) : null,
+                  )}
+                </FeedCard.Comment>
+              </FeedCard>
+            ),
+          });
+        }}
+      />
+      <div css={{ display: 'flex', justifyContent: 'center', padding: '30px 0' }}>
+        {isError ? <div>오류가 발생했어요.</div> : null}
+        {data != null && flattenData.length === 0 ? <div>글이 없어요!</div> : null}
+        {isLoading ? <Loading /> : null}
+      </div>
+    </>
+  );
+};
+
+export default FeedListItems;
