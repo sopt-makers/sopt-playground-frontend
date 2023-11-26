be043ba2b0fee25e084c483437a85b1ee6ed3ae4
Tekiter <tekiter8@gmail.com>
2023-11-26 19:33:44 +0900

    chore: 주석 추가

--- src/components/common/HorizontalScroller/index.tsx
+++ src/components/common/HorizontalScroller/index.tsx
@@ -5,82 +5,94 @@ import { forwardRef, ReactNode, useEffect, useRef, useState } from 'react';
 interface HorizontalScrollerProps {
   className?: string;
   children: ReactNode;
+  leftButton?: ReactNode;
+  rightButton?: ReactNode;
 }
 
-const HorizontalScroller = forwardRef<HTMLDivElement, HorizontalScrollerProps>(({ className, children }, ref) => {
-  const outerRef = useRef<HTMLDivElement>(null);
+/**
+ * 가로 스크롤바 없이 가로 스크롤을 편리하게 사용할 수 있게 해주는 컴포넌트입니다.
+ *
+ * - 클릭하면 화면에 보이는 크기의 2/3 만큼씩 이동합니다.
+ * - 왼쪽 끝이나 오른쪽 끝에 도달하면 화살표 사라집니다.
+ * - 마우스를 사용하면서 컨테이너 호버시에만 버튼이 노출됩니다.
+ * - 스크롤바는 감춰지되, 터치 or 가로 스크롤 가능한 디바이스로는 스크롤 가능합니다.
+ */
+const HorizontalScroller = forwardRef<HTMLDivElement, HorizontalScrollerProps>(
+  ({ className, children, leftButton, rightButton }, ref) => {
+    const outerRef = useRef<HTMLDivElement>(null);
 
-  const [isLeftMovable, setIsLeftMovable] = useState(false);
-  const [isRightMovable, setIsRightMovable] = useState(false);
+    const [isLeftMovable, setIsLeftMovable] = useState(false);
+    const [isRightMovable, setIsRightMovable] = useState(false);
 
-  useEffect(() => {
-    const outerDiv = outerRef.current;
-    if (outerDiv == null) {
-      return;
+    useEffect(() => {
+      const outerDiv = outerRef.current;
+      if (outerDiv == null) {
+        return;
+      }
+
+      const calculateScrollable = () => {
+        const left = outerDiv.scrollLeft;
+        const scrollWidth = outerDiv.scrollWidth;
+        const displayWidth = outerDiv.clientWidth;
+
+        setIsLeftMovable(left !== 0);
+        setIsRightMovable(scrollWidth - displayWidth !== left);
+      };
+
+      const observer = new ResizeObserver(([entry]) => {
+        if (entry) {
+          calculateScrollable();
+        }
+      });
+
+      calculateScrollable();
+
+      outerDiv.addEventListener('scroll', calculateScrollable);
+      observer.observe(outerDiv);
+
+      return () => {
+        outerDiv.removeEventListener('scroll', calculateScrollable);
+        observer.disconnect();
+      };
+    }, []);
+
+    function scrollLeft() {
+      const scrollWidth = outerRef?.current?.clientWidth ?? 0;
+
+      outerRef.current?.scrollBy({
+        left: -((scrollWidth * 2) / 3),
+        behavior: 'smooth',
+      });
     }
 
-    const calculateScrollable = () => {
-      const left = outerDiv.scrollLeft;
-      const scrollWidth = outerDiv.scrollWidth;
-      const displayWidth = outerDiv.clientWidth;
+    function scrollRight() {
+      const scrollWidth = outerRef?.current?.clientWidth ?? 0;
 
-      setIsLeftMovable(left !== 0);
-      setIsRightMovable(scrollWidth - displayWidth !== left);
-    };
+      outerRef.current?.scrollBy({
+        left: (scrollWidth * 2) / 3,
+        behavior: 'smooth',
+      });
+    }
 
-    const observer = new ResizeObserver(([entry]) => {
-      if (entry) {
-        calculateScrollable();
-      }
-    });
-
-    calculateScrollable();
-
-    outerDiv.addEventListener('scroll', calculateScrollable);
-    observer.observe(outerDiv);
-
-    return () => {
-      outerDiv.removeEventListener('scroll', calculateScrollable);
-      observer.disconnect();
-    };
-  }, []);
-
-  function scrollLeft() {
-    const scrollWidth = outerRef?.current?.clientWidth ?? 0;
-
-    outerRef.current?.scrollBy({
-      left: -((scrollWidth * 2) / 3),
-      behavior: 'smooth',
-    });
-  }
-
-  function scrollRight() {
-    const scrollWidth = outerRef?.current?.clientWidth ?? 0;
-
-    outerRef.current?.scrollBy({
-      left: (scrollWidth * 2) / 3,
-      behavior: 'smooth',
-    });
-  }
-
-  return (
-    <Container className={className} ref={ref}>
-      <ScrollContainer ref={outerRef}>{children}</ScrollContainer>
-      {isLeftMovable && (
-        <LeftScrollSlot>
-          <ScrollButton onClick={scrollLeft}>{backIcon}</ScrollButton>
-        </LeftScrollSlot>
-      )}
-      {isRightMovable && (
-        <RightScrollSlot>
-          <ScrollButton onClick={scrollRight} css={{ transform: 'rotate(180deg)' }}>
-            {backIcon}
-          </ScrollButton>
-        </RightScrollSlot>
-      )}
-    </Container>
-  );
-});
+    return (
+      <Container className={className} ref={ref}>
+        <ScrollContainer ref={outerRef}>{children}</ScrollContainer>
+        {isLeftMovable && (
+          <LeftScrollSlot>{leftButton ?? <ScrollButton onClick={scrollLeft}>{backIcon}</ScrollButton>}</LeftScrollSlot>
+        )}
+        {isRightMovable && (
+          <RightScrollSlot>
+            {rightButton ?? (
+              <ScrollButton onClick={scrollRight} css={{ transform: 'rotate(180deg)' }}>
+                {backIcon}
+              </ScrollButton>
+            )}
+          </RightScrollSlot>
+        )}
+      </Container>
+    );
+  },
+);
 
 export default HorizontalScroller;
 
