abc627a08e25dc001699e8a2833e23b323996e77
이정연 <appdler96@gmail.com>
2023-11-26 23:08:04 +0900

    feat: SUIT-Regular, Light 추가

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -7,10 +7,12 @@ export const baseTextStyles = css`
 export const textStyles = (() => {
   const sizes = [10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 22, 24, 26, 28, 30, 32, 40, 60] as const;
   const weights = [
+    ['L', 300],
     ['R', 400],
     ['M', 500],
     ['SB', 600],
     ['B', 700],
+    ['EB', 800],
   ] as const;
 
   type FontKey = `SUIT_${(typeof sizes)[number]}_${(typeof weights)[number][0]}`;
