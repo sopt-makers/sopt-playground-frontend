6e517373b46474c21a6fec3059d96ad6a54ada48
NamJwong <finn@lemonbase.com>
2023-11-30 21:48:58 +0900

    chore: 의미 없는 코드 이동 되돌리기

--- src/components/feed/detail/slider/FeedImageSlider.tsx
+++ src/components/feed/detail/slider/FeedImageSlider.tsx
@@ -38,6 +38,9 @@ const FeedImageSlider = ({ images, opened, onClose }: FeedImageSliderProps) => {
           <RemoveScroll>
             <Background initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
               {images.length > 1 ? <StyledIndex>{`${activeIndex + 1}/${images.length}`}</StyledIndex> : null}
+              <CloseButton onClick={onClose}>
+                <IconClose />
+              </CloseButton>
               <Responsive only='desktop' asChild>
                 <StyledSwiper
                   modules={[Navigation]}
@@ -60,9 +63,6 @@ const FeedImageSlider = ({ images, opened, onClose }: FeedImageSliderProps) => {
                   ))}
                 </StyledSwiper>
               </Responsive>
-              <CloseButton onClick={onClose}>
-                <IconClose />
-              </CloseButton>
             </Background>
           </RemoveScroll>
         </Portal>
