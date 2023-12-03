99b4f6bc3965f46c15b2885e3102c3af029af8ab
Seojisoo20191941 <amy0480@naver.com>
2023-12-04 00:34:34 +0900

    refactor: 인앱브라우저 탈출 useEffect로 구현방식 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -66,6 +66,41 @@ function MyApp({ Component, pageProps }: AppProps) {
     }
   }, []);
 
+  useEffect(() => {
+    const inappdeny_exec_vanillajs = (callback: () => void) => {
+      if (document.readyState !== 'loading') {
+        callback();
+      } else {
+        document.addEventListener('DOMContentLoaded', callback);
+      }
+    };
+    inappdeny_exec_vanillajs(() => {
+      function copytoclipboard(val: string) {
+        const t = document.createElement('textarea');
+        document.body.appendChild(t);
+        t.value = val;
+        t.select();
+        document.execCommand('copy');
+        document.body.removeChild(t);
+      }
+      function inappbrowserout() {
+        copytoclipboard(window.location.href);
+        alert(
+          'URL주소가 복사되었습니다.\n\nSafari가 열리면 주소창을 길게 터치한 뒤, "붙여놓기 및 이동"를 누르면 정상적으로 이용하실 수 있습니다.',
+        );
+        location.href = 'x-web-search://?';
+      }
+
+      const useragt = navigator.userAgent.toLowerCase();
+      const target_url = location.href;
+
+      if (useragt.match(/kakaotalk/i)) {
+        // MEMO: 카카오톡 외부브라우저로 호출
+        location.href = 'kakaotalk://web/openExternal?url=' + encodeURIComponent(target_url);
+      }
+    });
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <NextSeo
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,42 +12,6 @@ export default function Document() {
         <link rel='preload' href='/fonts/SUIT-Medium.woff2' as='font' type='font/woff2' crossOrigin='' />
         <link rel='preload' href='/fonts/SUIT-Regular.woff2' as='font' type='font/woff2' crossOrigin='' />
         <link rel='preload' href='/fonts/SUIT-Light.woff2' as='font' type='font/woff2' crossOrigin='' />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `const inappdeny_exec_vanillajs = (callback) => {
-              if (document.readyState !== 'loading') {
-                callback();
-              } else {
-                document.addEventListener('DOMContentLoaded', callback);
-              }
-            };
-            inappdeny_exec_vanillajs(() => {
-              function copytoclipboard(val) {
-                const t = document.createElement('textarea');
-                document.body.appendChild(t);
-                t.value = val;
-                t.select();
-                document.execCommand('copy');
-                document.body.removeChild(t);
-              }
-              function inappbrowserout() {
-                copytoclipboard(window.location.href);
-                alert(
-                  'URL주소가 복사되었습니다.\n\nSafari가 열리면 주소창을 길게 터치한 뒤, "붙여놓기 및 이동"를 누르면 정상적으로 이용하실 수 있습니다.',
-                );
-                location.href = 'x-web-search://?';
-              }
-
-              const useragt = navigator.userAgent.toLowerCase();
-              const target_url = location.href;
-
-              if (useragt.match(/kakaotalk/i)) {
-                // MEMO: 카카오톡 외부브라우저로 호출
-                location.href = 'kakaotalk://web/openExternal?url=' + encodeURIComponent(target_url);
-              }
-            })`,
-          }}
-        />
       </Head>
       <body>
         <GoogleTagManagerNoscript />
