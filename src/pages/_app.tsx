b8be83efc167cdecd0e11aeb4ccdaba3df469cbb
seojisoosoo <76681519+seojisoosoo@users.noreply.github.com>
2023-12-04 12:14:33 +0900

    feat: 인앱 브라우저 탈출 구현 (#1250)
    * feat: 인앱 브라우저 탈출 구현
    
    * refactor: script 태그 방식 변경
    
    * chore: 카카오톡 인앱브라우저 분기만 남기기
    
    * refactor: 인앱브라우저 탈출 useEffect로 구현방식 변경
    
    * feat: 브라우저 탈출 불가 시, 대응 뷰 구현
    
    * refactor: 모바일 대응 뷰 방식 변경
    
    * chore: 대응 뷰 포트 스타일 수정
    
    * chore: 대응 뷰 포트 스타일 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { OverlayProvider } from '@toss/use-overlay';
 import { LazyMotion } from 'framer-motion';
+import NextAdapterPages from 'next-query-params/pages';
+import { NextSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import Router, { useRouter } from 'next/router';
-import NextAdapterPages from 'next-query-params/pages';
-import { NextSeo } from 'next-seo';
 import { useEffect } from 'react';
 import { RecoilRoot } from 'recoil';
 import { QueryParamProvider } from 'use-query-params';
@@ -64,6 +64,45 @@ function MyApp({ Component, pageProps }: AppProps) {
         document.head.appendChild($meta);
       }
     }
+  }, []);
+
+  useEffect(() => {
+    const inappdeny_exec_vanillajs = (callback: () => void) => {
+      if (document.readyState !== 'loading') {
+        callback();
+      } else {
+        document.addEventListener('DOMContentLoaded', callback);
+      }
+    };
+    inappdeny_exec_vanillajs(() => {
+      const useragt = navigator.userAgent.toLowerCase();
+      const target_url = location.href;
+
+      if (useragt.match(/kakaotalk/i)) {
+        // MEMO: 카카오톡 외부브라우저로 호출
+        location.href = 'kakaotalk://web/openExternal?url=' + encodeURIComponent(target_url);
+      } else if (
+        useragt.match(
+          /inapp|naver|snapchat|wirtschaftswoche|thunderbird|instagram|everytimeapp|whatsApp|electron|wadiz|aliapp|zumapp|iphone(.*)whale|android(.*)whale|kakaostory|band|twitter|DaumApps|DaumDevice\/mobile|FB_IAB|FB4A|FBAN|FBIOS|FBSS|trill|SamsungBrowser\/[^1]/i,
+        )
+      ) {
+        // MEMO: 그외 다른 인앱들
+        if (useragt.match(/iphone|ipad|ipod/i)) {
+          // MEMO: 아이폰은 강제로 사파리를 실행할 수 없으므로, 모바일 대응 뷰 마운트
+
+          const mobile = document.createElement('meta');
+          mobile.name = 'viewport';
+          mobile.content = 'width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui';
+          document.getElementsByTagName('head')[0].appendChild(mobile);
+          document.body.innerHTML =
+            "<style>body{margin:0;padding:0;font-family: 'SUIT'; sans-serif;overflow: hidden;height: 100vh; width:100%; display:flex; justify-content:center; align-items: center; flex-wrap:wrap;}</style><article style='text-align:center; font-size:14px; font-weight: 400; line-height: 22px; letter-spacing: -0.14px; word-break:keep-all;color:#FCFCFC;'>인앱브라우저에서는 소셜로그인이 불가능해요 😭 <br/> 링크를 복사해 기본 브라우저에서 다시 시도해주시겠어요?</article>";
+        } else {
+          // MEMO: 안드로이드는 Chrome이 설치되어있음으로 강제로 스킴실행한다.
+          location.href =
+            'intent://' + target_url.replace(/https?:\/\//i, '') + '#Intent;scheme=http;package=com.android.chrome;end';
+        }
+      }
+    });
   }, []);
 
   return (
