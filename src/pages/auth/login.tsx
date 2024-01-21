5990d528a0b10618f23d820007b63abca2f9095e
Tekiter <tekiter8@gmail.com>
2024-01-22 02:06:41 +0900

    feat: 데스크톱 뷰 마크업

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import { colors } from '@sopt-makers/colors';
+import { fonts } from '@sopt-makers/fonts';
 import { m, Variants } from 'framer-motion';
 import Link from 'next/link';
 import { FC, useEffect, useState } from 'react';
@@ -66,13 +67,10 @@ const LoginPage: FC = () => {
     <StyledLoginPage>
       <LoginBox>
         <LoginTitle>
-          <LoginPrefix>SOPT Playground에</LoginPrefix> 오신 것을 환영해요!
-        </LoginTitle>
-        <LoginDescription>
-          Playground는 SOPT 회원만을 위한 공간이에요.
+          SOPT 회원으로 인증된
           <br />
-          지금 회원가입하고, 역대 SOPT 구성원들과 소통해 보아요!
-        </LoginDescription>
+          사용자만 로그인할 수 있어요
+        </LoginTitle>
         <LinkContainer>
           {googleAuth.isAvailable && <GoogleAuthButton onClick={googleAuth.login}>Google로 로그인</GoogleAuthButton>}
           {appleAuth.isAvailable && <AppleAuthButton onClick={appleAuth.login}>Apple로 로그인</AppleAuthButton>}
@@ -84,41 +82,35 @@ const LoginPage: FC = () => {
           </ResetLoginDescription>
           <ResetLoginAction>소셜 계정 재설정하기 {'>'}</ResetLoginAction>
         </ResetLoginCard>
-        <RegisterInfo>
-          Playground가 처음이신가요?{' '}
-          <RegisterLink href={playgroundLink.register()} onClick={() => logClickEvent('registerLink')}>
-            회원가입하기
-          </RegisterLink>
-        </RegisterInfo>
 
-        <LastLogin
-          initial='hide'
-          animate={lastLoginMessage ? 'show' : 'hide'}
-          variants={{
-            show: {
-              opacity: 1,
-            },
-            hide: {
-              opacity: 0,
-            },
-          }}
-        >
-          마지막으로 로그인한 계정은 {lastLoginMessage}이에요.
-        </LastLogin>
+        {lastLoginMessage != null && (
+          <LastLogin
+            initial='hide'
+            animate='show'
+            variants={{
+              show: {
+                opacity: 1,
+              },
+              hide: {
+                opacity: 0,
+              },
+            }}
+          >
+            마지막으로 로그인한 계정은 {lastLoginMessage}이에요.
+          </LastLogin>
+        )}
+        <HLine />
+        <RegisterInfo>Playground가 처음이신가요?</RegisterInfo>
+        <RegisterLink href={playgroundLink.register()} onClick={() => logClickEvent('registerLink')}>
+          <span>회원 인증하고 Playground 가입하러 가기</span>
+          <RegisterIcon />
+        </RegisterLink>
       </LoginBox>
 
-      <MotionMakersContainer initial='init' animate='open' whileHover='hover'>
-        <MadeByMakersLink href={playgroundLink.makers()}>
-          <MadeByTitle>made by</MadeByTitle>
-          <StyledMakersLogo src='/logos/logo-makers-full.svg' alt='makers-logo' />
-        </MadeByMakersLink>
-        <TooltipHolder>
-          <MotionTooltip variants={tooltipVariants}>
-            <TooltipRect>이 서비스를 만든 sopt makers 사람들이 궁금하다면?</TooltipRect>
-            <TooltipArrow />
-          </MotionTooltip>
-        </TooltipHolder>
-      </MotionMakersContainer>
+      <MadeByMakersLink href={playgroundLink.makers()}>
+        <MadeByTitle>이 서비스를 만든 SOPT makers가 궁금하다면?</MadeByTitle>
+        <StyledMakersLogo src='/logos/logo-makers-full.svg' alt='makers-logo' />
+      </MadeByMakersLink>
     </StyledLoginPage>
   );
 };
@@ -126,12 +118,14 @@ const LoginPage: FC = () => {
 export default LoginPage;
 
 setLayout(LoginPage, 'fullScreen');
+
 export const StyledLoginPage = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-around;
   height: 100%;
+  min-height: 700px;
 
   @media ${MOBILE_MEDIA_QUERY} {
     padding: 0 20px;
@@ -146,16 +140,10 @@ const LoginBox = styled.div`
 
 const LoginTitle = styled.h2`
   text-align: center;
-  ${textStyles.SUIT_32_SB}
+  ${fonts.HEADING_32_B}
 
   @media ${MOBILE_MEDIA_QUERY} {
     ${textStyles.SUIT_24_B}
-  }
-`;
-
-const LoginPrefix = styled.span`
-  @media ${MOBILE_MEDIA_QUERY} {
-    display: block;
   }
 `;
 
@@ -174,8 +162,8 @@ export const LoginDescription = styled.p`
 const LinkContainer = styled.div`
   display: flex;
   flex-direction: column;
-  row-gap: 20px;
-  margin-top: 50px;
+  row-gap: 12px;
+  margin-top: 36px;
   width: 420px;
 
   @media ${MOBILE_MEDIA_QUERY} {
@@ -190,9 +178,9 @@ const ResetLoginCard = styled(Link)`
     'icon description' auto
     'icon action' auto
     / auto 1fr;
-  row-gap: 8px;
-  column-gap: 14px;
-  margin-top: 24px;
+  row-gap: 14px;
+  column-gap: 9px;
+  margin-top: 12px;
   border-radius: 10px;
   background: rgb(24 119 242 / 20%);
   padding: 16px;
@@ -224,9 +212,22 @@ const ResetLoginAction = styled.div`
   ${textStyles.SUIT_14_M}
 `;
 
+const LastLogin = styled(m.div)`
+  padding-top: 25px;
+  overflow: hidden;
+  color: ${colors.gray100};
+
+  ${fonts.BODY_18_M}
+`;
+
+const HLine = styled.div`
+  margin: 30px 0;
+  border-bottom: 1px solid ${colors.gray700};
+  width: 100%;
+`;
+
 const RegisterInfo = styled.div`
-  margin-top: 30px;
-  color: ${colors.gray400};
+  color: ${colors.gray500};
 
   ${textStyles.SUIT_16_M};
 
@@ -237,75 +238,44 @@ const RegisterInfo = styled.div`
   }
 `;
 
-const LastLogin = styled(m.div)`
-  margin-top: 25px;
-  color: ${colors.gray10};
-
-  ${textStyles.SUIT_16_SB}
-`;
-
 const RegisterLink = styled(Link)`
-  transition: 0.2s color;
-  text-decoration: underline;
-  color: ${colors.gray10};
-`;
-
-const MotionMakersContainer = styled(m.div)`
-  position: relative;
-  margin-top: 40px;
-`;
-
-const TooltipHolder = styled.div`
-  position: absolute;
-  top: 0;
-  left: 50%;
-  transform: translate(-50%, calc(-100% - 20px));
-`;
-
-const MotionTooltip = styled(m.div)`
   display: flex;
-  flex-direction: column;
+  gap: 3px;
   align-items: center;
-`;
+  justify-content: center;
+  transition: 0.3s background-color;
+  margin-top: 13px;
+  border-radius: 100px;
+  background-color: ${colors.gray900};
+  padding: 14px;
+  color: ${colors.gray300};
 
-const TooltipRect = styled.div`
-  border-radius: 12px;
-  background-color: ${colors.gray700};
-  padding: 18px 16px;
-  white-space: nowrap;
-  color: ${colors.gray30};
+  ${fonts.LABEL_16_SB}
 
-  ${textStyles.SUIT_16_M}
-
-  @media ${MOBILE_MEDIA_QUERY} {
-    padding: 15px 20px;
-
-    ${textStyles.SUIT_12_M};
+  &:hover {
+    background-color: ${colors.gray800};
   }
 `;
 
-const TooltipArrow = styled.div`
-  border: 10px solid transparent;
-  border-top: 12px solid ${colors.gray700};
-  border-bottom: 0;
-  width: 0;
-  height: 0;
+const RegisterIcon = styled(ArrowIcon)`
+  width: 31px;
+  height: 30px;
+`;
+
+const MadeByTitle = styled.h3`
+  color: ${colors.gray600};
+  ${fonts.BODY_16_R}
 `;
 
 const MadeByMakersLink = styled(Link)`
   display: flex;
   flex-direction: column;
+  gap: 20px;
   align-items: center;
-  width: 100%;
-  max-width: 234px;
-`;
-
-const MadeByTitle = styled.h3`
-  ${textStyles.SUIT_20_R};
+  text-align: center;
 `;
 
 const StyledMakersLogo = styled.img`
-  margin-top: 10px;
   height: 53px;
 
   @media ${MOBILE_MEDIA_QUERY} {
@@ -322,6 +292,20 @@ function WarningIcon(props: React.SVGProps<SVGSVGElement>) {
         d='M7 0a7 7 0 100 14A7 7 0 007 0zm.572 4.143a.571.571 0 00-1.143 0v3.214a.571.571 0 101.143 0V4.143zM7 10.572a.714.714 0 100-1.43.714.714 0 000 1.43z'
         fill='#E4EDFF'
       />
+    </svg>
+  );
+}
+
+function ArrowIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox='0 0 31 30' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M18.07 11.696c-.895.895-2.526.895-3.422 0l-.367-.367-.734.734.367.367c.651.651 1.548.977 2.445.977l-4.53 4.53.734.735 4.53-4.53c0 .896.326 1.793.977 2.444l.367.367.735-.734-.368-.367c-.895-.896-.895-2.527 0-3.422l.368-.367-.735-.735-.367.368z'
+        fill='#808087'
+      />
+      <circle cx={15.5} cy={15} r={9.953} transform='rotate(-45 15.5 15)' stroke='#808087' strokeWidth={0.864} />
     </svg>
   );
 }
