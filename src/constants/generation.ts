30feed0793f6cc8a7976195fd6e97897bdefa921
Tekiter <tekiter8@gmail.com>
2024-03-01 00:11:46 +0900

    feat: 34기 적용 (#1301)

--- src/constants/generation.ts
+++ src/constants/generation.ts
@@ -1,3 +1,3 @@
-export const LATEST_GENERATION = 33;
+export const LATEST_GENERATION = 34; // TODO: 자동 갱신 로직 있으면 좋음
 
 export const GENERATIONS = Array.from({ length: LATEST_GENERATION }, (_, i) => (i + 1).toString()).reverse();
