3774325135595880de154cccbcc29ff94ad4d8a7
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-03-09 21:25:55 +0900

    feat: getMemberCrew endpoint scheme 정의

---
+++ src/api/endpoint/members/getMemberCrew.ts
@@ -0,0 +1,38 @@
+import { QS } from '@toss/utils';
+import { z } from 'zod';
+
+import { createEndpoint } from '@/api/typedAxios';
+
+interface Params {
+  page: number;
+  take: number;
+}
+
+export const getMemberCrew = createEndpoint({
+  request: (params: Params) => ({
+    method: 'GET',
+    url: `api/v1/members/crew${QS.create(params)}`,
+  }),
+  serverResponseScheme: z.object({
+    meetings: z.array(
+      z.object({
+        id: z.number(),
+        isMeetingLeader: z.boolean(),
+        title: z.string(),
+        imageUrl: z.string().nullable(),
+        category: z.string().nullable(),
+        isActiveMeeting: z.boolean(),
+        mstartDate: z.string(),
+        mendDate: z.string(),
+      }),
+    ),
+    meta: z.object({
+      page: z.number().nullable(),
+      take: z.number().nullable(),
+      itemCount: z.number().nullable(),
+      pageCount: z.number().nullable(),
+      hasPreviousPage: z.boolean().nullable(),
+      hasNextPage: z.boolean().nullable(),
+    }),
+  }),
+});
