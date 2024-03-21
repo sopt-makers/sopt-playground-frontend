37075c5c6a286e2c566febbce0041c32c07eb615
seojisoosoo <76681519+seojisoosoo@users.noreply.github.com>
2024-03-21 21:26:48 +0900

    feat: 34기 환영 배너 만들기 (#1321)
    * feat: 34기 환영 배너 구현
    
    * chore: 모바일 배너 크기 조정
    
    * feat: 34기 여부에 따라 다른 배너 보여지도록 구현
    
    * refactor: 배너 gif 깜빡이는 현상 해결
    
    * chore: 이미지 변경
    
    * chore: CTA버튼 다짐 메시지 연결 주석 작성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next';
 
 import AuthRequired from '@/components/auth/AuthRequired';
+import ActiveBannerSlot from '@/components/common/Banner/ActiveBannerSlot';
 import FeedHomePage from '@/components/feed/page/FeedHomePage';
 import { setLayout } from '@/utils/layout';
 
 const Home: NextPage = () => {
   return (
     <AuthRequired>
+      <ActiveBannerSlot />
       <FeedHomePage />
     </AuthRequired>
   );
