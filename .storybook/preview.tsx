562a73448313dc51ecb84bb68d72f6acfb8a9adc
seojisoosoo <76681519+seojisoosoo@users.noreply.github.com>
2024-03-25 03:41:49 +0900

    fix: 스토리북 에러 대응 (#1298)
    * fix: 스토리북 에러 대응
    
    * chore: 스토리북 void 타입 함수 수정
    
    * chore: LazyMotion strict 모드 빼기
    
    * chore: 불필요 코드 삭제

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
 import { themes } from '@storybook/theming';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { OverlayProvider } from '@toss/use-overlay';
 import { LazyMotion } from 'framer-motion';
 import { initialize, mswDecorator } from 'msw-storybook-addon';
 import NextAdapterPages from 'next-query-params/pages';
 import { RouterContext } from 'next/dist/shared/lib/router-context';
-import { QueryParamProvider } from 'use-query-params';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
 import { RecoilRoot } from 'recoil';
-import { OverlayProvider } from '@toss/use-overlay';
+import { QueryParamProvider } from 'use-query-params';
 
+import { colors } from '@sopt-makers/colors';
 import ResponsiveProvider from '../src/components/common/Responsive/ResponsiveProvider';
 import StorybookToastProvider from '../src/components/common/Toast/providers/StorybookToastProvider';
 import StorybookEventLoggerProvider from '../src/components/eventLogger/providers/StorybookEventLoggerProvider';
-import { colors } from '@sopt-makers/colors';
 import GlobalStyle from '../src/styles/GlobalStyle';
 
 initialize();
@@ -48,7 +48,7 @@ export const decorators = [
     <QueryClientProvider client={queryClient}>
       <QueryParamProvider adapter={NextAdapterPages}>
         <RecoilRoot>
-          <LazyMotion strict features={() => import('framer-motion').then((mod) => mod.domAnimation)}>
+          <LazyMotion features={() => import('framer-motion').then((mod) => mod.domAnimation)}>
             <StorybookEventLoggerProvider>
               <StorybookToastProvider>
                 <GlobalStyle />
--- src/components/feed/upload/Category/CategorySelector/index.stories.tsx
+++ src/components/feed/upload/Category/CategorySelector/index.stories.tsx
@@ -1,36 +1,114 @@
+import styled from '@emotion/styled';
+import { colors } from '@sopt-makers/colors';
 import { Meta } from '@storybook/react';
 
 import Button from '@/components/common/Button';
 import useModalState from '@/components/common/Modal/useModalState';
 import CategorySelector from '@/components/feed/upload/Category/CategorySelector';
+import { categories } from '@/components/feed/upload/Category/constants';
+import { DropDown } from '@/components/feed/upload/Category/DropDown';
+import { BasicCategory } from '@/components/feed/upload/Category/types';
+import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
+import { textStyles } from '@/styles/typography';
 
 export default {
   component: CategorySelector,
 } as Meta;
 
+// MEMO: CategorySelector의 내용은 api 통신을 통해 가져오는 데이터가 포함되어있습니다.
+// 스토리북에 띄워보기 위해 목데이터를 삽입하여 보여지도록 구현하였습니다.
 export const Default = {
   render: function Render() {
     const { isOpen, onClose, onOpen } = useModalState();
 
+    const feedData = {
+      categoryId: 1,
+      title: '제목',
+      content: '내용',
+      isQuestion: false,
+      isBlindWriter: false,
+      images: [],
+    };
+
     return (
       <>
         <Button onClick={onOpen}>클릭하여 셀렉터 열기</Button>
-        <CategorySelector
-          isOpen={isOpen}
-          onClose={onClose}
-          onSelect={onClose}
-          feedData={{
-            categoryId: 0,
-            title: '',
-            content: '',
-            isQuestion: false,
-            isBlindWriter: false,
-            images: [],
-          }}
-        />
+        <DropDown isOpen={isOpen} onClose={onClose} className='category-drop' header={<Title>어디에 올릴까요?</Title>}>
+          <Select>
+            {categories &&
+              categories.length > 0 &&
+              categories.map((category: BasicCategory) => {
+                return (
+                  <Option
+                    key={category.id}
+                    onClick={() => {
+                      //
+                    }}
+                    isSelected={category.id === feedData.categoryId}
+                  >
+                    <OptionTitle>{category.name}</OptionTitle>
+                    <OptionContents>{category.content}</OptionContents>
+                  </Option>
+                );
+              })}
+          </Select>
+        </DropDown>
       </>
     );
   },
 
   name: '카테고리 셀렉터',
 };
+
+const OptionTitle = styled.h2`
+  ${textStyles.SUIT_16_M};
+
+  color: ${colors.white};
+`;
+
+const OptionContents = styled.p`
+  text-align: left;
+  ${textStyles.SUIT_12_R};
+
+  color: ${colors.gray300};
+`;
+
+const Option = styled.button<{ isSelected: boolean }>`
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+  border-radius: 6px;
+  background-color: ${({ isSelected }) => isSelected && colors.gray700};
+  cursor: pointer;
+  padding: 12px;
+  width: 100%;
+
+  &:active {
+    background-color: ${colors.gray700};
+  }
+
+  &:hover {
+    background-color: ${colors.gray700};
+  }
+`;
+
+const Select = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  width: 100%;
+`;
+
+const Title = styled.header`
+  display: none;
+
+  @media ${MOBILE_MEDIA_QUERY} {
+    display: flex;
+    gap: 12px;
+    align-items: center;
+
+    ${textStyles.SUIT_20_B}
+
+    padding: 0 20px 12px;
+  }
+`;
