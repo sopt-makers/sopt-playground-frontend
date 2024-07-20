3beeb6b947df2e158df92e5ff37970866eb12fce
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-07-21 06:18:07 +0900

    feat: html2canvas로 이미지 다운로드 커스텀 훅 구현

---
+++ src/components/resolution/read/hooks/useImageDownload.ts
@@ -0,0 +1,26 @@
+import html2canvas from 'html2canvas';
+import { useRef } from 'react';
+
+const useImageDownload = () => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const onClick = () => {
+    html2canvas(ref.current as HTMLDivElement, {
+      backgroundColor: null,
+      scale: 2,
+    })
+      .then((canvas) => {
+        const link = document.createElement('a');
+        link.href = canvas.toDataURL('image/png');
+        link.download = 'test.png';
+        link.click();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return { ref, onClick };
+};
+
+export default useImageDownload;
