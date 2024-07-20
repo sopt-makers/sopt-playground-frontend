e15e7d3a07b6bc4b306108163490ef770ff210dc
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-07-21 01:03:19 +0900

    feat: 다짐메시지 조회 api 연결

---
+++ src/api/endpoint/resolution/getResolution.ts
@@ -0,0 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
+import { z } from 'zod';
+
+import { createEndpoint } from '@/api/typedAxios';
+
+const getResolution = createEndpoint({
+  request: {
+    method: 'GET',
+    url: 'api/v1/resolution',
+  },
+  serverResponseScheme: z.object({
+    memberImageUrl: z.string().or(z.null()),
+    memberName: z.string(),
+    tags: z.array(z.string()),
+    content: z.string(),
+  }),
+});
+
+export const useGetResolution = () => {
+  return useQuery({
+    queryKey: ['getResolution'],
+    queryFn: async () => {
+      const data = await getResolution.request();
+      return data;
+    },
+  });
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,14 @@ import type { NextPage } from 'next';
 import AuthRequired from '@/components/auth/AuthRequired';
 import ActiveBannerSlot from '@/components/common/Banner/ActiveBannerSlot';
 import FeedHomePage from '@/components/feed/page/FeedHomePage';
+import ResolutionReadModal from '@/components/resolution/read/ResolutionReadModal';
 import { setLayout } from '@/utils/layout';
 
 const Home: NextPage = () => {
   return (
     <AuthRequired>
       <ActiveBannerSlot />
+      <ResolutionReadModal onClose={() => {}} />
       <FeedHomePage />
     </AuthRequired>
   );
