7e4fe0cb6d2d232dc03c685a6bc799ef650df084
seojisoosoo <76681519+seojisoosoo@users.noreply.github.com>
2024-07-23 01:12:35 +0900

    feat: 회고 메시지 input 구현 (#1470)

--- src/api/endpoint/remember/uploadReview.ts
+++ src/api/endpoint/remember/uploadReview.ts
@@ -1,12 +1,28 @@
 import { z } from 'zod';
 
 import { createEndpoint } from '@/api/typedAxios';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-export const uploadFeed = createEndpoint({
-  request: (reqeustBody) => ({
+interface RequestBody {
+  content: string;
+}
+
+export const uploadReview = createEndpoint({
+  request: (reqeustBody: RequestBody) => ({
     method: 'POST',
     url: 'review/upload',
     data: reqeustBody,
   }),
   serverResponseScheme: z.unknown(),
 });
+
+export const useUploadReviewMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (reqeustBody: RequestBody) => uploadReview.request(reqeustBody),
+    onSuccess: () => {
+      //   queryClient.invalidateQueries({ queryKey: useGetReviewsInfiniteQuery.getKey('') }); // review
+    },
+  });
+};
--- src/components/remember/index.tsx
+++ src/components/remember/index.tsx
@@ -1,3 +1,4 @@
+import ReviewInput from '@/components/remember/ReviewInput';
 import Reviews from '@/components/remember/reviews';
 import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
 import styled from '@emotion/styled';
@@ -8,6 +9,7 @@ export default function RememberPage() {
   return (
     <>
       <Header>{'34기 NOW SOPT \n활동 후기를 남겨주세요!'}</Header>
+      <ReviewInput />
       <Reviews />
     </>
   );
