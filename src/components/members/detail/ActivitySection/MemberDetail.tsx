d226701d11e5f5a875fd9c9c1bf8dfe054d51397
Hyejun Lee <solar3070@naver.com>
2024-08-17 23:36:22 +0900

    feat: 상세 프로필 커피챗 아이콘 노출 및 직무 기간 변경 이슈 해결 (#1513)
    * feat: 상세 프로필에 커피챗 아이콘 보여주기
    
    * feat: 커리어 기간 setValue shouldDirty true 설정
    
    * feat: 커피 아이콘 모바일 반응형
    
    * fix: 스타일린트 에러 수정
    
    * fix: 스타일린트 에러 수정
    
    ---------
    
    Co-authored-by: solar3070 <>

--- src/components/members/detail/ActivitySection/MemberDetail.tsx
+++ src/components/members/detail/ActivitySection/MemberDetail.tsx
@@ -9,6 +9,7 @@ import CallIcon from 'public/icons/icon-call.svg';
 import EditIcon from 'public/icons/icon-edit.svg';
 import MailIcon from 'public/icons/icon-mail.svg';
 import ProfileIcon from 'public/icons/icon-profile.svg';
+import IconCoffee from '@/public/icons/icon-coffee.svg';
 import { FC, useMemo } from 'react';
 
 import { useGetMemberCrewInfiniteQuery } from '@/api/endpoint/members/getMemberCrew';
@@ -25,7 +26,6 @@ import CareerSection from '@/components/members/detail/CareerSection';
 import EmptyProfile from '@/components/members/detail/EmptyProfile';
 import InfoItem from '@/components/members/detail/InfoItem';
 import InterestSection from '@/components/members/detail/InterestSection';
-import MessageSection from '@/components/members/detail/MessageSection';
 import SoptActivitySection from '@/components/members/detail/SoptActivitySection';
 import { DEFAULT_DATE } from '@/components/members/upload/constants';
 import { playgroundLink } from '@/constants/links';
@@ -105,14 +105,20 @@ const MemberDetail: FC<MemberDetailProps> = ({ memberId }) => {
     <Container>
       <Wrapper>
         <ProfileContainer>
-          {profile.profileImage ? (
-            <ProfileImage src={profile.profileImage} height={171} />
-          ) : (
-            <EmptyProfileImage>
-              <ProfileIcon />
-            </EmptyProfileImage>
-          )}
-
+          <ImageSection>
+            {profile.profileImage ? (
+              <ProfileImage src={profile.profileImage} height={171} />
+            ) : (
+              <EmptyProfileImage>
+                <ProfileIcon />
+              </EmptyProfileImage>
+            )}
+            {profile.isCoffeeChatActivate && (
+              <IconContainer>
+                <IconCoffee />
+              </IconContainer>
+            )}
+          </ImageSection>
           <ProfileContents>
             <div>
               <NameWrapper>
@@ -299,7 +305,7 @@ const Container = styled.div`
   align-items: center;
   padding: 123px 0;
   @media ${MOBILE_MEDIA_QUERY} {
-    padding: 12px 20px;
+    padding: 16px 20px;
     padding-bottom: 100px;
   }
 `;
@@ -591,3 +597,32 @@ const Target = styled.div`
 `;
 
 export default MemberDetail;
+
+const ImageSection = styled.div`
+  position: relative;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  position: absolute;
+  top: -8px;
+  right: -8px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  background: ${colors.blue400};
+  padding: 5px;
+  width: 32px;
+  height: 32px;
+
+  @media ${MOBILE_MEDIA_QUERY} {
+    padding: 3px;
+    width: 26px;
+    height: 26px;
+
+    & > svg {
+      width: 19px;
+      height: 19px;
+    }
+  }
+`;
