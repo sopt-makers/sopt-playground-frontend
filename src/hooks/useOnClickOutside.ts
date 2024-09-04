daf212b063ed4ccb31c6b046c007751b543c2edb
hayoun <fishingest@kookmin.ac.kr>
2024-09-05 00:21:16 +0900

    feat: Not Overlay Confirm 모달 구현

--- src/hooks/useOnClickOutside.ts
+++ src/hooks/useOnClickOutside.ts
@@ -9,12 +9,8 @@ const useOnClickOutside = <T extends HTMLElement = HTMLElement>(ref: RefObject<T
       if (!el || el.contains((event?.target as Node) || null)) {
         return;
       }
-      if (el?.getAttribute('role') === 'dialog'&&el?.getAttribute('class')==='css-68r3cr') {
-        
-      }
-      else{
       handler(event);
-    } // Call the handler only if the click is outside of the element passed.
+   // Call the handler only if the click is outside of the element passed.
     };
 
     document.addEventListener('mousedown', listener);
