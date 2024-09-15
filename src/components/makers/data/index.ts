609a2dc9950c0123886cb226149c412bb7cfaae6
Seojisoo20191941 <amy0480@naver.com>
2024-09-15 19:26:57 +0900

    feat: 메이커스 35기 갱신

--- src/components/makers/data/index.ts
+++ src/components/makers/data/index.ts
@@ -2,10 +2,12 @@ import { contributors } from '@/components/makers/data/contributors';
 import { generation1 } from '@/components/makers/data/generation1';
 import { generation2 } from '@/components/makers/data/generation2';
 import { generation3 } from '@/components/makers/data/generation3';
+import { generation35 } from '@/components/makers/data/generation35';
 import { generation4 } from '@/components/makers/data/generation4';
 import { MakersGeneration } from '@/components/makers/data/types';
 
 export const makersGenerationsData: MakersGeneration[] = [
+  generation35,
   generation4,
   generation3,
   generation2,
