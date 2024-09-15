eac86f244017e75bac170174932ad2edb5c602c3
Seojisoo20191941 <amy0480@naver.com>
2024-09-15 19:01:01 +0900

    chore: 고화질 사진으로 변경

--- src/components/common/Banner/AdsBanner/constants/ads.ts
+++ src/components/common/Banner/AdsBanner/constants/ads.ts
@@ -2,17 +2,15 @@ export const ADS = [
   {
     // 코드잇 1차
     id: 1,
-    moImage:
-      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/codit_community_header.png',
-    pcImage:
-      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/codit_pc_community_header_2.png',
+    moImage: 'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/1000059924.png',
+    pcImage: 'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/1000059925.png',
     url: 'https://sprint.codeit.kr/track/frontend-advanced?utm_source=SOPT&utm_medium=partnership&utm_content=frontend-adv',
   },
   // {
   //   // 코드잇 2차
   //   id: 2,
-  //   moImage: 'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/codit_mo_community_header_1.png',
-  //   pcImage: 'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/codit_pc_community_header_1.png',
+  //   moImage: 'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/1000059927.png',
+  //   pcImage: 'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/1000059926.png',
   //   url: 'https://sprint.codeit.kr/track/frontend?utm_source=SOPT&utm_medium=partnership&utm_content=frontend',
   // },
   {
@@ -37,9 +35,18 @@ export const ADS = [
     // 꾸물꿈
     id: 5,
     moImage:
-      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/%E1%84%81%E1%85%AE%E1%84%86%E1%85%AE%E1%86%AF%E1%84%81%E1%85%AE%E1%86%B7_MO.png',
+      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/%E1%84%81%E1%85%AE%E1%84%86%E1%85%AE%E1%86%AF%E1%84%81%E1%85%AE%E1%86%B7_MO+(1).png',
     pcImage:
-      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/%E1%84%81%E1%85%AE%E1%84%86%E1%85%AE%E1%86%AF%E1%84%81%E1%85%AE%E1%86%B7_PC.png',
+      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/%E1%84%81%E1%85%AE%E1%84%86%E1%85%AE%E1%86%AF%E1%84%81%E1%85%AE%E1%86%B7_PC+(1).png',
     url: 'https://www.instagram.com/kumulkum_official/',
   },
+  // {
+  //   // 터닝
+  //   id: 6,
+  //   moImage:
+  //     'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/terning_mobile+%E1%84%87%E1%85%A2%E1%84%82%E1%85%A5.png',
+  //   pcImage:
+  //     'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/terning_pc+%E1%84%87%E1%85%A2%E1%84%82%E1%85%A5.png',
+  //   url: 'https://terning.framer.website/',
+  // },
 ];
