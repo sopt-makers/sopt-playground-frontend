1ccb51f82aace2f523fb2bb883cf86c8daaf6ebb
jerry <becomegoodguy@gmail.com>
2024-09-19 23:51:31 +0900

    fix: mds Dialog로 변경 현재 button Function 동작 X

--- src/components/feed/common/hooks/useReportFeed.ts
+++ src/components/feed/common/hooks/useReportFeed.ts
@@ -1,9 +1,7 @@
-import { useToast } from '@sopt-makers/ui';
+import { useDialog, useToast } from '@sopt-makers/ui';
 import { useCallback } from 'react';
 
 import { usePostReportPostMutation } from '@/api/endpoint/feed/postReportPost';
-import useConfirm from '@/components/common/Modal/useConfirm';
-import { zIndex } from '@/styles/zIndex';
 
 interface Options {
   postId: string;
@@ -11,38 +9,41 @@ interface Options {
 }
 
 export const useReportFeed = () => {
-  const { confirm } = useConfirm();
   const { mutate } = usePostReportPostMutation();
   const { open } = useToast();
+  const { open: confirm } = useDialog();
 
   const handleReport = useCallback(
-    async (options: Options) => {
-      const result = await confirm({
+    (options: Options) => {
+      confirm({
         title: '이 글을 신고하시겠습니까?',
         description: '글을 신고할 경우, 메이커스에서 검토를 거쳐 적절한 조치 및 게시자 제재를 취할 예정이에요.',
-        okButtonText: '신고하기',
-        cancelButtonText: '취소',
-        maxWidth: 400,
-        zIndex: zIndex.헤더,
-      });
+        type: 'default',
+        typeOptions: {
+          cancelButtonText: '취소',
+          approveButtonText: '확인',
+          buttonFunction: () => {
+            console.log('inside buttonFunction');
 
-      if (result) {
-        mutate(options.postId, {
-          onSuccess: () => {
-            open({
-              icon: 'success',
-              content: '신고가 완료되었어요.\n건전한 커뮤니티를 함께 만들어주셔서 감사해요!',
-              style: {
-                content: {
-                  whiteSpace: 'pre-wrap',
-                },
+            mutate(options.postId, {
+              onSuccess: () => {
+                open({
+                  icon: 'success',
+                  content: '신고가 완료되었어요.\n건전한 커뮤니티를 함께 만들어주셔서 감사해요!',
+
+                  style: {
+                    content: {
+                      whiteSpace: 'pre-wrap',
+                    },
+                  },
+                });
+
+                options.onSuccess?.();
               },
             });
-
-            options.onSuccess?.();
           },
-        });
-      }
+        },
+      });
     },
     [confirm, open, mutate],
   );
