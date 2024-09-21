759fa994abbb6c0123e472d29766f45f1c3d7c5a
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-09-22 00:36:12 +0900

    feat: 35기 여부에 따른 뒷면 로고 분기처리

--- src/components/members/upload/complete/CardBack.tsx
+++ src/components/members/upload/complete/CardBack.tsx
@@ -1,13 +1,19 @@
 import styled from '@emotion/styled';
 import { FC } from 'react';
 
+import { useGetMemberOfMe } from '@/api/endpoint/members/getMemberOfMe';
 import ResizedImage from '@/components/common/ResizedImage';
+import { LATEST_GENERATION } from '@/constants/generation';
 
 const CardBack: FC = () => {
+  const { data: myData } = useGetMemberOfMe();
+  const is35 = myData?.generation === LATEST_GENERATION;
+
+  const logoSrc = is35 ? '/logos/and-sopt.svg' : '/logos/logo-playground-full.svg';
   return (
     <MemberCard>
       <ImageHolder>
-        <Image className='image' src='/logos/logo-playground-full.svg' width={187} alt='now_sopt' />
+        <Image className='image' src={logoSrc} width={187} alt='now_sopt' />
       </ImageHolder>
     </MemberCard>
   );
