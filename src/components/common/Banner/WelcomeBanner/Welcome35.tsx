6f73c1642a73568e3d0627d3b2ce29c1c752e06b
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-09-25 01:35:53 +0900

    feat: 환영배너에 Skeleton 적용

--- src/components/common/Banner/WelcomeBanner/Welcome35.tsx
+++ src/components/common/Banner/WelcomeBanner/Welcome35.tsx
@@ -1,12 +1,13 @@
 import { useGetMemberOfMe } from '@/api/endpoint/members/getMemberOfMe';
 import WelcomeBanner from '@/components/common/Banner/WelcomeBanner';
+import Skeleton from '@/components/common/Skeleton';
 import { LATEST_GENERATION } from '@/constants/generation';
 
 const Welcome35 = () => {
   const { data: myData, isPending } = useGetMemberOfMe();
   const is35 = myData?.generation === LATEST_GENERATION;
 
-  if (isPending) return <></>;
+  if (isPending) return <Skeleton height={168} />;
 
   return <WelcomeBanner is35={is35} />;
 };
