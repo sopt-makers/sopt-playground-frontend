08c2c95ad8a1f589349b272fb64b45bc6ed670d0
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-09-25 02:15:22 +0900

    feat: Skeleton 컴포넌트에 children, margin 옵션 추가

--- src/components/common/Skeleton/index.tsx
+++ src/components/common/Skeleton/index.tsx
@@ -1,21 +1,35 @@
 import styled from '@emotion/styled';
 import { colors } from '@sopt-makers/colors';
+import { ReactNode } from 'react';
 
 interface SkeletonProps {
   width?: number;
   height?: number;
   borderRadius?: number;
+  margin?: string;
   color?: string;
+  children?: ReactNode;
 }
 
-const Skeleton = ({ width, height, borderRadius, color }: SkeletonProps) => {
-  return <StyledSkeleton width={width} height={height} borderRadius={borderRadius} color={color} />;
+const Skeleton = ({ width, height, borderRadius, color, margin, children, ...props }: SkeletonProps) => {
+  return (
+    <StyledSkeleton width={width} height={height} borderRadius={borderRadius} margin={margin} color={color} {...props}>
+      {children}
+    </StyledSkeleton>
+  );
 };
 
 export default Skeleton;
 
-const StyledSkeleton = styled.div<{ width?: number; height?: number; borderRadius?: number; color?: string }>`
-  border-radius: ${({ borderRadius }) => borderRadius && `${borderRadius}%`};
+const StyledSkeleton = styled.div<{
+  width?: number;
+  height?: number;
+  borderRadius?: number;
+  margin?: string;
+  color?: string;
+}>`
+  margin: ${({ margin }) => margin && margin};
+  border-radius: ${({ borderRadius }) => borderRadius && `${borderRadius}px`};
   background-color: ${({ color }) => color ?? colors.gray900};
   width: ${({ width }) => (width ? `${width}px` : '100%')};
   height: ${({ height }) => (height ? `${height}px` : '100%')};
