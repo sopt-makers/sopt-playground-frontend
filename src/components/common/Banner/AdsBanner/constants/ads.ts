b4142acc525d59231dc83663327ad33286d128a5
Seojisoo20191941 <amy0480@naver.com>
2024-10-14 00:24:05 +0900

    feat: 광고 구좌 변경

--- src/components/common/Banner/AdsBanner/constants/ads.ts
+++ src/components/common/Banner/AdsBanner/constants/ads.ts
@@ -1,52 +1,20 @@
 export const ADS = [
-  // {
-  //   // 코드잇 1차
-  //   id: 1,
-  //   moImage: 'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/1000059924.png',
-  //   pcImage: 'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/1000059925.png',
-  //   url: 'https://sprint.codeit.kr/track/frontend-advanced?utm_source=SOPT&utm_medium=partnership&utm_content=frontend-adv',
-  // },
   {
-    // 코드잇 2차
+    // 픽플
+    id: 1,
+    moImage:
+      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/%E1%84%91%E1%85%B5%E1%86%A8%E1%84%91%E1%85%B3%E1%86%AF+MO.png',
+    pcImage:
+      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/%E1%84%91%E1%85%B5%E1%86%A8%E1%84%91%E1%85%B3%E1%86%AF+PC.png',
+    url: 'https://pick-ple.com/class-list?category=all',
+  },
+  {
+    // 메이커스 마케팅 오거나이저
     id: 2,
-    moImage: 'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/1000059927.png',
-    pcImage: 'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/1000059926.png',
-    url: 'https://sprint.codeit.kr/track/frontend?utm_source=SOPT&utm_medium=partnership&utm_content=frontend',
-  },
-  {
-    // 마일
-    id: 3,
     moImage:
-      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/%E1%84%86%E1%85%A1%E1%84%8B%E1%85%B5%E1%86%AF_%E1%84%8B%E1%85%A5%E1%86%B8%E1%84%89%E1%85%B3%E1%84%8F%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AF_%E1%84%87%E1%85%A2%E1%84%82%E1%85%A5_%E1%84%86%E1%85%A9%E1%84%87%E1%85%A1%E1%84%8B%E1%85%B5%E1%86%AF.png',
+      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/%E1%84%91%E1%85%B3%E1%86%AF%E1%84%80%E1%85%B3+MO.png',
     pcImage:
-      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/%E1%84%86%E1%85%A1%E1%84%8B%E1%85%B5%E1%86%AF_%E1%84%8B%E1%85%A5%E1%86%B8%E1%84%89%E1%85%B3%E1%84%8F%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AF_%E1%84%87%E1%85%A2%E1%84%82%E1%85%A5_%E1%84%83%E1%85%A6%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%90%E1%85%A1%E1%86%B8.png',
-    url: 'https://www.milewriting.com/',
-  },
-  {
-    // 한끼족보
-    id: 4,
-    moImage:
-      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/%E1%84%92%E1%85%A1%E1%86%AB%E1%84%81%E1%85%B5%E1%84%8C%E1%85%A9%E1%86%A8%E1%84%87%E1%85%A9_MOBILE.png',
-    pcImage:
-      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/%E1%84%92%E1%85%A1%E1%86%AB%E1%84%81%E1%85%B5%E1%84%8C%E1%85%A9%E1%86%A8%E1%84%87%E1%85%A9_PC.png',
-    url: 'https://teamhankki.imweb.me/',
-  },
-  {
-    // 꾸물꿈
-    id: 5,
-    moImage:
-      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/%E1%84%81%E1%85%AE%E1%84%86%E1%85%AE%E1%86%AF%E1%84%81%E1%85%AE%E1%86%B7_MO+(1).png',
-    pcImage:
-      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/%E1%84%81%E1%85%AE%E1%84%86%E1%85%AE%E1%86%AF%E1%84%81%E1%85%AE%E1%86%B7_PC+(1).png',
-    url: 'https://www.instagram.com/kumulkum_official/',
-  },
-  {
-    // 터닝
-    id: 6,
-    moImage:
-      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/terning_mobile+%E1%84%87%E1%85%A2%E1%84%82%E1%85%A5.png',
-    pcImage:
-      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/terning_pc+%E1%84%87%E1%85%A2%E1%84%82%E1%85%A5.png',
-    url: 'https://terning.framer.website/',
+      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/%E1%84%91%E1%85%B3%E1%86%AF%E1%84%80%E1%85%B3+PC.png',
+    url: 'https://forms.gle/7MQkMrXdr5HEyE6q9',
   },
 ];
