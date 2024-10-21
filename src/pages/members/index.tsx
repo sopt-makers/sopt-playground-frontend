4be66e102f2ed75374733c82fbd9d3a31a07d89b
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-10-22 00:49:01 +0900

    feat: 커피챗 배너 내리기

--- src/pages/members/index.tsx
+++ src/pages/members/index.tsx
@@ -7,7 +7,6 @@ import MemberList from '@/components/members/main/MemberList';
 import OnBoardingBanner from '@/components/members/main/MemberList/OnBoardingBanner';
 import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
 import { setLayout } from '@/utils/layout';
-import CoffeeChatList from '@/components/coffeechat/CoffeeChatList';
 
 const MemberPage: FC = () => {
   const { data: memberOfMeData } = useGetMemberOfMe();
@@ -17,7 +16,6 @@ const MemberPage: FC = () => {
 
   return (
     <AuthRequired>
-      <CoffeeChatList />
       <MemberList banner={onboardingBanner} />
     </AuthRequired>
   );
