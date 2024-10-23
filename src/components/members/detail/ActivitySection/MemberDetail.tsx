e38793d533637fbedcaea37b36d9cb0dc4bdb4e5
jerry <becomegoodguy@gmail.com>
2024-10-23 15:32:02 +0900

    feat: 멤버 상세 정보, 프로젝트, 모임 정보 컴포넌트로 분리

--- src/components/members/detail/ActivitySection/MemberDetail.tsx
+++ src/components/members/detail/ActivitySection/MemberDetail.tsx
@@ -4,7 +4,6 @@ import { fonts } from '@sopt-makers/fonts';
 import { IconAlertTriangle, IconUserX } from '@sopt-makers/icons';
 import { Flex } from '@toss/emotion-utils';
 import axios from 'axios';
-import dayjs from 'dayjs';
 import { uniq } from 'lodash-es';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -12,49 +11,35 @@ import CallIcon from 'public/icons/icon-call.svg';
 import EditIcon from 'public/icons/icon-edit.svg';
 import MailIcon from 'public/icons/icon-mail.svg';
 import ProfileIcon from 'public/icons/icon-profile.svg';
-import { CSSProperties, FC, useEffect, useMemo, useState } from 'react';
+import { FC, useMemo } from 'react';
 
 import { useGetMemberCrewInfiniteQuery } from '@/api/endpoint/members/getMemberCrew';
 import { useGetMemberOfMe } from '@/api/endpoint/members/getMemberOfMe';
 import { useGetMemberProfileById } from '@/api/endpoint_LEGACY/hooks';
 import Loading from '@/components/common/Loading';
 import ResizedImage from '@/components/common/ResizedImage';
-import Text from '@/components/common/Text';
 import useEventLogger from '@/components/eventLogger/hooks/useEventLogger';
 import FeedDropdown from '@/components/feed/common/FeedDropdown';
-import MemberDetailSection from '@/components/members/detail/ActivitySection/MemberDetailSection';
-import MemberMeetingCard from '@/components/members/detail/ActivitySection/MemberMeetingCard';
-import MemberProjectCard from '@/components/members/detail/ActivitySection/MemberProjectCard';
 import CareerSection from '@/components/members/detail/CareerSection';
+import DetailInfoSection from '@/components/members/detail/DetailinfoSection';
 import EmptyProfile from '@/components/members/detail/EmptyProfile';
-import InfoItem from '@/components/members/detail/InfoItem';
+import GroupSection from '@/components/members/detail/GroupSection';
 import InterestSection from '@/components/members/detail/InterestSection';
+import ProjectSection from '@/components/members/detail/ProjectSection';
 import SoptActivitySection from '@/components/members/detail/SoptActivitySection';
 import { useBlockMember } from '@/components/members/hooks/useBlockMember';
 import { useReportMember } from '@/components/members/hooks/useReportMember';
-import { DEFAULT_DATE } from '@/components/members/upload/constants';
 import { playgroundLink } from '@/constants/links';
 import useEnterScreen from '@/hooks/useEnterScreen';
 import { useRunOnce } from '@/hooks/useRunOnce';
 import IconCoffee from '@/public/icons/icon-coffee.svg';
 import IconMore from '@/public/icons/icon-dots-vertical.svg';
 import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
-import { textStyles } from '@/styles/typography';
 import { safeParseInt } from '@/utils';
 
 interface MemberDetailProps {
   memberId: string;
 }
-
-const convertBirthdayFormat = (birthday?: string) => {
-  // FIXME: 서버쪽에 YYYY-MM-DD 형태로 무조건 업로드시 전송해줘야 하는 이슈가 있어서,
-  // 생년월일을 보내지 않았을 경우에 DEFAULT_DATE를 전송하도록 임시처리 해 두었습니다. 이를 클라에서 보여주기 위해 대응합니다.
-  if (birthday) {
-    const isDefaultDay = dayjs(birthday).isSame(dayjs(DEFAULT_DATE));
-    return isDefaultDay ? '' : dayjs(birthday).format('YYYY-MM-DD');
-  }
-  return '';
-};
 
 const MemberDetail: FC<MemberDetailProps> = ({ memberId }) => {
   const { logClickEvent, logPageViewEvent } = useEventLogger();
@@ -192,22 +177,7 @@ const MemberDetail: FC<MemberDetailProps> = ({ memberId }) => {
           )}
         </ProfileContainer>
 
-        {(profile.birthday || profile.address || profile.university || profile.address) && (
-          <MemberDetailSection style={{ gap: '30px' }}>
-            {profile.birthday && <InfoItem label='생년월일' content={convertBirthdayFormat(profile.birthday)} />}
-            {profile.university && <InfoItem label='학교'>{profile.university}</InfoItem>}
-            {profile.major && <InfoItem label='전공'>{profile.major}</InfoItem>}
-            {profile.address && (
-              <InfoItem label='활동 지역'>
-                <StyledAddressBadgeWrapper>
-                  {profile.address.split(',').map((address) => (
-                    <AddressBadge key={address}>{address}</AddressBadge>
-                  ))}
-                </StyledAddressBadgeWrapper>
-              </InfoItem>
-            )}
-          </MemberDetailSection>
-        )}
+        <DetailInfoSection profile={profile} />
 
         <SoptActivitySection soptActivities={sortedSoptActivities} />
 
@@ -257,80 +227,15 @@ const MemberDetail: FC<MemberDetailProps> = ({ memberId }) => {
           />
         )}
 
-        <ActivityContainer>
-          <ActivityTitle>{profile.name}님이 참여한 프로젝트</ActivityTitle>
-          {profile.projects.length > 0 && (
-            <>
-              <ActivitySub>{profile.projects.length}개의 프로젝트에 참여</ActivitySub>
-              <ActivityDisplay>
-                {profile.projects.map((project) => (
-                  <MemberProjectCard key={project.id} {...project} />
-                ))}
-              </ActivityDisplay>
-            </>
-          )}
-          {profile.projects.length === 0 && (
-            <>
-              <ActivitySub>아직 참여한 프로젝트가 없어요</ActivitySub>
-              {String(me?.id) === memberId && (
-                <ActivityUploadNudge>
-                  <Text typography='SUIT_14_M' style={{ textAlign: 'center', lineHeight: '24px' }}>
-                    참여한 프로젝트를 등록하면 <br />
-                    공식 홈페이지에도 프로젝트가 업로드 돼요!
-                  </Text>
-                  <ActivityUploadButton
-                    onClick={() =>
-                      logClickEvent('projectUpload', {
-                        referral: 'myPage',
-                      })
-                    }
-                    href={playgroundLink.projectUpload()}
-                  >
-                    + 내 프로젝트 올리기
-                  </ActivityUploadButton>
-                  <ActivityUploadMaskImg src='/icons/img/project-mask.png' alt='project-mask-image' height={317} />
-                </ActivityUploadNudge>
-              )}
-            </>
-          )}
-        </ActivityContainer>
-        <ActivityContainer>
-          <ActivityTitle>{profile.name}님이 참여한 모임</ActivityTitle>
-          {meetingList.length > 0 && (
-            <>
-              <ActivitySub>{meetingList.length}개의 모임에 참여</ActivitySub>
-              <ActivityDisplay>
-                {meetingList.map((meeting) => (
-                  <MemberMeetingCard
-                    key={meeting.id}
-                    {...meeting}
-                    {...(meeting.isMeetingLeader && { userName: profile.name })}
-                  />
-                ))}
-              </ActivityDisplay>
-              <Target ref={ref} />
-            </>
-          )}
-          {meetingList.length === 0 && (
-            <>
-              <ActivitySub>아직 참여한 모임이 없어요</ActivitySub>
-              {String(me?.id) === memberId && (
-                <ActivityUploadNudge>
-                  <Text typography='SUIT_14_M' style={{ textAlign: 'center', lineHeight: '24px' }}>
-                    모임을 참여하여 <br />
-                    SOPT 구성원들과의 추억을 쌓아보세요!
-                  </Text>
-                  <ActivityUploadButton href={playgroundLink.groupList()}>모임 둘러보러 가기</ActivityUploadButton>
-                  <ActivityUploadMaskImg src='/icons/img/meeting-mask.png' alt='meeting-mask-image' height={134} />
-                </ActivityUploadNudge>
-              )}
-            </>
-          )}
-        </ActivityContainer>
+        <ProjectSection profile={profile} memberId={memberId} meId={me?.id} />
+
+        <GroupSection profile={profile} meetingList={meetingList} ref={ref} meId={me?.id} memberId={memberId} />
       </Wrapper>
     </Container>
   );
 };
+
+export default MemberDetail;
 
 const Container = styled.div`
   display: flex;
@@ -528,120 +433,6 @@ const ContactWrapper = styled.div<{ shouldDivide: boolean }>`
     margin-top: 16px;
   }
 `;
-
-const StyledAddressBadgeWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
-  align-items: center;
-
-  @media ${MOBILE_MEDIA_QUERY} {
-    gap: 10px;
-  }
-`;
-
-const AddressBadge = styled.div`
-  border-radius: 13px;
-  background-color: ${colors.gray700};
-  padding: 6px 14px;
-  line-height: 16px;
-  color: ${colors.gray10};
-
-  ${textStyles.SUIT_14_M};
-`;
-
-const ActivityContainer = styled.div`
-  margin-top: 80px;
-`;
-
-const ActivityTitle = styled.div`
-  line-height: 100%;
-  font-size: 32px;
-  font-weight: 700;
-  @media ${MOBILE_MEDIA_QUERY} {
-    font-size: 22px;
-  }
-`;
-
-const ActivitySub = styled.div`
-  margin-top: 18px;
-  line-height: 100%;
-  color: #989ba0;
-  font-size: 22px;
-  font-weight: 500;
-  @media ${MOBILE_MEDIA_QUERY} {
-    margin-top: 10px;
-    font-size: 14px;
-  }
-`;
-
-const ActivityDisplay = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, minmax(10px, 1fr));
-  row-gap: 20px;
-  column-gap: 29px;
-  margin-top: 32px;
-
-  @media ${MOBILE_MEDIA_QUERY} {
-    display: flex;
-    flex-direction: column;
-    gap: 24px;
-    margin-top: 24px;
-  }
-`;
-
-const ActivityUploadNudge = styled.div`
-  display: flex;
-  position: relative;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 60px;
-  border-radius: 30px;
-  background-color: ${colors.gray800};
-  height: 317px;
-
-  @media ${MOBILE_MEDIA_QUERY} {
-    padding: 20px;
-    height: 212px;
-  }
-`;
-
-const ActivityUploadMaskImg = styled(ResizedImage)`
-  position: absolute;
-  max-height: 317px;
-  object-fit: cover;
-
-  @media ${MOBILE_MEDIA_QUERY} {
-    top: 0;
-    max-height: 134px;
-  }
-`;
-
-const ActivityUploadButton = styled(Link)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index: 1;
-  margin-top: 24px;
-  border-radius: 14px;
-  background-color: ${colors.gray10};
-  padding: 14px 48px;
-  color: ${colors.gray800};
-
-  ${textStyles.SUIT_15_SB};
-
-  @media ${MOBILE_MEDIA_QUERY} {
-    margin-top: 50px;
-    width: 100%;
-  }
-`;
-
-const Target = styled.div`
-  width: 100%;
-`;
-
-export default MemberDetail;
 
 const ImageSection = styled.div`
   position: relative;
