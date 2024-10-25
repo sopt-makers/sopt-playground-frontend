ab07f82cf0384c4292fcc13fdcb4c95f962ccb31
Seojisoo20191941 <amy0480@naver.com>
2024-10-25 23:15:03 +0900

    chore: 불필요 코드 삭제

--- src/components/coffeechat/upload/CoffeechatForm/UploadButton/index.tsx
+++ src/components/coffeechat/upload/CoffeechatForm/UploadButton/index.tsx
@@ -1,56 +1,3 @@
-// import styled from '@emotion/styled';
-// import { IconPlus } from '@sopt-makers/icons';
-// import { Button } from '@sopt-makers/ui';
-// import { useFormContext } from 'react-hook-form';
-
-// import { CoffeechatFormContent } from '@/components/coffeechat/upload/CoffeechatForm/types';
-// import Responsive from '@/components/common/Responsive';
-
-// interface UploadButtonProps {
-//   onSubmit: (data: CoffeechatFormContent) => void;
-// }
-
-// export default function UploadButton({ onSubmit }: UploadButtonProps) {
-//   const { handleSubmit, getValues } = useFormContext();
-
-//   const submitCoffeechat = () => {
-//     console.log('Asdf');
-//     onSubmit({
-//       memberInfo: {
-//         career: getValues('memberInfo.career') ? getValues('memberInfo.career')[0] : null,
-//         introduction: getValues('memberInfo.introduction'),
-//       },
-//       coffeeChatInfo: {
-//         sections: getValues('coffeeChatInfo.sections'),
-//         bio: getValues('coffeeChatInfo.bio'),
-//         topicTypes: getValues('coffeeChatInfo.topicTypes'),
-//         topic: getValues('coffeeChatInfo.topic'),
-//         meetingType: getValues('coffeeChatInfo.meetingType'),
-//         guideline: getValues('coffeeChatInfo.guideline'),
-//       },
-//     });
-//   };
-
-//   return (
-//     <div onClick={handleSubmit(submitCoffeechat)}>
-//       <Responsive only='desktop'>
-//         <Button size='md' LeftIcon={IconPlus} type='button'>
-//           커피챗 오픈하기
-//         </Button>
-//       </Responsive>
-//       <Responsive only='mobile'>
-//         <MobileButton size='lg' LeftIcon={IconPlus} type='button'>
-//           커피챗 오픈하기
-//         </MobileButton>
-//       </Responsive>
-//     </div>
-//   );
-// }
-
-// const MobileButton = styled(Button)`
-//   width: 100%;
-// `;
-
 import styled from '@emotion/styled';
 import { IconPlus } from '@sopt-makers/icons';
 import { Button } from '@sopt-makers/ui';
@@ -61,12 +8,12 @@ export default function UploadButton() {
   return (
     <div>
       <Responsive only='desktop'>
-        <Button size='md' LeftIcon={IconPlus} type='submit'>
+        <Button size='md' LeftIcon={IconPlus} type='button'>
           커피챗 오픈하기
         </Button>
       </Responsive>
       <Responsive only='mobile'>
-        <MobileButton size='lg' LeftIcon={IconPlus} type='submit'>
+        <MobileButton size='lg' LeftIcon={IconPlus} type='button'>
           커피챗 오픈하기
         </MobileButton>
       </Responsive>
