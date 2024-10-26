f16aecc3cf0545cb83275b284d6d1c73bcd4ff0a
hayoun <fishingest@kookmin.ac.kr>
2024-10-26 16:44:45 +0900

    fix: 상수 컴포넌트 수정

--- src/components/coffeechat/constants/index.ts
+++ src/components/coffeechat/constants/index.ts
@@ -4,15 +4,15 @@ export const COFFECHAT_SAMPLE_DATA = {
       memberId: 6,
       bio: "놀기 상담",
       topicTypeList: [
-          "직무 전문성"
+          "직무 전문성", "커리어"
       ],
       profileImage: null,
       name: "이솝트",
       career: "아직 없어요",
-      organization: null,
+      organization: "서울대학교",
       companyJob: null,
       soptActivities: [
-          "29기 기획"
+          "35기 기획"
       ]
     },
     {
@@ -44,14 +44,65 @@ export const COFFECHAT_SAMPLE_DATA = {
       profileImage: null,
       name: "김솝트",
       career: "미들 (4-8년)",
-      organization: null,
-      companyJob: null,
+      organization: "Google Korea",
+      companyJob: "디자인",
       soptActivities: [
           "28기 기획"
       ]
   },
+  {
+    memberId: 3,
+    bio: "디자인 커리어 상담",
+    topicTypeList: [
+        "포트폴리오"
+    ],
+    profileImage: null,
+    name: "김솝트",
+    career: "미들 (4-8년)",
+    organization: "네이버",
+    companyJob: "백엔드 개발",
+    soptActivities: [
+        "35기 기획",
+        "28기 기획"
+    ]
+},
+{
+    memberId: 2,
+    bio: "기획 커리어 상담",
+    topicTypeList: [
+        "커리어"
+    ],
+    profileImage: null,
+    name: "박솝트",
+    career: "시니어 (9년 이상)",
+    organization: "국민대학교",
+
+    companyJob: null,
+    soptActivities: [
+        "29기 기획",
+        "29기 기획"
+    ]
+},
+{
+    memberId: 1,
+    bio: "백엔드 커리어 상담",
+    topicTypeList: [
+        "창업"
+    ],
+    profileImage: "https://s3.ap-northeast-2.amazonaws.com/sopt-makers-internal//dev/image/project/d06f0b1d-0f1f-4d2f-8d4a-1145074c3acd-Screenshot 2023-03-05 at 4.30.18 PM.png",
+    name: "송정우",
+    career: "주니어 (0-3년)",
+    organization: "솝트대학교",
+    companyJob: null,
+    soptActivities: [
+        "28기 서버 파트장",
+        "27기 서버",
+        "29기 안드로이드",
+        "30기 안드로이드 파트장\n\n",
+        "28기 서버 파트장"
+    ]
+}
   ],
-  totalCount: 3,
 };
 export const categoryList={
   categoryList:[
