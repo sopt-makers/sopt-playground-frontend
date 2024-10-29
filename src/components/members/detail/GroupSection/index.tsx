427697227fc6d95872c73c0427135fdab410421a
jerry <becomegoodguy@gmail.com>
2024-10-29 15:51:05 +0900

    refactor: 모임 섹션 라이팅 변경

--- src/components/members/detail/GroupSection/index.tsx
+++ src/components/members/detail/GroupSection/index.tsx
@@ -40,10 +40,11 @@ const GroupSection = ({ profile, meId, memberId }: GroupSectionProps) => {
 
   return (
     <Container>
-      <ActivityTitle>{profile.name}님이 참여한 모임</ActivityTitle>
-      {meetingList.length > 0 && (
+      {meetingList.length > 0 ? (
         <>
-          <ActivitySub>{meetingList.length}개의 모임에 참여</ActivitySub>
+          <ActivityTitle>
+            {profile.name}님이 참여한 {meetingList.length}개의 모임이에요!
+          </ActivityTitle>
           <ActivityDisplay>
             {meetingList.map((meeting) => (
               <MemberMeetingCard
@@ -55,21 +56,24 @@ const GroupSection = ({ profile, meId, memberId }: GroupSectionProps) => {
           </ActivityDisplay>
           <Target ref={ref} />
         </>
-      )}
-      {meetingList.length === 0 && (
+      ) : (
         <>
-          <ActivitySub>아직 참여한 모임이 없어요</ActivitySub>
-          {String(meId) === memberId && (
-            <ActivityUploadNudge>
-              <Text typography='SUIT_14_M' style={{ textAlign: 'center', lineHeight: '24px' }}>
-                모임을 참여하여 <br />
-                SOPT 구성원들과의 추억을 쌓아보세요!
-              </Text>
-              <ActivityUploadButton href={playgroundLink.groupList()}>
-                <Text typography='SUIT_15_SB'>모임 둘러보러 가기</Text>
-              </ActivityUploadButton>
-              <ActivityUploadMaskImg src='/icons/img/meeting-mask.png' alt='meeting-mask-image' height={134} />
-            </ActivityUploadNudge>
+          {String(meId) === memberId ? (
+            <>
+              <ActivityTitle>아직 참여한 모임이 없어요</ActivityTitle>
+              <ActivityUploadNudge>
+                <NudgeSubText typography='SUIT_16_M' style={{ textAlign: 'center', lineHeight: '24px' }}>
+                  모임을 참여하여 <br />
+                  SOPT 구성원들과의 추억을 쌓아보세요!
+                </NudgeSubText>
+                <ActivityUploadButton href={playgroundLink.groupList()}>
+                  <Text typography='SUIT_15_SB'>모임 둘러보러 가기</Text>
+                </ActivityUploadButton>
+                <ActivityUploadMaskImg src='/icons/img/meeting-mask.png' alt='meeting-mask-image' height={134} />
+              </ActivityUploadNudge>
+            </>
+          ) : (
+            <ActivityTitle>아직 {profile.name}님이 참여한 모임이 없어요</ActivityTitle>
           )}
         </>
       )}
@@ -89,18 +93,6 @@ const ActivityTitle = styled.div`
   font-weight: 700;
   @media ${MOBILE_MEDIA_QUERY} {
     font-size: 22px;
-  }
-`;
-
-const ActivitySub = styled.div`
-  margin-top: 18px;
-  line-height: 100%;
-  color: #989ba0;
-  font-size: 22px;
-  font-weight: 500;
-  @media ${MOBILE_MEDIA_QUERY} {
-    margin-top: 10px;
-    font-size: 14px;
   }
 `;
 
@@ -166,4 +158,10 @@ const ActivityUploadButton = styled(Link)`
 
 const Target = styled.div`
   width: 100%;
+`;
+
+const NudgeSubText = styled(Text)`
+  @media ${MOBILE_MEDIA_QUERY} {
+    font-size: 14px;
+  }
 `;
