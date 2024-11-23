5eaef99b9da61e435f1c1509e9875d742b82d110
Seojisoo20191941 <amy0480@naver.com>
2024-11-24 01:25:39 +0900

    feat: 11월24일 광고 구좌 변경

--- src/components/common/Banner/AdsBanner/constants/ads.ts
+++ src/components/common/Banner/AdsBanner/constants/ads.ts
@@ -8,9 +8,19 @@ export const ADS = [
       'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/20241111-20241126/ASAP_banner_desktop.png',
     url: 'https://www.beginwithasap.com/',
   },
+
+  {
+    // 김태희 님
+    id: 2,
+    moImage:
+      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/20241124-20241207/%E1%84%8F%E1%85%A5%E1%84%86%E1%85%B2%E1%84%82%E1%85%B5%E1%84%90%E1%85%B5+(%E1%84%86%E1%85%A9%E1%84%87%E1%85%A1%E1%84%8B%E1%85%B5%E1%86%AF+%E1%84%87%E1%85%A5%E1%84%8C%E1%85%A5%E1%86%AB).png',
+    pcImage:
+      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/20241124-20241207/%E1%84%8F%E1%85%A5%E1%84%86%E1%85%B2%E1%84%82%E1%85%B5%E1%84%90%E1%85%B5+(PC+%E1%84%87%E1%85%A5%E1%84%8C%E1%85%A5%E1%86%AB).png',
+    url: 'https://playground.sopt.org/group/detail?id=394&fbclid=PAZXh0bgNhZW0CMTEAAabm-2g8SKZjOhiJs17TJxwrBRLjAek6f3rxg89MpBrhCFR_KHvZ8DMZu3M_aem_H1uBBjFciE9mA3RMayJTfA',
+  },
   {
     // 메이커스 마케팅 오거나이저
-    id: 2,
+    id: 3,
     moImage:
       'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/%E1%84%91%E1%85%B3%E1%86%AF%E1%84%80%E1%85%B3+MO.png',
     pcImage:
