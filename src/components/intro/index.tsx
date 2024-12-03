b142b9b9cdb851dc87f245efe87eacd6f6cb27b4
jerry <becomegoodguy@gmail.com>
2024-12-03 22:45:39 +0900

    test: 세션 스토리지에 들어있는 path 확인

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 
+import useLastUnauthorized from '@/components/auth/util/useLastUnauthorized';
 import CatchPhraseSection from '@/components/intro/sections/CatchPhrase';
 import Entry from '@/components/intro/sections/Entry';
 import Footer from '@/components/intro/sections/Footer';
@@ -9,8 +10,11 @@ import ValueSection from '@/components/intro/sections/ValueSection';
 interface IntroProps {}
 
 const Intro: FC<IntroProps> = ({}) => {
+  const lastUnauthorized = useLastUnauthorized();
+
   return (
     <>
+      <p>path: {lastUnauthorized.popPath()}</p>
       <Login />
       <CatchPhraseSection />
       <ValueSection />
