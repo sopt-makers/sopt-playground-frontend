4bef7aba823a842f60f1fc6a048bc7ab9f577909
Seojisoo20191941 <amy0480@naver.com>
2024-12-22 00:56:25 +0900

    refactor: 광고 구좌 이미지 수정

--- src/components/common/Banner/AdsBanner/constants/ads.ts
+++ src/components/common/Banner/AdsBanner/constants/ads.ts
@@ -3,7 +3,7 @@ export const ADS = [
     // 송승훈 님
     id: 1,
     moImage:
-      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/20241222-/%E1%84%8F%E1%85%A5%E1%84%86%E1%85%B2%E1%84%82%E1%85%B5%E1%84%90%E1%85%B5+%E1%84%89%E1%85%A1%E1%86%BC%E1%84%83%E1%85%A1%E1%86%AB%E1%84%87%E1%85%A2%E1%84%82%E1%85%A5+(MO)_%E1%84%8E%E1%85%AC%E1%84%8C%E1%85%A9%E1%86%BC.png',
+      'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/20241222-/%E1%84%8F%E1%85%A5%E1%84%86%E1%85%B2%E1%84%82%E1%85%B5%E1%84%90%E1%85%B5+%E1%84%89%E1%85%A1%E1%86%BC%E1%84%83%E1%85%A1%E1%86%AB%E1%84%87%E1%85%A2%E1%84%82%E1%85%A5+(MO)_%E1%84%8E%E1%85%AC%E1%84%8E%E1%85%AC%E1%84%8C%E1%85%A9%E1%86%BC.png',
     pcImage:
       'https://sopt-makers-internal.s3.ap-northeast-2.amazonaws.com/prod/image/advertisements/20241222-/%E1%84%8F%E1%85%A5%E1%84%86%E1%85%B2%E1%84%82%E1%85%B5%E1%84%90%E1%85%B5+%E1%84%89%E1%85%A1%E1%86%BC%E1%84%83%E1%85%A1%E1%86%AB%E1%84%87%E1%85%A2%E1%84%82%E1%85%A5+(PC)_%E1%84%8E%E1%85%AC%E1%84%8C%E1%85%A9%E1%86%BC.png',
     url: 'https://www.miricanvas.com/campaign/end_of_year_2024?from=event_sopt',
