90b049de7a9fe7b826990285c89e954d705cb8b0
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2025-01-23 21:33:43 +0900

    fix: 이미지 다운로드 테스트용 페이지 모바일에서 안뜨는 현상 해결 (#1725)
    * fix: 이미지 다운로드 로직에서 document 접근시 window 객체 확인
    
    * fix: 이미지 테스트 페이지에서 버그 유발 로직 제거

--- src/components/resolution/read/hooks/useImageDownload.ts
+++ src/components/resolution/read/hooks/useImageDownload.ts
@@ -3,42 +3,42 @@ import { useRef } from 'react';
 
 const useImageDownload = (fileName: string) => {
   const ref = useRef<HTMLDivElement>(null);
+  const style = typeof window !== 'undefined' ? document.createElement('style') : null;
 
   const removePaddingCss = `
-  body > div:last-child > span + img {
-    display: inline !important;
-  }
-`;
-
-  const style = document.createElement('style');
+    body > div:last-child > span + img {
+      display: inline !important;
+    }
+  `;
 
   const createStyle = (css: string) => {
+    if (!style) return;
     style.appendChild(document.createTextNode(css));
     document.head.appendChild(style);
   };
 
   const removeStyle = () => {
-    if (style.parentNode) {
+    if (style?.parentNode) {
       style.parentNode.removeChild(style);
     }
   };
 
   const onClick = () => {
+    if (typeof window === 'undefined') return;
+
     createStyle(removePaddingCss);
     html2canvas(ref.current as HTMLDivElement, {
       backgroundColor: null,
       scale: 4,
     })
-      .then((canvas: HTMLCanvasElement) => {
+      .then((canvas) => {
         removeStyle();
         const link = document.createElement('a');
         link.href = canvas.toDataURL('image/png');
         link.download = `${fileName}.png`;
         link.click();
       })
-      .catch((error: Error) => {
-        console.error(error);
-      });
+      .catch(console.error);
   };
 
   return { ref, onClick };
