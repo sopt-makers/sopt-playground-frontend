4bcf24527450c4cd02fc5e72930a52b34321fa5c
Seojisoo20191941 <amy0480@naver.com>
2025-01-30 16:01:04 +0900

    feat: 메뉴 탭 바 구현

--- src/components/mySoptReport/index.tsx
+++ src/components/mySoptReport/index.tsx
@@ -1,3 +1,19 @@
+import ReportNav from '@/components/mySoptReport/ReportNav';
+import { useState } from 'react';
+
 export default function MySoptReport() {
-  return <></>;
+  const [activeTab, setActiveTab] = useState<'sopt' | 'playground' | 'my-pg'>('sopt');
+
+  const handleSetActive = (tab: 'sopt' | 'playground' | 'my-pg') => {
+    setActiveTab(tab);
+  };
+
+  // TODO: 각 컴포넌트 안에서 선언
+  // const ref = useIntersectionObserver(id, handleSetActive);
+
+  return (
+    <>
+      <ReportNav activeTab={activeTab} handleSetActive={handleSetActive} />
+    </>
+  );
 }
