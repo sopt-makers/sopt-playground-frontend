0e1feebad452a91e86749b7f9ec0e66a38675dc6
Seojisoo20191941 <amy0480@naver.com>
2025-01-30 16:58:13 +0900

    feat: 배너 구현

--- src/components/mySoptReport/index.tsx
+++ src/components/mySoptReport/index.tsx
@@ -1,3 +1,33 @@
+import MySoptReportBackground from '@/public/logos/my-sopt-report-background.svg';
+import MySoptReportImg from '@/public/logos/my-sopt-report.svg';
+import styled from '@emotion/styled';
+import { Button } from '@sopt-makers/ui';
+
 export default function MySoptReport() {
-  return <></>;
+  return (
+    <>
+      <MySoptReportBanner>
+        <div>
+          <MySoptReportImg />
+          <Button>마이 솝트 리포트 보러가기</Button>
+          <p>*데이터 집계 기준 : 2024.01.01 ~ 2024.12.31</p>
+        </div>
+        <MySoptReportBackground />
+      </MySoptReportBanner>
+    </>
+  );
 }
+
+const MySoptReportBanner = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+
+  & > div {
+    position: absolute;
+  }
+`;
+
+const MySoptReportBackgroundImg = styled(MySoptReportBackground)`
+  height: 600px;
+`;
