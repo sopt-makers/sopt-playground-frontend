e8688da712ebdf9792a4e8fd735555f48e32785d
imddoy <imddoy@duksung.ac.kr>
2025-03-23 21:51:42 +0900

    chore: 텍스트 중앙정렬

--- src/components/resolution/submit/PlaygroundGuideModal.tsx
+++ src/components/resolution/submit/PlaygroundGuideModal.tsx
@@ -109,9 +109,9 @@ export default PlaygroundGuideModal;
 const Card = ({ name, description, color, hover, icon, button, href }: CardProps) => {
   return (
     <StyledCard color={color} hover={hover} href={href}>
-      <Text typography='SUIT_14_SB' lineHeight={20}>
+      <Description typography='SUIT_14_SB' color={colors.black} lineHeight={20}>
         {description}
-      </Text>
+      </Description>
       {icon}
       <StyledButton color={button}>
         {name} 보러가기
@@ -123,8 +123,8 @@ const Card = ({ name, description, color, hover, icon, button, href }: CardProps
 
 const IconChevronRight = () => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' width='5' height='8' viewBox='0 0 5 8' fill='none'>
-      <path d='M1 7L4 4L1 1' stroke='white' stroke-width='1.125' stroke-linecap='round' stroke-linejoin='round' />
+    <svg xmlns='http://www.w3.org/2000/svg' width='13' height='12' viewBox='0 0 13 12' fill='none'>
+      <path d='M5 9L8 6L5 3' stroke='white' stroke-width='1.125' stroke-linecap='round' stroke-linejoin='round' />
     </svg>
   );
 };
