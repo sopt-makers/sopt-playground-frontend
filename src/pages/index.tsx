558ef6f8835d1d36fbca5e59afb0349164d5b631
moonseonghui <msh9980@naver.com>
2025-03-25 01:30:12 +0900

    feat: 최신 기수 유저에 한해 HomePopup 노출을 위한 HomePopupContainer 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,14 @@ import type { NextPage } from 'next';
 
 import AuthRequired from '@/components/auth/AuthRequired';
 import ActiveBannerSlot from '@/components/common/Banner/ActiveBannerSlot';
-import { HomePopup } from '@/components/common/HomePopup';
+import HomePopupContainer from '@/components/common/HomePopupContainer';
 import FeedHomePage from '@/components/feed/page/FeedHomePage';
 import { setLayout } from '@/utils/layout';
 
 const Home: NextPage = () => {
   return (
     <AuthRequired>
-      <HomePopup />
+      <HomePopupContainer />
       {/* 홈팝업 사용시에 주석제거 */}
       <ActiveBannerSlot />
       <FeedHomePage />
