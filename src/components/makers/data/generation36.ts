ffbb0474f8c0b9d2bf53076962204df4eaf69442
imddoy <imddoy@duksung.ac.kr>
2025-03-29 05:54:58 +0900

    feat: 멤버 수정

---
+++ src/components/makers/data/generation36.ts
@@ -0,0 +1,91 @@
+import { MakersGeneration } from '@/components/makers/data/types';
+
+export const generation35: MakersGeneration = {
+  title: '36기',
+  teams: [
+    {
+      title: 'makers lead',
+      people: [{ type: 'member', id: 812, name: '김가연' }],
+    },
+    {
+      title: 'makers organizer',
+      description:
+        'makers의 지속 가능한 활동 운영을 포함해서 makers의 문화를 만들어가며, 메이커들이 오너십을 가지고 제품을 더 잘 만들어갈 수 있도록 고민하는 역할을 해요.',
+      people: [
+        { type: 'member', id: 390, name: '백송현' },
+        { type: 'member', id: 794, name: '김세은' },
+        { type: 'member', id: 419, name: '박기현' },
+        { type: 'member', id: 1031, name: '이재영' },
+        { type: 'member', id: 982, name: '이해인' },
+      ],
+    },
+    {
+      title: 'SOPT Playground 팀',
+      description:
+        'SOPT 구성원들을 연결하는 Playground를 만드는 팀이에요. 모든 기수의 구성원들과 함께 이야기하고 소통할 수 있는 커뮤니티, 전체 회원들의 소개를 볼 수 있는 ‘멤버프로필’, 프로젝트를 등록할 수 있는 ‘프로젝트’, 가볍게 소통할 수 있는 ‘끝말잇기’ 기능을 통해, SOPT 구성원들의 가치 있는 연결을 만들어가고자 해요.',
+      people: [
+        { type: 'member', id: 809, name: '남지우', position: 'PM' },
+        { type: 'member', id: 238, name: '이서영', position: '프로덕트 디자이너' },
+        { type: 'member', id: 381, name: '황현진', position: '프로덕트 디자이너' },
+        { type: 'member', id: 797, name: '김채현', position: '프론트엔드 개발자' },
+        { type: 'member', id: 792, name: '문성희', position: '프론트엔드 개발자' },
+        { type: 'member', id: 221, name: '임주민', position: '백엔드 개발자' },
+        { type: 'member', id: 793, name: '정정교', position: '백엔드 개발자' },
+      ],
+    },
+    {
+      title: 'Product Crew 팀',
+      description: 'SOPT 회원들이 다양한 번개, 스터디, 행사를 만들고 참여할 수 있는 모임 서비스를 만들어요.',
+      people: [
+        { type: 'member', id: 371, name: '유주현', position: 'PM' },
+        { type: 'member', id: 760, name: '김가온', position: '프로덕트 디자이너' },
+        { type: 'member', id: 813, name: '이유지', position: '프로덕트 디자이너' },
+        { type: 'member', id: 918, name: '김민정', position: '프론트엔드 개발자' },
+        { type: 'member', id: 858, name: '이진', position: '프론트엔드 개발자' },
+        { type: 'member', id: 989, name: '김효준', position: '백엔드 개발자' },
+        { type: 'member', id: 744, name: '이동훈', position: '백엔드 개발자' },
+      ],
+    },
+    {
+      title: 'SOPT APP 팀',
+      description:
+        'SOPT 공식 앱을 만드는 팀이에요. SOPT를 더욱 재미있고, 알차게 활동할 수 있도록 도와주는 서비스를 만들고 있어요.',
+      people: [
+        { type: 'member', id: 89, name: '고은비', position: 'PM' },
+        { type: 'member', id: 312, name: '이성오', position: '프로덕트 디자이너' },
+        { type: 'member', id: 233, name: '강윤서', position: 'iOS 개발자' },
+        { type: 'member', id: 70, name: '이재현', position: 'iOS 개발자' },
+        { type: 'member', id: 172, name: '장석우', position: 'iOS 개발자' },
+        { type: 'member', id: 803, name: '박효빈', position: 'Android 개발자' },
+        { type: 'member', id: 899, name: '한민재', position: 'Android 개발자' },
+        { type: 'member', id: 771, name: '유영재', position: '백엔드 개발자' },
+        { type: 'member', id: 778, name: '황혜린', position: '백엔드 개발자' },
+      ],
+    },
+    {
+      title: '플랫폼 팀',
+      description:
+        '플랫폼 팀은 프로덕트 팀들의 중심에서 각 팀이 프로덕트에만 집중할 수 있도록, 모든 팀 공통의 문제를 고민하고 해결해요.',
+      people: [
+        { type: 'member', id: 812, name: '김가연', position: 'PM' },
+        { type: 'member', id: 871, name: '서대원', position: '프론트엔드 개발자' },
+        { type: 'member', id: 784, name: '최주용', position: '프론트엔드 개발자' },
+        { type: 'member', id: 309, name: '김성은', position: '백엔드 개발자' },
+        { type: 'member', id: 952, name: '서연진', position: '백엔드 개발자' },
+        { type: 'member', id: 861, name: '신정윤', position: '백엔드 개발자' },
+      ],
+    },
+    {
+      title: '어드민 팀',
+      description: '리서치 TF는 메이커스 여러 프로덕트와 SOPT 회원을 연결하는 역할을 해요.',
+      people: [
+        { type: 'member', id: 823, name: '김소희', position: 'PM' },
+        { type: 'member', id: 795, name: '노하은', position: '프로덕트 디자이너' },
+        { type: 'member', id: 741, name: '김규홍', position: '프론트엔드 개발자' },
+        { type: 'member', id: 689, name: '남다은', position: '프론트엔드 개발자' },
+        { type: 'member', id: 569, name: '임찬기', position: '백엔드 개발자' },
+        { type: 'member', id: 826, name: '최영철', position: '백엔드 개발자' },
+      ],
+    },
+  ],
+};
