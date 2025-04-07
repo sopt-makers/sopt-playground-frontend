53cb4e0549e29d38ac88ba0f502715c629039cbc
imddoy <imddoy@duksung.ac.kr>
2025-04-07 19:31:26 +0900

    feat: 36기 팀 소개 변경

--- src/components/makers/data/generation36.ts
+++ src/components/makers/data/generation36.ts
@@ -8,9 +8,9 @@ export const generation36: MakersGeneration = {
       people: [{ type: 'member', id: 812, name: '김가연' }],
     },
     {
-      title: 'makers organizer',
+      title: 'Organizer 팀',
       description:
-        'makers의 지속 가능한 활동 운영을 포함해서 makers의 문화를 만들어가며, 메이커들이 오너십을 가지고 제품을 더 잘 만들어갈 수 있도록 고민하는 역할을 해요.',
+        '메이커스에서 CX(Customer Experience)를 담당하는 팀이에요. 메이커스 팀과 제품의 가치를 SOPT에 전달하고, 지속가능한 조직의 운영 방향성을 만들어요.',
       people: [
         { type: 'member', id: 390, name: '백송현' },
         { type: 'member', id: 794, name: '김세은' },
@@ -20,22 +20,9 @@ export const generation36: MakersGeneration = {
       ],
     },
     {
-      title: 'SOPT Playground 팀',
+      title: 'Crew 팀',
       description:
-        'SOPT 구성원들을 연결하는 Playground를 만드는 팀이에요. 모든 기수의 구성원들과 함께 이야기하고 소통할 수 있는 커뮤니티, 전체 회원들의 소개를 볼 수 있는 ‘멤버프로필’, 프로젝트를 등록할 수 있는 ‘프로젝트’, 가볍게 소통할 수 있는 ‘끝말잇기’ 기능을 통해, SOPT 구성원들의 가치 있는 연결을 만들어가고자 해요.',
-      people: [
-        { type: 'member', id: 809, name: '남지우', position: 'PM' },
-        { type: 'member', id: 238, name: '이서영', position: '프로덕트 디자이너' },
-        { type: 'member', id: 381, name: '황현진', position: '프로덕트 디자이너' },
-        { type: 'member', id: 797, name: '김채현', position: '프론트엔드 개발자' },
-        { type: 'member', id: 792, name: '문성희', position: '프론트엔드 개발자' },
-        { type: 'member', id: 221, name: '임주민', position: '백엔드 개발자' },
-        { type: 'member', id: 793, name: '정정교', position: '백엔드 개발자' },
-      ],
-    },
-    {
-      title: 'Product Crew 팀',
-      description: 'SOPT 회원들이 다양한 번개, 스터디, 행사를 만들고 참여할 수 있는 모임 서비스를 만들어요.',
+        'Crew 팀은 SOPT 회원들이 스터디 및 모임을 쉽고 빠르게 개설할 수 있도록 적극적으로 돕고 있어요. SOPT 회원들이 모임 서비스를 통해 활기차고 즐거운 SOPT 생활을 누릴 수 있도록, 안정적인 모임 플랫폼 구축에 힘쓰고 있어요.',
       people: [
         { type: 'member', id: 371, name: '유주현', position: 'PM' },
         { type: 'member', id: 760, name: '김가온', position: '프로덕트 디자이너' },
@@ -47,9 +34,36 @@ export const generation36: MakersGeneration = {
       ],
     },
     {
+      title: 'Playground 팀',
+      description:
+        '3,000명의 SOPT가 연결되는 공간, Playground를 만드는 팀이에요. 역대 기수들과 자유롭게 소통할 수 있는 공간인 커뮤니티와 커피솝, 멤버프로필과 프로젝트 기능을 운영하며  SOPT 구성원들의 가치 있는 연결을 만들어가고자 해요.',
+      people: [
+        { type: 'member', id: 809, name: '남지우', position: 'PM' },
+        { type: 'member', id: 238, name: '이서영', position: '프로덕트 디자이너' },
+        { type: 'member', id: 381, name: '황현진', position: '프로덕트 디자이너' },
+        { type: 'member', id: 797, name: '김채현', position: '프론트엔드 개발자' },
+        { type: 'member', id: 792, name: '문성희', position: '프론트엔드 개발자' },
+        { type: 'member', id: 221, name: '임주민', position: '백엔드 개발자' },
+        { type: 'member', id: 793, name: '정정교', position: '백엔드 개발자' },
+      ],
+    },
+    {
+      title: '어드민 팀',
+      description:
+        'SOPT 어드민팀은 공홈부터 지원서, 출석, 배너 어드민까지 흩어져 있던 시스템을 하나로 통합해, 내부 운영을 더 빠르고 효율적으로 만들어가고 있어요. 눈에 잘 띄지는 않지만, 꼭 필요한 운영의 기반을 든든하게 책임지는 팀이에요.',
+      people: [
+        { type: 'member', id: 823, name: '김소희', position: 'PM' },
+        { type: 'member', id: 795, name: '노하은', position: '프로덕트 디자이너' },
+        { type: 'member', id: 741, name: '김규홍', position: '프론트엔드 개발자' },
+        { type: 'member', id: 689, name: '남다은', position: '프론트엔드 개발자' },
+        { type: 'member', id: 569, name: '임찬기', position: '백엔드 개발자' },
+        { type: 'member', id: 826, name: '최영철', position: '백엔드 개발자' },
+      ],
+    },
+    {
       title: 'SOPT APP 팀',
       description:
-        'SOPT 공식 앱을 만드는 팀이에요. SOPT를 더욱 재미있고, 알차게 활동할 수 있도록 도와주는 서비스를 만들고 있어요.',
+        '내 손 안의 SOPT, 연결과 재미의 가치를 창출하는 공식 앱을 만드는 App팀이에요. 세미나 출석기능으로 시작하여, 솝탬프, 콕찌르기, 오늘의솝마디 등 회원들의 즐거운 SOPT 경험을 위한 시도를 하는 팀이에요.',
       people: [
         { type: 'member', id: 89, name: '고은비', position: 'PM' },
         { type: 'member', id: 312, name: '이성오', position: '프로덕트 디자이너' },
@@ -65,7 +79,7 @@ export const generation36: MakersGeneration = {
     {
       title: '플랫폼 팀',
       description:
-        '플랫폼 팀은 프로덕트 팀들의 중심에서 각 팀이 프로덕트에만 집중할 수 있도록, 모든 팀 공통의 문제를 고민하고 해결해요.',
+        '플랫폼 팀은 프로덕트 팀들의 중심에서 각 팀이 프로덕트에만 집중할 수 있도록, 모든 팀 공통의 문제를 고민하고 해결해요. 탄탄한 기술적 기반을 위해 인증 중앙화, 알림 어드민 등 다양한 노력을 하고 있는 팀이에요.',
       people: [
         { type: 'member', id: 812, name: '김가연', position: 'PM' },
         { type: 'member', id: 871, name: '서대원', position: '프론트엔드 개발자' },
@@ -73,18 +87,6 @@ export const generation36: MakersGeneration = {
         { type: 'member', id: 309, name: '김성은', position: '백엔드 개발자' },
         { type: 'member', id: 952, name: '서연진', position: '백엔드 개발자' },
         { type: 'member', id: 861, name: '신정윤', position: '백엔드 개발자' },
-      ],
-    },
-    {
-      title: '어드민 팀',
-      description: '리서치 TF는 메이커스 여러 프로덕트와 SOPT 회원을 연결하는 역할을 해요.',
-      people: [
-        { type: 'member', id: 823, name: '김소희', position: 'PM' },
-        { type: 'member', id: 795, name: '노하은', position: '프로덕트 디자이너' },
-        { type: 'member', id: 741, name: '김규홍', position: '프론트엔드 개발자' },
-        { type: 'member', id: 689, name: '남다은', position: '프론트엔드 개발자' },
-        { type: 'member', id: 569, name: '임찬기', position: '백엔드 개발자' },
-        { type: 'member', id: 826, name: '최영철', position: '백엔드 개발자' },
       ],
     },
   ],
