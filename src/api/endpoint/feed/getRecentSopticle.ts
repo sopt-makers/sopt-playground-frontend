064d0dbc8f5899a7a93e9621fbe6702daa024f54
moonseonghui <msh9980@naver.com>
2025-05-20 17:07:14 +0900

    feat: getRecentSopticle api 구현

---
+++ src/api/endpoint/feed/getRecentSopticle.ts
@@ -0,0 +1,55 @@
+import { useQuery } from '@tanstack/react-query';
+import { z } from 'zod';
+
+import { createEndpoint } from '@/api/typedAxios';
+
+const ActivitySchema = z.object({
+  id: z.number(),
+  memberId: z.number(),
+  part: z.string(),
+  generation: z.number(),
+  team: z.string().nullable(),
+});
+
+const CareerSchema = z.object({
+  id: z.number(),
+  memberId: z.number(),
+  companyName: z.string(),
+  title: z.string(),
+  startDate: z.string(),
+  endDate: z.string(),
+  isCurrent: z.boolean(),
+});
+
+const MemberSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  profileImage: z.string().nullable(),
+  activity: ActivitySchema,
+  careers: CareerSchema.nullable(),
+});
+
+const recentSopticleSchema = z.object({
+  id: z.number(),
+  member: MemberSchema,
+  createdAt: z.string(),
+  title: z.string(),
+  content: z.string(),
+  sopticleUrl: z.string(),
+});
+
+export type RecentSopticleType = z.infer<typeof recentSopticleSchema>;
+
+const getRecentSopticles = createEndpoint({
+  request: {
+    method: 'GET',
+    url: '/api/v1/community/posts/sopticle',
+  },
+  serverResponseScheme: z.array(recentSopticleSchema),
+});
+
+export const useRecentSopticles = () =>
+  useQuery({
+    queryKey: ['recentSopticles'],
+    queryFn: () => getRecentSopticles.request(),
+  });
