6b3d271ab1abe339f4d392b9411af79e091a3c81
imddoy <imddoy@duksung.ac.kr>
2025-05-20 19:46:35 +0900

    fix: 스토리 파일 args 변경 반영

--- src/components/feed/home/PopularCard/PopularCard.stories.tsx
+++ src/components/feed/home/PopularCard/PopularCard.stories.tsx
@@ -11,9 +11,14 @@ const Template: ComponentStory<typeof PopularCard> = (args) => <PopularCard {...
 export const Default = Template.bind({});
 Default.args = {
   rank: 1,
-  category: '자유',
-  title: '여기는 제목입니다',
-  profileImage: 'https://avatars.githubusercontent.com/u/90364711?v=4',
-  name: '김솝트',
-  hits: 432,
+  card: {
+    id: 1,
+    category: '자유',
+    title: '여기는 제목입니다',
+    member: {
+      profileImage: 'https://avatars.githubusercontent.com/u/90364711?v=4',
+      name: '김솝트',
+    },
+    hits: 432,
+  },
 };
