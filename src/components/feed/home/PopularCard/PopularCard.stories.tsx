75f95288c3d96c3a5978a543bb265b21ddadf789
또이 <90364711+imddoy@users.noreply.github.com>
2025-05-24 17:22:30 +0900

    feat: 커뮤니티 홈 인기글 컴포넌트 구현 (#1850)
    * feat: 인기글 컴포넌트 구현
    
    * feat: 인기글 리스트 컴포넌트 구현
    
    * feat: 인기글 api 연결
    
    * feat: 로딩 처리
    
    * feat: 마우스 이벤트 추가
    
    * chore: 타이틀 왼쪽 정렬
    
    * fix: 스토리 파일 args 변경 반영
    
    * chore: 홈 화면으로 컴포넌트 이동
    
    * fix: 코드리뷰 반영
    
    * fix: 오탈자 수정
    
    * chore: 카테고리 id 없으면 기존 카테고리에서 상세로 가도록

---
+++ src/components/feed/home/PopularCard/PopularCard.stories.tsx
@@ -0,0 +1,24 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import PopularCard from './PopularCard';
+
+export default {
+  component: PopularCard,
+} as ComponentMeta<typeof PopularCard>;
+
+const Template: ComponentStory<typeof PopularCard> = (args) => <PopularCard {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  rank: 1,
+  card: {
+    id: 1,
+    category: '자유',
+    title: '여기는 제목입니다',
+    member: {
+      profileImage: 'https://avatars.githubusercontent.com/u/90364711?v=4',
+      name: '김솝트',
+    },
+    hits: 432,
+  },
+};
---
+++ src/components/feed/home/PopularCard/index.tsx
@@ -0,0 +1,79 @@
+import { useGetPopularPost } from '@/api/endpoint/feed/getPopularPost';
+import Text from '@/components/common/Text';
+import { categoryIdNameMap } from '@/components/feed/common/utils';
+import PopularCard from '@/components/feed/home/PopularCard/PopularCard';
+import { MB_SM_MEDIA_QUERY } from '@/styles/mediaQuery';
+import styled from '@emotion/styled';
+import { colors } from '@sopt-makers/colors';
+import { useRouter } from 'next/router';
+
+const PopularCardList = () => {
+  const { data, isLoading, isError } = useGetPopularPost();
+  const router = useRouter();
+
+  const handleClickPopular = (category: string, feedId: number) => {
+    const categoryId = Object.entries(categoryIdNameMap).find(([_, name]) => name === category)?.[0];
+
+    if (!categoryId) {
+      router.push(`/?feed=${feedId}`);
+    }
+    router.push(`/?category=${categoryId}&feed=${feedId}`);
+  };
+
+  return (
+    <>
+      <TitleWrapper>
+        <Text typography='SUIT_18_B' color={colors.white} lineHeight={28}>
+          실시간 인기글 🚀
+        </Text>
+        <Text typography='SUIT_12_SB' color={colors.gray400} lineHeight={16}>
+          이번 주 동안 가장 많은 솝트인이 봤어요!
+        </Text>
+      </TitleWrapper>
+      <ContentWrapper>
+        {isError && (
+          <Text
+            typography='SUIT_14_M'
+            color={colors.gray300}
+            lineHeight={16}
+            style={{ textAlign: 'center', padding: '80px' }}
+          >
+            인기글을 보여주는데 문제가 발생했어요.
+          </Text>
+        )}
+        {isLoading &&
+          Array.from({ length: 3 }).map((_, index) => (
+            <PopularCard key={`skeleton-${index}`} rank={index + 1} isLoading />
+          ))}
+        {data?.map((card, index) => (
+          <PopularCard
+            key={card.id}
+            rank={index + 1}
+            card={card}
+            onClick={() => handleClickPopular(card.category, card.id)}
+          />
+        ))}
+      </ContentWrapper>
+    </>
+  );
+};
+
+export default PopularCardList;
+
+const TitleWrapper = styled.h1`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  margin-bottom: 16px;
+  text-align: left;
+
+  @media ${MB_SM_MEDIA_QUERY} {
+    flex-direction: column;
+  }
+`;
+
+const ContentWrapper = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
