560554b24383fe1c5d8cd64e7b0f16df82c99159
moonseonghui <msh9980@naver.com>
2025-05-29 16:22:12 +0900

    fix: 솝티클 캐러셀 자동 스크롤 속도 4초로 수정

--- src/components/feed/home/SopticleArea/index.tsx
+++ src/components/feed/home/SopticleArea/index.tsx
@@ -17,7 +17,7 @@ const SopticleArea = () => {
 
   const { containerRef, activeIndex, getActualIndex, scrollToIndex } = useScrollCarousel({
     itemCount: sopticles.length,
-    autoSlideInterval: 3000,
+    autoSlideInterval: 4000,
   });
 
   const navigateToSopticle = () => {
