f1cb061b0d9f0c1cfd45eb3fe5ff435247cdb39a
imddoy <imddoy@duksung.ac.kr>
2025-07-01 16:53:58 +0900

    refactor: 투표 선택 성공 응답으로 쿼리 반영하기

--- src/api/endpoint/feed/postVote.ts
+++ src/api/endpoint/feed/postVote.ts
@@ -1,15 +1,32 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { InfiniteData, useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { z } from 'zod';
 
-import { useGetPostsInfiniteQuery } from '@/api/endpoint/feed/getPosts';
+import { PostsType, useGetPostsInfiniteQuery } from '@/api/endpoint/feed/getPosts';
 import { getWaitingQuestions } from '@/api/endpoint/feed/getWaitingQuestions';
 import { createEndpoint } from '@/api/typedAxios';
-import { getPost } from '@/api/endpoint/feed/getPost';
+import { getPost, PostType } from '@/api/endpoint/feed/getPost';
 import { getParentCategoryId } from '@/components/feed/common/utils';
 import { getCategory } from '@/api/endpoint/feed/getCategory';
+import { produce } from 'immer';
 
 const OptionSchema = z.object({
   selectedOptions: z.array(z.number().int()).min(1).max(5),
+});
+
+const VoteResponseSchema = z.object({
+  id: z.number(),
+  isMultiple: z.boolean(),
+  hasVoted: z.boolean(),
+  totalParticipants: z.number(),
+  options: z.array(
+    z.object({
+      id: z.number(),
+      content: z.string(),
+      voteCount: z.number(),
+      votePercent: z.number(),
+      isSelected: z.boolean(),
+    }),
+  ),
 });
 
 export const postVote = createEndpoint({
@@ -18,7 +35,7 @@ export const postVote = createEndpoint({
     url: `api/v1/community/posts/${postId}/vote`,
     data: requestBody,
   }),
-  serverResponseScheme: z.record(z.boolean()),
+  serverResponseScheme: VoteResponseSchema,
 });
 
 export const usePostVoteMutation = (
@@ -38,11 +55,31 @@ export const usePostVoteMutation = (
 
   return useMutation({
     mutationFn: (requestBody: z.infer<typeof OptionSchema>) => postVote.request(postId, requestBody),
-    onSuccess: () => {
+    onSuccess: (data: z.infer<typeof VoteResponseSchema>) => {
       options?.onSuccess?.();
-      queryClient.refetchQueries({ queryKey: getPost.cacheKey(String(postId)) });
-      queryClient.refetchQueries({ queryKey: useGetPostsInfiniteQuery.getKey(parentCategoryId?.toString()) });
-      queryClient.refetchQueries({ queryKey: getWaitingQuestions.cacheKey() });
+      queryClient.setQueryData(getPost.cacheKey(String(postId)), (oldData: PostType) => {
+        return produce(oldData, (draft) => {
+          if (draft && draft.posts) {
+            draft.posts.vote = data;
+          }
+        });
+      });
+      queryClient.setQueryData(
+        useGetPostsInfiniteQuery.getKey(parentCategoryId?.toString()),
+        (oldData: InfiniteData<PostsType>) => {
+          return produce(oldData, (draft) => {
+            if (draft && draft.pages) {
+              draft.pages.forEach((page) => {
+                const post = page.posts.find((p) => p.id === postId);
+                if (post) {
+                  post.vote = data;
+                }
+              });
+            }
+          });
+        },
+      );
+      queryClient.invalidateQueries({ queryKey: getWaitingQuestions.cacheKey() });
     },
   });
 };
