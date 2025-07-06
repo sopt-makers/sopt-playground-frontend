e22087619b01af92e414a7723509172edb59e379
imddoy <imddoy@duksung.ac.kr>
2025-07-06 15:53:44 +0900

    feat: 투표 글자수 길면 내려가도록 공간 배치

--- src/components/vote/RadioBox.tsx
+++ src/components/vote/RadioBox.tsx
@@ -29,7 +29,7 @@ const RadioBox = ({
   return (
     <Container onClick={onClick} role='button' mode={mode} isSelected={isSelected}>
       {isSelected && <CheckedIcon />}
-      <Text typography='SUIT_14_R' color={colors.gray10} style={{ zIndex: 1 }}>
+      <Text typography='SUIT_14_R' color={colors.gray10} style={{ zIndex: 1, flex: 1 }}>
         {content}
       </Text>
       {isResult && (
@@ -79,8 +79,6 @@ const CheckedIcon = styled(IconCheck)`
 `;
 
 const VoteResult = styled.span<{ isWinner: boolean }>`
-  position: absolute;
-  right: 12px;
   z-index: 1;
   color: ${({ isWinner }) => (isWinner ? colors.secondary : colors.gray100)};
   font: ${fonts.BODY_13_M};
