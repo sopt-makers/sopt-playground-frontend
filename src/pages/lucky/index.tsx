1d0c7215cb0d472832e2b338c8f367b0d6dc0d8c
moonseonghui <msh9980@naver.com>
2025-07-25 17:16:23 +0900

    feat: luckypick api 구현

--- src/pages/lucky/index.tsx
+++ src/pages/lucky/index.tsx
@@ -8,14 +8,17 @@ import LuckyLoading from '@/components/luckydraw/LuckyLoading';
 import LuckyResult from '@/components/luckydraw/LuckyResult';
 import LuckyWinnerGuide from '@/components/luckydraw/LuckyWinnerGuide';
 import LuckyReady from '@/components/luckydraw/LuckyReady';
+import { useGetLuckyPick } from '@/api/endpoint/resolution/getLuckyPick';
 
 type Step = 'ready' | 'loading' | 'result' | 'winnerGuide';
 
 const Lucky = () => {
   const router = useRouter();
   const [step, setStep] = useState<Step>('ready');
-  const [isWinner, setIsWinner] = useState(() => Math.random() < 0.5); // 일단 랜덤으로
-  const { data: { name } = {}, isLoading } = useGetMemberProfileOfMe();
+  const { data: { name } = {}, isLoading: isNameLoading } = useGetMemberProfileOfMe();
+  const { data: luckyPickResult, isLoading: isLuckyPickLoading } = useGetLuckyPick(step === 'loading');
+
+  const isWinner = luckyPickResult?.isWinner ?? false;
 
   const goToLoading = () => setStep('loading');
   const goToResult = () => setStep('result');
@@ -38,11 +41,15 @@ const Lucky = () => {
     <AuthRequired>
       {step === 'ready' && <LuckyReady onStart={goToLoading} />}
       {step === 'loading' && <LuckyLoading onComplete={goToResult} />}
-      {step === 'result' && <LuckyResult isWinner={isWinner} username={name || ''} onClickButton={handleClickButton} />}
+      {step === 'result' &&
+        (isNameLoading || isLuckyPickLoading ? (
+          <LuckyLoading onComplete={goToResult} />
+        ) : (
+          <LuckyResult isWinner={isWinner} username={name || ''} onClickButton={handleClickButton} />
+        ))}
       {step === 'winnerGuide' && <LuckyWinnerGuide onClickNext={handleClickFinalButton} />}
     </AuthRequired>
   );
 };
 
-setLayout(Lucky, 'header');
 export default Lucky;
