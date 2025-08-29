48880e6d537515b7ef0a366070fa8142b80f87fc
j-nary <dlwls0921@naver.com>
2025-08-29 22:33:46 +0900

    feat: CrewFeedList 구현

---
+++ src/components/feed/list/CrewFeedList/CrewFeedListItem.tsx
@@ -0,0 +1,118 @@
+import { colors } from '@sopt-makers/colors';
+import { IconAlertTriangle, IconShare } from '@sopt-makers/icons';
+import { Flex } from '@toss/emotion-utils';
+
+import { useGetCrewPostQuery } from '@/api/crew/getCrewPost';
+import Text from '@/components/common/Text';
+import { LoggingClick } from '@/components/eventLogger/components/LoggingClick';
+import FeedDropdown from '@/components/feed/common/FeedDropdown';
+import FeedLike from '@/components/feed/common/FeedLike';
+import { useReportFeed } from '@/components/feed/common/hooks/useReportFeed';
+import { useShareFeed } from '@/components/feed/common/hooks/useShareFeed';
+import { getRelativeTime } from '@/components/feed/common/utils';
+import FeedCard from '@/components/feed/list/FeedCard';
+
+interface CrewFeedListItemProps {
+  meetingId: number;
+  postId: number;
+  onFeedCardClick: () => void;
+  memberInfo: string;
+}
+
+const CrewFeedListItem = ({ meetingId, postId, onFeedCardClick, memberInfo }: CrewFeedListItemProps) => {
+  const { data: post } = useGetCrewPostQuery(postId);
+
+  const { handleShareFeed } = useShareFeed();
+  const { handleReport } = useReportFeed();
+
+  const handleFeedLike = () => {
+    console.log('handleFeedLike');
+  };
+
+  console.info({ crewPost: post });
+
+  if (!post) {
+    return null;
+  }
+
+  return (
+    <FeedCard
+      onClick={onFeedCardClick}
+      name={post.user.name}
+      title={post.title}
+      content={post.contents}
+      profileImage={post.user.profileImage}
+      createdAt={getRelativeTime(post.createdDate)}
+      commentLength={post.commentCount}
+      hits={post.viewCount}
+      isShowInfo={false}
+      memberId={post.user.id}
+      thumbnailUrl={post.meeting.imageURL[0].url}
+      sopticleUrl={''}
+      info={
+        <>
+          <Text typography='SUIT_14_R' lineHeight={20} color={colors.gray400} style={{ margin: '0 2px' }}>
+            ∙
+          </Text>
+          {memberInfo}
+          <Text typography='SUIT_14_R' lineHeight={20} color={colors.gray400} style={{ margin: '0 2px' }}>
+            ∙
+          </Text>
+          {getRelativeTime(post.createdDate)}
+        </>
+      }
+      rightIcon={
+        <FeedDropdown
+          trigger={
+            <Flex as='button'>
+              <FeedCard.Icon />
+            </Flex>
+          }
+        >
+          <LoggingClick eventKey='feedShareButton' param={{ feedId: String(post.id), referral: 'list' }}>
+            <FeedDropdown.Item
+              onClick={(e) => {
+                e.stopPropagation();
+                handleShareFeed(`${post.id}`);
+              }}
+            >
+              <Flex align='center' css={{ gap: '10px', color: `${colors.gray10}` }}>
+                <IconShare css={{ width: '16px', height: '16px' }} />
+                공유
+              </Flex>
+            </FeedDropdown.Item>
+          </LoggingClick>
+          <FeedDropdown.Item
+            onClick={(e) => {
+              e.stopPropagation();
+              handleReport({ postId: `${post.id}` });
+            }}
+          >
+            <Flex align='center' css={{ gap: '10px', color: `${colors.gray10}` }}>
+              <IconAlertTriangle css={{ width: '16px', height: '16px' }} />
+              신고
+            </Flex>
+          </FeedDropdown.Item>
+        </FeedDropdown>
+      }
+      like={
+        <LoggingClick
+          eventKey={post.isLiked ? 'feedUnlike' : 'feedLike'}
+          param={{ feedId: String(post.id), category: post.categoryName }}
+        >
+          <FeedLike isLiked={post.isLiked} likes={post.likes} onClick={handleFeedLike} type={'heart'} />
+        </LoggingClick>
+      }
+    >
+      {post.images.length !== 0 && (
+        <FeedCard.Image>
+          {post.images.map((image: string, index: number) => (
+            <FeedCard.ImageItem key={`${image}-${index}`} src={image} height={240} />
+          ))}
+        </FeedCard.Image>
+      )}
+    </FeedCard>
+  );
+};
+
+export default CrewFeedListItem;
---
+++ src/components/feed/list/CrewFeedList/index.tsx
@@ -0,0 +1,113 @@
+import styled from '@emotion/styled';
+import { colors } from '@sopt-makers/colors';
+import { useRef } from 'react';
+import { Virtuoso, VirtuosoHandle } from 'react-virtuoso';
+import { atom, useRecoilState } from 'recoil';
+
+import { useGetPostsInfiniteQuery } from '@/api/endpoint/feed/getPosts';
+import { getMemberInfo } from '@/components/feed/common/utils';
+import CrewFeedListItem from '@/components/feed/list/CrewFeedList/CrewFeedListItem';
+import FeedSkeleton from '@/components/feed/list/FeedSkeleton';
+import { useNavigateBack } from '@/components/navigation/useNavigateBack';
+import { textStyles } from '@/styles/typography';
+
+interface CrewFeedListProps {
+  categoryId: string;
+  onScrollChange?: (scrolling: boolean) => void;
+}
+
+const scrollIndexAtom = atom<Record<string, number>>({
+  key: 'scrollAtom',
+  default: {},
+});
+
+const CrewFeedList = ({ categoryId, onScrollChange }: CrewFeedListProps) => {
+  const { data, refetch, fetchNextPage, isLoading, isError } = useGetPostsInfiniteQuery({
+    categoryId,
+  });
+
+  const flattenData = data?.pages.flatMap((page) => page.posts) ?? [];
+
+  const [map, setMap] = useRecoilState(scrollIndexAtom);
+  const virtuoso = useRef<VirtuosoHandle>(null);
+
+  useNavigateBack(() => {
+    const idx = map[categoryId ?? ''];
+    if (idx != null) {
+      virtuoso.current?.scrollToIndex({
+        index: idx,
+        align: 'center',
+      });
+    }
+  });
+
+  const handleFeedCardClick = (idx: number, postId: number) => {
+    setMap((map) => ({ ...map, [categoryId]: idx }));
+    window.location.href = `/group/post?id=${postId}`;
+  };
+
+  if (isLoading) return <FeedSkeleton />;
+
+  if (isError) {
+    return (
+      <div css={{ display: 'flex', justifyContent: 'center', padding: '30px 0' }}>
+        <AlertText>오류가 발생했어요.</AlertText>
+      </div>
+    );
+  }
+
+  if (data != null && flattenData.length === 0) {
+    return (
+      <div css={{ display: 'flex', justifyContent: 'center', padding: '30px 0' }}>
+        <AlertText>아직 작성된 글이 없어요(ㅠ_ㅠ)</AlertText>
+      </div>
+    );
+  }
+
+  return (
+    <Virtuoso
+      data={flattenData}
+      ref={virtuoso}
+      rangeChanged={({ startIndex, endIndex }) => {
+        setMap((map) => ({ ...map, [categoryId ?? '']: (startIndex + endIndex) / 2 }));
+      }}
+      useWindowScroll
+      endReached={() => {
+        fetchNextPage();
+      }}
+      isScrolling={onScrollChange}
+      itemContent={(idx, post) => {
+        console.info({ post });
+
+        if (post.meetingId) {
+          return (
+            <CrewFeedListItem
+              postId={post.id}
+              meetingId={post.meetingId}
+              onFeedCardClick={() => handleFeedCardClick(idx, post.id)}
+              memberInfo={getMemberInfo({
+                categoryId: post.categoryId,
+                categoryName: post.categoryName,
+                member: {
+                  activity: post.member?.activity ?? { generation: 0, part: '' },
+                  careers: post.member?.careers ?? null,
+                },
+              })}
+            />
+          );
+        }
+      }}
+    />
+  );
+};
+
+export default CrewFeedList;
+
+const AlertText = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 90px;
+  width: 100%;
+  color: ${colors.gray300};
+  ${textStyles.SUIT_14_M};
+`;
