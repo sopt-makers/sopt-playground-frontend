286e461305e78fd7d4ae230abf1c9f496e0970c0
moonseonghui <msh9980@naver.com>
2025-09-22 20:49:25 +0900

    fix: 타임캡솝 open 및 최근 기수 37기로 변경

--- src/components/common/Banner/ActiveBannerSlot.tsx
+++ src/components/common/Banner/ActiveBannerSlot.tsx
@@ -3,15 +3,16 @@ import { FC } from 'react';
 
 import AdsBanner from '@/components/common/Banner/AdsBanner';
 import { ClosingBanner } from '@/components/common/Banner/ClosingBanner';
-
+import WelcomeBannerContainer from '@/components/common/Banner/WelcomeBanner/WelcomeBannerContainer';
 interface ActiveBannerSlotProps {}
 
 const ActiveBannerSlot: FC<ActiveBannerSlotProps> = ({}) => {
+  const isTimecapsopOpen = true; // 타임캡솝 오픈 기간에만 이 값을 true로 변경
   return (
     <StyledActiveBanner>
       {/* 이 밑에 노출할 배너를 넣으세요. */}
-      <AdsBanner />
-      {/* ==== */}
+      {isTimecapsopOpen ? <WelcomeBannerContainer /> : <AdsBanner />}
+      <WelcomeBannerContainer />
     </StyledActiveBanner>
   );
 };
--- src/constants/generation.ts
+++ src/constants/generation.ts
@@ -1,4 +1,4 @@
-export const LATEST_GENERATION = 36; // TODO: 자동 갱신 로직 있으면 좋음
+export const LATEST_GENERATION = 37; // TODO: 자동 갱신 로직 있으면 좋음
 
 export const GENERATIONS = Array.from({ length: LATEST_GENERATION }, (_, i) => (i + 1).toString()).reverse();
 
