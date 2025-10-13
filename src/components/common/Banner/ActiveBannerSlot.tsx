020e07cf1df89d16af8584b93eda99d5706ab224
seonghmo <52481403+seong-hui@users.noreply.github.com>
2025-10-13 22:50:22 +0900

    feat: 타임캡솝 배너 제거 (#2008)
    * feat: 타임캡솝 배너 제거
    
    * fix: makers 페이지 빌드 에러 핸들링 추가

--- src/components/common/Banner/ActiveBannerSlot.tsx
+++ src/components/common/Banner/ActiveBannerSlot.tsx
@@ -7,7 +7,7 @@ import WelcomeBannerContainer from '@/components/common/Banner/WelcomeBanner/Wel
 interface ActiveBannerSlotProps {}
 
 const ActiveBannerSlot: FC<ActiveBannerSlotProps> = ({}) => {
-  const isTimecapsopOpen = true; // 타임캡솝 오픈 기간에만 이 값을 true로 변경
+  const isTimecapsopOpen = false; // 타임캡솝 오픈 기간에만 이 값을 true로 변경
   return (
     <StyledActiveBanner>
       {/* 이 밑에 노출할 배너를 넣으세요. */}
