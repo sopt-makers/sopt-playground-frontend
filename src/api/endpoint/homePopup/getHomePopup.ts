bb0ea7a7c5f2919792bfed9bf09cce3f494d2c0b
seonghmo <52481403+seong-hui@users.noreply.github.com>
2025-11-08 10:45:21 +0900

    feat: 홈팝업 어드민 구좌 구현 (#2016)
    * feat: homepopup get api 훅 구현
    
    * feat: homepopup post api 훅 구현
    
    * fix: homepopup api 기반 홈 팝업 렌더링으로 변경
    
    * feat: HomePopupUploader 컴포넌트 구현
    
    * fix: 멤버 검색창 ui 수정
    
    * fix: HomePopupUploader 렌더 로직 제거
    
    * fix: HomePopupUploader로직 제거
    
    * fix: 유효한 팝업만 가져오도록 api endpoint 변경

---
+++ src/api/endpoint/homePopup/getHomePopup.ts
@@ -0,0 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
+import { z } from 'zod';
+
+import { createEndpoint } from '@/api/typedAxios';
+
+const HomePopupSchema = z.object({
+  id: z.number(),
+  startDate: z.string(),
+  endDate: z.string(),
+  pcImageUrl: z.string(),
+  mobileImageUrl: z.string(),
+  linkUrl: z.string().nullable(),
+  openInNewTab: z.boolean(),
+  showOnlyToRecentGeneration: z.boolean(),
+});
+
+export const getHomePopup = createEndpoint({
+  request: () => ({
+    method: 'GET',
+    url: 'api/v1/popups/current',
+  }),
+  serverResponseScheme: HomePopupSchema,
+});
+
+export const useGetHomePopup = () =>
+  useQuery({
+    queryKey: getHomePopup.cacheKey(),
+    queryFn: () => getHomePopup.request(),
+  });
