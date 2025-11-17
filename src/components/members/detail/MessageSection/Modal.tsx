ff611de0e92db1687c4319919fa895a94f2ed9d6
imddoy <imddoy@duksung.ac.kr>
2025-11-17 17:51:17 +0900

    fix: 2차 QA 반영

--- src/components/members/detail/MessageSection/Modal.tsx
+++ src/components/members/detail/MessageSection/Modal.tsx
@@ -119,10 +119,10 @@ const ModalContent = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 0 32px 40px;
+  padding: 0 32px 32px;
 
   @media ${MB_BIG_MEDIA_QUERY} {
-    padding: 0 24px 32px;
+    padding: 0 24px 24px;
   }
 `;
 
