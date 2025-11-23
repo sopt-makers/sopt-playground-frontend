ad3084450c28fe607924414e7c289671845c95fc
imddoy <imddoy@duksung.ac.kr>
2025-11-23 15:51:31 +0900

    feat: 모임 가상화 추가

--- src/components/members/detail/GroupSection/index.tsx
+++ src/components/members/detail/GroupSection/index.tsx
@@ -3,6 +3,8 @@ import { colors } from '@sopt-makers/colors';
 import { fonts } from '@sopt-makers/fonts';
 import axios from 'axios';
 import Link from 'next/link';
+import { useCallback, useRef } from 'react';
+import { useVirtualizer } from '@tanstack/react-virtual';
 
 import { ProfileDetail } from '@/api/endpoint_LEGACY/members/type';
 import ResizedImage from '@/components/common/ResizedImage';
@@ -10,9 +12,7 @@ import Text from '@/components/common/Text';
 import MemberMeetingCard from '@/components/members/detail/ActivitySection/MemberMeetingCard';
 import EmptyProfile from '@/components/members/detail/EmptyProfile';
 import { playgroundLink } from '@/constants/links';
-import useEnterScreen from '@/hooks/useEnterScreen';
 import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
-import { safeParseInt } from '@/utils';
 import { useMemberMeetingList } from '@/api/crew/getMeetingList';
 import Loading from '@/components/common/Loading';
 
@@ -25,16 +25,21 @@ interface GroupSectionProps {
 const GroupSection = ({ profile, meId, memberId }: GroupSectionProps) => {
   const { data: meetingData, isPending, error: crewError } = useMemberMeetingList(memberId);
 
-  // 추후 플그 API 재사용 가능성 있음
-  // const { ref } = useEnterScreen({
-  //   onEnter: () => {
-  //     fetchNextPage();
-  //   },
-  // });
+  const parentRef = useRef<HTMLDivElement>(null);
 
   if (axios.isAxiosError(crewError) && crewError.response?.status === 400) {
     return <EmptyProfile />;
   }
+  const meetingList = meetingData?.userAppliedMeetings ?? []; // 데이터를 안전하게 추출
+  const ITEMS_PER_ROW = 2;
+  const ROW_COUNT = Math.ceil(meetingList.length / ITEMS_PER_ROW);
+
+  const rowVirtualizer = useVirtualizer({
+    count: ROW_COUNT,
+    getScrollElement: () => parentRef.current,
+    overscan: 6,
+    estimateSize: useCallback(() => 136, []),
+  });
 
   if (isPending || !profile || !meetingData)
     return (
@@ -43,8 +48,6 @@ const GroupSection = ({ profile, meId, memberId }: GroupSectionProps) => {
       </Container>
     );
 
-  const meetingList = meetingData.userAppliedMeetings;
-
   return (
     <>
       {meetingList.length > 0 ? (
@@ -52,16 +55,45 @@ const GroupSection = ({ profile, meId, memberId }: GroupSectionProps) => {
           <ActivityTitle>
             {profile.name}님이 참여한 {meetingList.length}개의 모임이에요!
           </ActivityTitle>
-          <ActivityDisplay>
-            {meetingList.map((meeting) => (
-              <MemberMeetingCard
-                key={meeting.meetingId}
-                {...meeting}
-                {...(meeting.isLeader && { userName: profile.name })}
-              />
-            ))}
+          <ActivityDisplay
+            ref={parentRef}
+            style={{
+              height: '800px',
+              overflowY: 'auto',
+            }}
+          >
+            <div
+              style={{
+                height: `${rowVirtualizer.getTotalSize()}px`,
+                position: 'relative',
+              }}
+            >
+              {rowVirtualizer.getVirtualItems().map((virtualRow) => {
+                const startIndex = virtualRow.index * ITEMS_PER_ROW;
+                const endIndex = Math.min(startIndex + ITEMS_PER_ROW, meetingList.length);
+                const itemsInRow = meetingList.slice(startIndex, endIndex);
+
+                return (
+                  <MemberMeetingCardWrapper
+                    data-index={virtualRow.index}
+                    style={{
+                      transform: `translateY(${virtualRow.start}px)`,
+                      height: `${virtualRow.size}px`,
+                    }}
+                  >
+                    {itemsInRow.map((meeting) => (
+                      <MemberMeetingCard
+                        key={meeting.meetingId}
+                        {...meeting}
+                        {...(meeting.isLeader && { userName: profile.name })}
+                      />
+                    ))}
+                    {itemsInRow.length === 1 && <div />}
+                  </MemberMeetingCardWrapper>
+                );
+              })}
+            </div>
           </ActivityDisplay>
-          {/* <Target ref={ref} /> */}
         </Container>
       ) : (
         <>
@@ -101,20 +133,26 @@ const ActivityTitle = styled.div`
 `;
 
 const ActivityDisplay = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, minmax(10px, 1fr));
-  row-gap: 20px;
-  column-gap: 29px;
   margin-top: 32px;
+`;
+
+const MemberMeetingCardWrapper = styled.div`
+  display: grid;
+  position: absolute;
+  top: 0;
+  left: 0;
+  grid-template-columns: repeat(2, minmax(10px, 1fr));
+  column-gap: 29px;
+  padding-bottom: 20px;
+  width: 100%;
 
   @media ${MOBILE_MEDIA_QUERY} {
     display: flex;
     flex-direction: column;
     gap: 24px;
-    margin-top: 24px;
+    padding-bottom: 0;
   }
 `;
-
 const ActivityUploadNudge = styled.div`
   display: flex;
   position: relative;
